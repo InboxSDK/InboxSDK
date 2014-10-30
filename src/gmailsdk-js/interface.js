08cd12f1c0d47935cfd2143135d891154d205ce8
Chris Cowan <agentme49@gmail.com>
2014-10-29 17:44:08 -0700

    Move example mailbox event out of interface.js

--- src/gmailsdk-js/interface.js
+++ src/gmailsdk-js/interface.js
@@ -1,6 +1,5 @@
 var RSVP = require('rsvp');
 var _ = require('lodash');
-var Mailbox = require('./mailbox');
 var loadImpFile = require('./load-imp-file');
 
 var Interface = {
@@ -12,7 +11,6 @@ var Interface = {
       return global.__GmailSDKImpLoader.load("0.1");
     }).then(function(Imp) {
       Interface.Imp = Imp;
-      Mailbox.emit('example', 'implementation loaded');
       return Imp;
     });
   }),
