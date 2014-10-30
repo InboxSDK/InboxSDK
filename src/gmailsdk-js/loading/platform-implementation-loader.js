041952a0d550d992699aaa65c564740c754b6628
Chris Cowan <agentme49@gmail.com>
2014-10-30 15:19:56 -0700

    simplify loader

--- src/gmailsdk-js/loading/platform-implementation-loader.js
+++ src/gmailsdk-js/loading/platform-implementation-loader.js
@@ -1,31 +1,28 @@
 var RSVP = require('rsvp');
 var _ = require('lodash');
-var loadScript = require('../../common/load-script');
-var Mailbox = require('../api-definitions/mailbox');
 
 var PlatformImplementationLoader = function(appId) {
-    this._appId = appId;
+    var self = this;
+    this.load = _.once(function() {
+        return RSVP.resolve().then(function() {
+            if (!global.__GmailSDKImpLoader) {
+                return self._loadScript().then(function() {
+                    if (!global.__GmailSDKImpLoader) {
+                        throw new Error("Implementation file did not load correctly");
+                    }
+                });
+            }
+        }).then(function() {
+            return global.__GmailSDKImpLoader.load("0.1", appId);
+        });
+    });
 };
 
 _.extend(PlatformImplementationLoader.prototype, {
-    load: _.once(function() {
-        var appId = this._appId;
-        if (global.__GmailSDKImpLoader) {
-            return RSVP.resolve();
-        }
-        return this._loadScript().then(function() {
-            if (!global.__GmailSDKImpLoader) {
-                throw new Error("Implementation file did not load correctly");
-            }
-            return global.__GmailSDKImpLoader.load("0.1", appId);
-        }).then(function(platformImplementation) {
-            Mailbox.emit('example', 'implementation loaded');
-            return platformImplementation;
-        });
-    }),
-
     _loadScript: function(){
-      return loadScript('http://localhost:4567/platform-implementation.js');
+        var loadScript = require('../../common/load-script');
+        return loadScript('http://localhost:4567/platform-implementation.js');
     }
 });
+
 module.exports = PlatformImplementationLoader;
