2a1d8cb7fbd6350848c446ff59c5357b2b832954
Chris Cowan <agentme49@gmail.com>
2014-10-30 19:04:59 -0700

    track tweak

--- src/gmailsdk-js/track.js
+++ src/gmailsdk-js/track.js
@@ -5,7 +5,10 @@ module.exports = function(platformImplementationLoader) {
     assert.equal(typeof eventName, 'string', 'eventName must be a string');
     if (details) {
       assert.equal(typeof details, 'object', 'details must be an object');
+      // Make sure it's JSON encodable
+      JSON.stringify(details);
     }
+
     if (arguments.length > 2) {
       throw new Error("Too many parameters");
     }
