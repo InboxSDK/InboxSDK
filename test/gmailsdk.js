ec127671797fa331ae52fa2f4e1075cc671c85b0
Chris Cowan <agentme49@gmail.com>
2014-10-30 17:41:24 -0700

    mocha test to try to load gmail sdk. (Fails currently)

---
+++ test/gmailsdk.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var RSVP = require('./lib/rsvp');
+var env = require('jsdom').env;
+
+var GmailSDK = require('./lib/gmailsdk');
+
+describe('gmailsdk', function() {
+  global.window = global.document = null;
+
+  before(function() {
+    return new RSVP.Promise(function(resolve, reject) {
+      env('<!doctype html><html><body><div id="canvas"></div></body></html>', function (errs, _window) {
+        if (errs) {
+          reject(errs);
+          return;
+        }
+        window = _window;
+        document = window.document;
+        resolve();
+      });
+    });
+  });
+
+  after(function() {
+    window.close();
+    delete global.window;
+    delete global.document;
+  });
+
+  it('should load', function() {
+    this.slow();
+    var gmailsdk = new GmailSDK("test");
+    return gmailsdk._platformImplementationLoader.load();
+  });
+});
---
+++ test/lib/gmailsdk.js
@@ -0,0 +1,3 @@
+require('../../src/gmailsdk-js/loading/platform-implementation-loader').prototype._loadScript = require('../../src/gmailsdk-js/loading/load-platform-implementation-DEV')(0);
+
+module.exports = require('../../src/gmailsdk-js/gmailsdk.js');
