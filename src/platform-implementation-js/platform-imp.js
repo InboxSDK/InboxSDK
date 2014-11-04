fbd1985769ae7f3524ba8f972b75a05a8b7e46b3
Chris Cowan <agentme49@gmail.com>
2014-11-03 19:06:24 -0800

    Underp error tracking in the implementation.
    Allows the implementation to log its own errors without too much hassle,
    by adding the "Tracker" object.

--- src/platform-implementation-js/platform-imp.js
+++ src/platform-implementation-js/platform-imp.js
@@ -1,7 +1,7 @@
 var AttachmentCardManager = require('./managers/attachment-card-manager');
 var ComposeManager = require('./managers/compose-manager');
 var MessageManager = require('./managers/message-manager');
-
+var Tracker = require('./tracker');
 var Email = require('./email');
 
 var GmailDriver = require('./dom-driver/gmail/gmail-driver');
@@ -10,17 +10,19 @@ var GmailDriver = require('./dom-driver/gmail/gmail-driver');
 var PlatformImplementation = function(appId){
 	this._appId = appId;
 
+	this._tracker = new Tracker(appId);
 	this._driver = new GmailDriver();
+
+	this.Email = new Email(appId, this._driver);
+	this._tracker.setEmail(this.Email);
 
 	this.AttachmentCardManager = new AttachmentCardManager(appId, this._driver);
 	this.ComposeManager = new ComposeManager(appId, this._driver);
 	this.MessageManager = new MessageManager(appId, this._driver);
 
-	this.Email = new Email(appId, this._driver);
-
 	this.Utils = {
-	    logErrorToServer: require('./log-error-to-server')(this.Email),
-	    track: require('./track')(this.Email)
+	    logErrorToServer: this._tracker.logErrorToServer.bind(this._tracker),
+	    track: this._tracker.track.bind(this._tracker)
 	};
 };
 
---
+++ src/platform-implementation-js/tracker.js
@@ -0,0 +1,80 @@
+var Ajax = require('../common/ajax');
+var logErrorFactory = require('../common/log-error-factory');
+
+function Tracker(appId) {
+  this._appId = appId;
+  this._email = null;
+
+  this.logError = logErrorFactory(this.logErrorToServer.bind(this));
+}
+
+Tracker.prototype.setEmail = function(email) {
+  this._email = email;
+};
+
+Tracker.prototype._getUserEmailAddressAsync = function() {
+  if (this._email) {
+    return this._email.getUserAsync().catch(function(err) {
+      return null;
+    }).then(function(user) {
+      return user ? user.emailAddress : null;
+    });
+  } else {
+    return RSVP.Promise.resolve(null);
+  }
+};
+
+Tracker.prototype.logErrorToServer = function(reporterExtras, name, err, details) {
+  return this._getUserEmailAddressAsync().then(function(email) {
+    var stringReport = _.map(reporterExtras.stuffToLog, function(piece) {
+      if (typeof piece == 'string') {
+        return piece;
+      } else {
+        try {
+          return JSON.stringify(piece);
+        } catch(e) {
+          return '((Could not convert to JSON))';
+        }
+      }
+    }).join(' ');
+    return Ajax({
+      url: 'https://events.inboxsdk.com/api/v2/errors',
+      method: 'POST',
+      data: {
+        error: stringReport,
+        email: email
+      }
+    });
+  });
+};
+
+Tracker.prototype.track = function(eventName, details) {
+  return this._getUserEmailAddressAsync().then(function(user) {
+    details = _.extend({
+      'timestamp': new Date().getTime()*1000,
+      'screenWidth': screen.width,
+      'screenHeight': screen.height,
+      'windowWidth': window.innerWidth,
+      'windowHeight': window.innerHeight,
+      'location': document.location.href,
+      'referrer': document.referrer,
+      'email': email
+    }, details);
+
+    // TODO queue a bunch before sending
+    var events = [details];
+
+    return Ajax({
+      url: 'https://events.inboxsdk.com/api/v2/track',
+      method: 'POST',
+      data: {
+        json: JSON.stringify({
+          data: events,
+          clientRequestTimestamp: new Date().getTime()*1000
+        })
+      }
+    });
+  });
+};
+
+module.exports = Tracker;
