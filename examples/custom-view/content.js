3d5d07efa0a7b4f1119d4bdfe193076cf6471cba
Omar Ismail <omar@streak.com>
2014-11-07 19:17:44 -0800

    custom fullscreen example extension

---
+++ examples/custom-view/content.js
@@ -0,0 +1,13 @@
+var inboxSDK = new InboxSDK('custom-view');
+
+inboxSDK.FullscreenViews.registerCustom({
+	name: 'example',
+	onActivate: function(event){
+		event.el.innerHTML = 'hello world!';
+	}
+});
+
+inboxSDK.FullscreenViews.on('change', function(event){
+	console.log('name', event.view.getDescriptor().getName());
+	console.log('params', event.view.getParams());
+});
