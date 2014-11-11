0ab83a82bccd9b4a1c6c5a7cea3cd5c043a8f756
Omar Ismail <omar@streak.com>
2014-11-11 11:33:29 -0800

    only allow one style tag to be added

--- src/platform-implementation-js/dom-driver/gmail/custom-style.js
+++ src/platform-implementation-js/dom-driver/gmail/custom-style.js
@@ -1,7 +1,11 @@
-var fs = require('fs');
-var cssContent = fs.readFileSync(__dirname + '/style.css', 'utf8');
+if(!document.getElementById('inboxsdk__style')){
+	var fs = require('fs');
+	var cssContent = fs.readFileSync(__dirname + '/style.css', 'utf8');
 
-var style = document.createElement('style');
-style.textContent = cssContent;
+	var style = document.createElement('style');
+	style.id = 'inboxsdk__style';
+	style.textContent = cssContent;
 
-document.head.appendChild(style);
+	document.head.appendChild(style);
+}
+
