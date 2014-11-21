996d8ec5d8dc971c04401aad74798ecae2c06af2
Omar Ismail <omar@streak.com>
2014-11-21 12:07:30 -0800

    document modal options

--- src/inboxsdk-js/api-definitions/modal.js
+++ src/inboxsdk-js/api-definitions/modal.js
@@ -8,6 +8,22 @@ var Modal = function(platformImplementationLoader){
 
 _.extend(Modal.prototype, {
 
+    /*
+     *
+       options = {
+         title: string,
+         el: string or Element,
+         chrome: true/false, false = no padding or X button
+         buttons: [
+           {
+             isPrimary: true/false, only one primary button allowed
+           }
+         ],
+       }
+
+
+     */
+
     show: function(options){
         var modalView = new ModalView(this._platformImplementationLoader);
         modalView.show(options);
