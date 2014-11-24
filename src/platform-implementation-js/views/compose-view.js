b79a9f33ea2a141e30e95aacb9ebd196e928400f
Aleem Mawani <aleem@streak.com>
2014-11-24 09:04:53 -0800

    add docs for ComposeView and reorder

--- src/platform-implementation-js/views/compose-view.js
+++ src/platform-implementation-js/views/compose-view.js
@@ -2,6 +2,12 @@ var _ = require('lodash');
 var EventEmitter = require('events').EventEmitter;
 
 
+/**
+* @class
+* Object that represents an open compose window <b>or</b> reply widget.
+* The fields can be easily read and modified, and certain elements can
+* be attached to it. This includes buttons and sidebars.
+*/
 var ComposeView = function(composeViewImplementation, appId){
 	EventEmitter.call(this);
 
@@ -16,41 +22,109 @@ var ComposeView = function(composeViewImplementation, appId){
 	});
 };
 
+
+
 ComposeView.prototype = Object.create(EventEmitter.prototype);
 
-_.extend(ComposeView.prototype, {
+_.extend(ComposeView.prototype, /** @lends ComposeView */ {
 
-	/*
-	 * adds button to the compose
-	 */
+	/**
+	* Inserts a button into the compose bar.
+	* @param {ComposeButtonDescriptor} buttonDescriptor - The details of the button to add to the compose bar.
+	* @return {void}
+	*/
 	addButton: function(buttonDescriptor){
 		this._composeViewImplementation.addButton(buttonDescriptor, this._appId);
 	},
 
-	/*
-	* Places text inside the body of the message at the cursor or at the beginning of the message if the cursor is not in the body of the message. If anything inside the body is selected, it will be replaced.
-	*/
-	insertTextIntoBodyAtCursor: function(text){
-		this._composeViewImplementation.insertBodyTextAtCursor(text);
+	addInnerSidebar: function(options){
+		this._composeViewImplementation.addInnerSidebar(options);
+	},
+
+	addMessageSendModifier: function(modifier){
+		this._composeViewImplementation.addMessageSendModifier(modifier);
+	},
+
+	addOuterSidebar: function(options){
+		this._composeViewImplementation.addOuterSidebar(options);
 	},
 
 	/*
-	* Places html inside the body of the message at the cursor or at the beginning of the message if the cursor is not in the body of the message. If anything inside the body is selected, it will be replaced.
+	* closes the compose window
+	*/
+	close: function(){
+		this._composeViewImplementation.close();
+	},
+
+	getBccRecipients: function(){
+		return this._composeViewImplementation.getBccRecipients();
+	},
+
+	getBodyElement: function(){
+		return this._composeViewImplementation.getBodyElement();
+	},
+
+	getCcRecipients: function(){
+		return this._composeViewImplementation.getCcRecipients();
+	},
+
+	getComposeID: function(){
+		return this._composeViewImplementation.getComposeID();
+	},
+
+	getHTMLContent: function(){
+		return this._composeViewImplementation.getHTMLContent();
+	},
+
+	/*
+	* Returns a string of HTML containing any text and elements highlighted by the user inside the email body.
+	* @return {string}
+	*/
+	getSelectedBodyHTML: function(){
+		return this._composeViewImplementation.getSelectedBodyHTML();
+	},
+
+	/*
+	* Returns a plain string containing any text highlighted by the user inside the email body.
+	* @return {string}
+	*/
+	getSelectedBodyText: function(){
+		return this._composeViewImplementation.getSelectedBodyText();
+	},
+
+	getSubject: function(){
+		return this._composeViewImplementation.getSubject();
+	},
+
+	getTextContent: function(){
+		return this._composeViewImplementation.getTextContent();
+	},
+
+	getToRecipients: function(){
+		return this._composeViewImplementation.getToRecipients();
+	},
+
+	/*
+	* Places HTML inside the body of the message at the cursor or at the beginning of the message if the cursor is not in the body of the message.
+	* If anything inside the body is selected, it will be replaced.
+	* @param {string | HTMLElement} html - The HTML to insert. You can provide an HTML string or a DOM element  (HTMLElement)
+	* @return {void}
 	*/
 	insertHTMLIntoBodyAtCursor: function(html){
 		this._composeViewImplementation.insertBodyHTMLAtCursor(html);
 	},
 
 	/*
-	* inserts link into body
-	*/
-	insertLinkIntoBodyAtCursor: function(text, href){
-		this._composeViewImplementation.insertLinkIntoBody(text, href);
-	},
-
-	/*
+	* Places a link chip inside the body of the message at the cursor or at the beginning of the message if
+	* the cursor is not in the body of the message. If anything inside the body is selected, it will be replaced.
+	* A link chip is an enhanced link with an icon and a callout. These are typically used by Gmail when inserting
+	* a Google Drive link.
 	*
-	* inserts a drive-like "chip"
+	* @param {string} text - The anchor text of the link to insert.
+	* @param {string} url - The URL of the link to insert.
+	* @param {string} iconUrl - The URL of the icon that will be shown in the chip.
+	*
+	* @return {void}
 	*
 	*/
 	insertLinkChipIntoBodyAtCursor: function(text, url, iconUrl){
@@ -66,98 +140,92 @@ _.extend(ComposeView.prototype, {
 		});
 	},
 
-
 	/*
-	 * returns true/false if the current compose view is for a reply. This includes "popped-out" replies
-	 */
-	isReply: function(){
-		return this._composeViewImplementation.isReply();
+	* Places a link inside the body of the message at the cursor or at the beginning of the message
+	* if the cursor is not in the body of the message. If anything inside the body is selected, it will be replaced.
+	*
+	* @param {string} text - The anchor text of the link to insert.
+	* @param {string} url - The URL of the link to insert.
+	* @return {void}
+	*/
+	insertLinkIntoBodyAtCursor: function(text, url){
+		this._composeViewImplementation.insertLinkIntoBody(text, href);
 	},
 
 
 	/*
-	 * returns true/false if current compose view is an inline reply form
-	 */
+	* Places text inside the body of the message at the cursor or at the beginning of the message if the cursor is not in the body of the message. If anything inside the body is selected, it will be replaced.
+	* @param {string} text - the text to insert
+	* @return {void}
+	*/
+	insertTextIntoBodyAtCursor: function(text){
+		this._composeViewImplementation.insertBodyTextAtCursor(text);
+	},
+
+
+	/*
+	* returns true/false if current compose view is an inline reply form
+	*/
 	isInlineReplyForm: function(){
 		return this._composeViewImplementation.isInlineReplyForm();
 	},
 
 	/*
-	 * closes the compose window
-	 */
-	close: function(){
-		this._composeViewImplementation.close();
-	},
-
-	/*
-	 * Returns a string containing any text and elements highlighted by the user inside the email body.
-	 */
-	getSelectedBodyHTML: function(){
-		return this._composeViewImplementation.getSelectedBodyHTML();
-	},
-
-	/*
-	 * Returns a string containing any text highlighted by the user inside the email body.
-	 */
-	getSelectedBodyText: function(){
-		return this._composeViewImplementation.getSelectedBodyText();
-	},
-
-	getBodyElement: function(){
-		return this._composeViewImplementation.getBodyElement();
-	},
-
-	getHTMLContent: function(){
-		return this._composeViewImplementation.getHTMLContent();
-	},
-
-	getTextContent: function(){
-		return this._composeViewImplementation.getTextContent();
-	},
-
-	getSubject: function(){
-		return this._composeViewImplementation.getSubject();
-	},
-
-	getToRecipients: function(){
-		return this._composeViewImplementation.getToRecipients();
-	},
-
-	getCcRecipients: function(){
-		return this._composeViewImplementation.getCcRecipients();
-	},
-
-	getBccRecipients: function(){
-		return this._composeViewImplementation.getBccRecipients();
-	},
-
-	getComposeID: function(){
-		return this._composeViewImplementation.getComposeID();
-	},
-
-	setToRecipients: function(emails){
-		this._composeViewImplementation.setToRecipients(emails);
-	},
-
-	setCcRecipients: function(emails){
-		this._composeViewImplementation.setCcRecipients(emails);
+	* returns true/false if the current compose view is for a reply. This includes "popped-out" replies
+	*/
+	isReply: function(){
+		return this._composeViewImplementation.isReply();
 	},
 
 	setBccRecipients: function(emails){
 		this._composeViewImplementation.setBccRecipients(emails);
 	},
 
-	addOuterSidebar: function(options){
-		this._composeViewImplementation.addOuterSidebar(options);
+	setCcRecipients: function(emails){
+		this._composeViewImplementation.setCcRecipients(emails);
 	},
 
-	addInnerSidebar: function(options){
-		this._composeViewImplementation.addInnerSidebar(options);
-	},
-
-	addMessageSendModifier: function(modifier){
-		this._composeViewImplementation.addMessageSendModifier(modifier);
+	setToRecipients: function(emails){
+		this._composeViewImplementation.setToRecipients(emails);
 	}
+
+	/**
+	* Fires when the compose view is closed. This can be triggered by the .close method, the user
+	* clicking the close or discard buttons, the message being sent, etc.
+	* @event ComposeView#closed
+	*/
+
+	/**
+	* Fires when the user presses send.
+	* @event ComposeView#sending
+	*/
+
+	/**
+	* Fires when the Gmail server has confirmed receiving the message. An event object
+	* containing the new threadId will be passed to the event listeners.
+	* @event ComposeView#sent
+	*/
+
+	/**
+	* Fires when any of the To/Cc/Bcc fields are changed.
+	* @event ComposeView#recipientsChanged
+	*/
 });
 
 module.exports = ComposeView;
+
+
+/**
+*
+*
+* @class ComposeButtonDescriptor
+*/
+var ComposeButtonDescriptor = /** @lends ComposeButtonDescriptor */{
+
+
+/**
+* @property {string} propName
+*/
+prop: null
+
+};
