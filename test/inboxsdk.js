18ac961fa56db7eefa17fe4276200933a478a307
Chris Cowan <agentme49@gmail.com>
2014-11-26 16:38:55 -0800

    Simpler jsdom setup.

--- test/inboxsdk.js
+++ test/inboxsdk.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var sinon = require('sinon');
 var RSVP = require('./lib/rsvp');
-var env = require('jsdom').env;
+var jsdom = require('jsdom').jsdom;
 
 var InboxSDK = require('./lib/inboxsdk');
 
@@ -10,18 +10,9 @@ describe('InboxSDK', function() {
   global.MutationObserver = require('./lib/mock-mutation-observer');
 
   before(function() {
-    return new RSVP.Promise(function(resolve, reject) {
-      env('<!doctype html><html><body><div id="canvas"></div></body></html>', function (errs, _window) {
-        if (errs) {
-          reject(errs);
-          return;
-        }
-        window = _window;
-        document = window.document;
-        document.body.classList = []; // hack
-        resolve();
-      });
-    });
+    global.document = jsdom('<!doctype html><html><body><div id="canvas"></div></body></html>');
+    window = document.parentWindow;
+    document.body.classList = []; // hack
   });
 
   after(function() {
