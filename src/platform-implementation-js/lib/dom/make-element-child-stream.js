3523367c63265336605024af4aea33cddb4bf878
Chris Cowan <agentme49@gmail.com>
2014-12-12 17:16:51 -0800

    New makeElementChildStream2 function which is more composable, allowing hierarchies of observers to be made easily. Original makeElementChildStream function is still around for now for old code, but it's re-implemented as a wrapper around the new version.

--- src/platform-implementation-js/lib/dom/make-element-child-stream.js
+++ src/platform-implementation-js/lib/dom/make-element-child-stream.js
@@ -1,36 +1,24 @@
 var RSVP = require('rsvp');
 var Bacon = require('baconjs');
 
+var makeElementChildStream2 = require('./make-element-child-stream2');
+
+// DEPRECATED, use make-element-child-stream2.
 // Emits events whenever the given element has any children added or removed.
 // Also when first listened to, it emits events for existing children.
 function makeElementChildStream(element) {
-  return Bacon.fromBinder(function(sink) {
-    // We don't want to emit the start children synchronously before all
-    // stream listeners are subscribed.
-    RSVP.Promise.resolve().then(function() {
-      if (observer) {
-        Array.prototype.forEach.call(element.children, function(node) {
-          sink({type:'added', el:node});
-        });
-        observer.observe(element, {childList: true});
-      }
-    });
-
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation){
-        Array.prototype.forEach.call(mutation.addedNodes, function(node) {
-          sink({type:'added', el:node});
-        });
-        Array.prototype.forEach.call(mutation.removedNodes, function(node) {
-          sink({type:'removed', el:node});
-        });
-      });
-    });
-
-    return function() {
-      observer.disconnect();
-      observer = null;
-    };
+  return makeElementChildStream2(element, null).flatMap(function(newEvent) {
+    return Bacon.once({
+      type: 'added',
+      el: newEvent.el
+    }).merge(
+      newEvent.removalStream.map(function() {
+        return {
+          type: 'removed',
+          el: newEvent.el
+        };
+      })
+    );
   });
 }
 
---
+++ test/make-element-child-stream2.js
@@ -0,0 +1,110 @@
+var assert = require('assert');
+var RSVP = require('./lib/rsvp');
+var Bacon = require('baconjs');
+var EventEmitter = require('events').EventEmitter;
+var Symbol = require('../src/common/symbol');
+
+var makeElementChildStream = require('../src/platform-implementation-js/lib/dom/make-element-child-stream2.js');
+
+describe('makeElementChildStream2', function() {
+  global.MutationObserver = null;
+  before(function() {
+    global.MutationObserver = require('./lib/mock-mutation-observer');
+  });
+  after(function() {
+    delete global.MutationObserver;
+  });
+
+  it('should work', function(done) {
+    var child1 = Symbol('child1'), child2 = Symbol('child2'), child3 = Symbol('child3');
+
+    var target = new EventEmitter();
+    target._emitsMutations = true;
+    target.children = [child1, child2];
+
+    var call = 0;
+    makeElementChildStream(target).onValue(function(event) {
+      switch(++call) {
+        case 1:
+          assert.strictEqual(event.el, child1);
+          event.removalStream.onValue(function() {
+            target.emit('mutation', {
+              addedNodes: [child3],
+              removedNodes: []
+            });
+          });
+          break;
+        case 2:
+          assert.strictEqual(event.el, child2);
+          assert(event.removalStream instanceof Bacon.EventStream);
+          break;
+        case 3:
+          assert.strictEqual(event.el, child3);
+          assert(event.removalStream instanceof Bacon.EventStream);
+          done();
+          break;
+        default:
+          throw new Error("should not happen");
+      }
+    });
+
+    setTimeout(function() {
+      target.emit('mutation', {
+        addedNodes: [],
+        removedNodes: [child1]
+      });
+    }, 0);
+  });
+
+  it('emits removals when stopper emits', function(done) {
+    var child1 = Symbol('child1'), child2 = Symbol('child2');
+    var stopper = new Bacon.Bus();
+
+    var target = new EventEmitter();
+    target._emitsMutations = true;
+    target.children = [child1];
+
+    var call = 0;
+    var child1Removed = 0, child2Removed = 0;
+    var child1Ended = false, child2Ended = false;
+    var stream = makeElementChildStream(target, stopper);
+    stream.onValue(function(event) {
+      switch(++call) {
+        case 1:
+          assert.strictEqual(event.el, child1);
+          event.removalStream.onValue(function() {
+            child1Removed++;
+          });
+          event.removalStream.onEnd(function() {
+            child1Ended = true;
+          });
+          setTimeout(function() {
+            target.emit('mutation', {
+              addedNodes: [child2],
+              removedNodes: []
+            });
+          }, 0);
+          break;
+        case 2:
+          assert.strictEqual(event.el, child2);
+          event.removalStream.onValue(function() {
+            child2Removed++;
+          });
+          event.removalStream.onEnd(function() {
+            child2Ended = true;
+          });
+          stopper.push();
+          break;
+        default:
+          throw new Error("should not happen");
+      }
+    });
+    stream.onEnd(function() {
+      assert.strictEqual(child1Removed, 1);
+      assert.strictEqual(child2Removed, 1);
+      assert(child1Ended);
+      assert(child2Ended);
+      done();
+    });
+  });
+});
