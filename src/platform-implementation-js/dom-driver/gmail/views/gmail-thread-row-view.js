6b15ccf338cd77003f1f54ed2290c483d7d47e1a
Chris Cowan <agentme49@gmail.com>
2014-12-16 19:09:19 -0800

    Fix thread row modifications when Gmail re-renders a thread row.

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-thread-row-view.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-thread-row-view.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 var Bacon = require('baconjs');
 
+var makeMutationObserverStream = require('../../../lib/dom/make-mutation-observer-stream');
 var convertForeignInputToBacon = require('../../../lib/convert-foreign-input-to-bacon');
 var ThreadRowViewDriver = require('../../../driver-interfaces/thread-row-view-driver');
 
@@ -9,6 +10,14 @@ var GmailThreadRowView = function(element) {
 
   this._eventStream = new Bacon.Bus();
   this._element = element;
+  this._stopper = this._eventStream.filter(false).mapEnd();
+
+  // Stream that emits an event after whenever Gmail replaces the ThreadRow DOM nodes
+  this._refresher = makeMutationObserverStream(this._element, {
+    childList: true
+  }).doAction(function(x) {
+    console.log('refresher firing for element', element, x);
+  }).map(null).takeUntil(this._stopper);
 };
 
 GmailThreadRowView.prototype = Object.create(ThreadRowViewDriver.prototype);
@@ -17,7 +26,9 @@ _.extend(GmailThreadRowView.prototype, {
 
   __memberVariables: [
     {name: '_element', destroy: false},
-    {name: '_eventStream', destroy: true, get: true, destroyFunction: 'end'}
+    {name: '_eventStream', destroy: true, get: true, destroyFunction: 'end'},
+    {name: '_stopper', destroy: false},
+    {name: '_refresher', destroy: false}
   ],
 
   destroy: function() {
@@ -31,7 +42,7 @@ _.extend(GmailThreadRowView.prototype, {
   },
 
   addLabel: function(label) {
-    var labelParentDiv = this._element.querySelector('td.a4W div.xS div.xT');
+    var self = this;
     var labelDiv = document.createElement('div');
     labelDiv.className = 'yi inboxSDKadded inboxSDKlabel';
     labelDiv.innerHTML = '<div class="ar as"><div class="at" title="text" style="background-color: #ddd; border-color: #ddd;"><div class="au" style="border-color:#ddd"><div class="av" style="color: #666">text</div></div></div></div><div class="as">&nbsp;</div>';
@@ -40,9 +51,8 @@ _.extend(GmailThreadRowView.prototype, {
     var au = labelDiv.querySelector('div.au');
     var av = labelDiv.querySelector('div.av');
 
-    convertForeignInputToBacon(label).takeUntil(
-      this._eventStream.filter(false).mapEnd()
-    ).onValue(function(label) {
+    var prop = convertForeignInputToBacon(label).takeUntil(this._stopper).toProperty();
+    prop.sampledBy(this._refresher.merge(prop.toEventStream())).onValue(function(label) {
       if (!label) {
         labelDiv.remove();
       } else {
@@ -60,7 +70,8 @@ _.extend(GmailThreadRowView.prototype, {
           av.style.color = label.textColor;
         }
 
-        if (!labelDiv.parentElement) {
+        var labelParentDiv = self._element.querySelector('td.a4W div.xS div.xT');
+        if (!labelParentDiv.contains(labelDiv)) {
           labelParentDiv.insertBefore(labelDiv, labelParentDiv.firstChild);
         }
       }
@@ -72,16 +83,14 @@ _.extend(GmailThreadRowView.prototype, {
   },
 
   addAttachmentIcon: function(opts) {
-    var attachmentDiv = this._element.querySelector('td.yf.xY');
-
+    var self = this;
     var img = document.createElement('img');
     img.className = 'iP inboxSDKadded inboxSDKattachmentIcon';
     img.src = 'images/cleardot.gif';
     var currentIconUrl;
 
-    convertForeignInputToBacon(opts).takeUntil(
-      this._eventStream.filter(false).mapEnd()
-    ).onValue(function(opts) {
+    var prop = convertForeignInputToBacon(opts).takeUntil(this._stopper).toProperty();
+    prop.sampledBy(this._refresher.merge(prop.toEventStream())).onValue(function(opts) {
       if (!opts) {
         img.remove();
       } else {
@@ -92,7 +101,9 @@ _.extend(GmailThreadRowView.prototype, {
           img.style.background = "url("+opts.iconUrl+") no-repeat 0 0";
           currentIconUrl = opts.iconUrl;
         }
-        if (!img.parentElement) {
+
+        var attachmentDiv = self._element.querySelector('td.yf.xY');
+        if (!attachmentDiv.contains(img)) {
           attachmentDiv.appendChild(img);
         }
       }
@@ -100,20 +111,21 @@ _.extend(GmailThreadRowView.prototype, {
   },
 
   replaceDate: function(opts) {
-    var dateContainer = this._element.querySelector('td.xW');
-    var originalDateSpan = dateContainer.firstChild;
-    var customDateSpan = originalDateSpan.nextElementSibling;
-    if (!customDateSpan) {
-      customDateSpan = document.createElement('span');
-      customDateSpan.className = 'inboxSDKadded inboxSDKcustomDate';
-      dateContainer.appendChild(customDateSpan);
+    var self = this;
 
-      originalDateSpan.classList.add('inboxSDKhiddenInline');
-    }
+    var prop = convertForeignInputToBacon(opts).takeUntil(this._stopper).toProperty();
+    prop.sampledBy(this._refresher.merge(prop.toEventStream())).onValue(function(opts) {
+      var dateContainer = self._element.querySelector('td.xW');
+      var originalDateSpan = dateContainer.firstChild;
+      var customDateSpan = originalDateSpan.nextElementSibling;
+      if (!customDateSpan) {
+        customDateSpan = document.createElement('span');
+        customDateSpan.className = 'inboxSDKadded inboxSDKcustomDate';
+        dateContainer.appendChild(customDateSpan);
 
-    convertForeignInputToBacon(opts).takeUntil(
-      this._eventStream.filter(false).mapEnd()
-    ).onValue(function(opts) {
+        originalDateSpan.classList.add('inboxSDKhiddenInline');
+      }
+
       if (!opts) {
         customDateSpan.style.display = 'none';
         originalDateSpan.style.display = 'inline';
