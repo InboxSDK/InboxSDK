39876864fc2e6cdc63740569ec6ea60085629622
Aleem Mawani <aleem@streak.com>
2015-01-06 00:30:05 -0800

    don't append the version number to the build file

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ var dir = require('node-dir');
 var sys = require('sys');
 var execSync = require('exec-sync');
 
-var sdkFilename = 'inboxsdk-'+require('./package.json').version+'.js';
+var sdkFilename = 'inboxsdk.js';
 
 var args = stdio.getopt({
   'watch': {key: 'w', description: 'Automatic rebuild'},
