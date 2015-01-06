ee181679c7f718f3f4e484fcad35f2f2cd43c997
Chris Cowan <agentme49@gmail.com>
2015-01-06 10:15:56 -0800

    tweak comment

--- gulpfile.js
+++ gulpfile.js
@@ -110,7 +110,7 @@ function browserifyTask(name, deps, entry, destname) {
           preserveComments: 'some'
         }))))
         .pipe(streamify(sourcemaps.write(args.production ? '.' : null, {
-          // don't include sourcemap comment in the inboxsdk-x.js file that we
+          // don't include sourcemap comment in the inboxsdk.js file that we
           // distribute to developers since it'd always be broken.
           addComment: !args.production || name != 'sdk',
           sourceMappingURLPrefix: name == 'injected' ?
