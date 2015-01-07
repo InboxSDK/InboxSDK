b862a7e7ea490f3983a3d19b7783ed38de4fda3d
Chris Cowan <agentme49@gmail.com>
2015-01-07 14:16:41 -0800

    StopperBus: Accept arrays.

--- src/platform-implementation-js/lib/stopper-bus.js
+++ src/platform-implementation-js/lib/stopper-bus.js
@@ -5,7 +5,7 @@ var Set = require('es6-unweak-collections').Set;
 // streams added to it. It has a stream property which is a stopper stream which
 // emits a stop event and ends only after all of its input stopper streams have
 // stopped.
-function StopperBus(stream) {
+function StopperBus(streams) {
   this._ended = false;
   this._bus = new Bacon.Bus();
 
@@ -15,21 +15,30 @@ function StopperBus(stream) {
 
   this.stream = this._bus.mapEnd(null);
   this._streams = new Set();
-  this.add(stream);
+  this.add(streams);
 }
 
-StopperBus.prototype.add = function(newStream) {
+StopperBus.prototype.add = function(newStreams) {
   var self = this;
   if (this._ended) {
     throw new Error("Tried to add a stream to a stopped StopperBus");
   }
-  this._streams.add(newStream);
-  newStream.takeUntil(this.stream).onValue(function() {
-    self._streams.delete(newStream);
-    if (self._streams.size === 0) {
-      self._ended = true;
-      self._bus.end();
-    }
+  if (!Array.isArray(newStreams)) {
+    newStreams = [newStreams];
+  }
+  // All the new streams must be in the set before we listen on them, or else
+  // if the first stream synchronously ends, the stopperbus will end there.
+  newStreams.forEach(function(newStream) {
+    self._streams.add(newStream);
+  });
+  newStreams.forEach(function(newStream) {
+    newStream.take(1).takeUntil(self.stream).onValue(function() {
+      self._streams.delete(newStream);
+      if (self._streams.size === 0) {
+        self._ended = true;
+        self._bus.end();
+      }
+    });
   });
 };
 
--- test/stopper-bus.js
+++ test/stopper-bus.js
@@ -68,4 +68,30 @@ describe('StopperBus', function() {
       ender.add(Bacon.once(null));
     });
   });
+
+  it('stops listening to streams after first event', function(done) {
+    var ender = new StopperBus(new Bacon.Bus());
+    ender.stream.subscribe(function() {
+      throw new Error("Should not happen");
+    });
+    ender.add(Bacon.fromBinder(function(sink) {
+      sink(1);
+      return done;
+    }));
+  });
+
+  it('supports arrays of streams', function(done) {
+    var hasReachedEnd = false;
+    var b1 = new Bacon.Bus();
+    var b2 = new Bacon.Bus();
+    var ender = new StopperBus([Bacon.once(null), b1.mapEnd(null)]);
+    ender.stream.onValue(function() {
+      assert(hasReachedEnd);
+      done();
+    });
+    ender.add([b2.mapEnd(null), Bacon.once(null)]);
+    b1.end();
+    hasReachedEnd = true;
+    b2.end();
+  });
 });
