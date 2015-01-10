376d825490458d4a07556b7bec9a590cdae757b6
Chris Cowan <agentme49@gmail.com>
2015-01-09 18:38:08 -0800

    Update example extensions

--- examples/modal-example/content.js
+++ examples/modal-example/content.js
@@ -3,31 +3,34 @@ div.style.width = '800px';
 div.style.height = '400px';
 div.style.backgroundColor = 'red';
 
-var modal = (new InboxSDK('simple-example')).Modal.show({
-	el: div,
-	chrome: true,
-	buttons: [
-		{
-			type: 'PRIMARY_BUTTON',
-			text: 'Monkeys',
-			onClick: function(){
-				modal.close();
+InboxSDK.load(1, 'simple-example').then(function(inboxSDK) {
+
+	var modal = inboxSDK.Modal.show({
+		el: div,
+		chrome: true,
+		buttons: [
+			{
+				type: 'PRIMARY_BUTTON',
+				text: 'Monkeys',
+				onClick: function(){
+					modal.close();
+				},
+				orderHin: 5
 			},
-			orderHin: 5
-		},
-		{
-			text: 'Monkeys 2',
-			onClick: function(){
-				alert('bye');
+			{
+				text: 'Monkeys 2',
+				onClick: function(){
+					alert('bye');
+				},
+				orderHint: 10
 			},
-			orderHint: 10
-		},
-		{
-			text: 'Monkeys 3',
-			onClick: function(){
-				alert('bye');
-			},
-			orderHint: 0
-		}
-	]
+			{
+				text: 'Monkeys 3',
+				onClick: function(){
+					alert('bye');
+				},
+				orderHint: 0
+			}
+		]
+	});
 });
--- examples/sidebar-example/content.js
+++ examples/sidebar-example/content.js
@@ -1,45 +1,47 @@
-var inboxSDK = new InboxSDK('sidebar-example');
+InboxSDK.load(1, 'sidebar-example').then(function(inboxSDK) {
 
-inboxSDK.Conversations.registerThreadViewHandler(function(threadView){
+	inboxSDK.Conversations.registerThreadViewHandler(function(threadView){
 
-	var el = document.createElement("div");
-	el.innerHTML = 'Hello world!';
+		var el = document.createElement("div");
+		el.innerHTML = 'Hello world!';
 
-	threadView.addSidebarContentPanel({
-		title: 'Monkey',
-		iconUrl: chrome.runtime.getURL('monkey-face.jpg'),
-		el: el,
-		orderHint: 2
-	});
-
-
-	var monkeyImages = [chrome.runtime.getURL('monkey.png'), chrome.runtime.getURL('monkey-face.jpg')];
-	var monkeyIndex = 1;
-
-	var el2 = document.createElement('div');
-	el2.innerHTML = 'Hellow back!';
-
-	var options = {
-		title: 'Monkey 2',
-		iconUrl: monkeyImages[monkeyIndex],
-		el: el2,
-		orderHint: 1
-	};
-
-	var stream = new Bacon.Bus();
-	var contentPanel = threadView.addSidebarContentPanel(stream);
-
-	stream.push(options);
+		threadView.addSidebarContentPanel({
+			title: 'Monkey',
+			iconUrl: chrome.runtime.getURL('monkey-face.jpg'),
+			el: el,
+			orderHint: 2
+		});
 
 
-	contentPanel.on('activate', function(){
-		monkeyIndex = (monkeyIndex + 1)%2;
-		options.iconUrl = monkeyImages[monkeyIndex];
+		var monkeyImages = [chrome.runtime.getURL('monkey.png'), chrome.runtime.getURL('monkey-face.jpg')];
+		var monkeyIndex = 1;
+
+		var el2 = document.createElement('div');
+		el2.innerHTML = 'Hellow back!';
+
+		var options = {
+			title: 'Monkey 2',
+			iconUrl: monkeyImages[monkeyIndex],
+			el: el2,
+			orderHint: 1
+		};
+
+		var stream = new Bacon.Bus();
+		var contentPanel = threadView.addSidebarContentPanel(stream);
+
 		stream.push(options);
-	});
 
-	contentPanel.on('unload', function(){
-		console.log('unload');
+
+		contentPanel.on('activate', function(){
+			monkeyIndex = (monkeyIndex + 1)%2;
+			options.iconUrl = monkeyImages[monkeyIndex];
+			stream.push(options);
+		});
+
+		contentPanel.on('unload', function(){
+			console.log('unload');
+		});
+
 	});
 
 });
