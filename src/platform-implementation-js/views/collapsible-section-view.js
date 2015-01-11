41b3e06bf837d5c4202b7355c4f332b03e055aca
Aleem Mawani <aleem@streak.com>
2015-01-11 13:35:47 -0800

    docs for collapsible section view

--- src/platform-implementation-js/views/collapsible-section-view.js
+++ src/platform-implementation-js/views/collapsible-section-view.js
@@ -8,8 +8,18 @@ var Map = require('es6-unweak-collections').Map;
 var membersMap = new Map();
 
 /**
- * This class represents a result section.
- */
+* @class
+* CollapsibleSectionViews allow you to display additional content on ListRouteViews. They are
+* typically rendered as additional content above the list of threads below. The visual style
+* is similar to that of multiple inbox sections used in native Gmail and Inbox. Note that the
+* rendering may vary slightly depending on the actual ListRouteView that the CollapsibleSectionView
+* is rendered in. For example, CollapsibleSectionViews rendered on search results pages use different
+* header styles to match Gmail's style more accurately.
+*
+* You can either render rows (that are visually similar to Gmail/Inbox rows) or custom content in
+* your CollapsibleSectionView. However, until you call one of <code>setTableRows</code> or <code>setContent</code>
+* the CollapsibleSectionView will simply display a "Loading..." indicator.
+*/
 var CollapsibleSectionView = function(collapsibleSectionViewDriver, driver){
 	EventEmitter.call(this);
 
@@ -22,11 +32,13 @@ var CollapsibleSectionView = function(collapsibleSectionViewDriver, driver){
 
 CollapsibleSectionView.prototype = Object.create(EventEmitter.prototype);
 
-_.extend(CollapsibleSectionView.prototype, {
+_.extend(CollapsibleSectionView.prototype, /** @lends CollapsibleSectionView */ {
 
 	/**
-	 * Set the results for this section
-	 * @param {ResultDescriptor[]}
+	 * Sets the rows to render in this section. These render like typical Gmail or Inbox rows with respect to style.
+	 *
+	 * Calling this function with an empty array is allowed and encouraged. Doing this will remove the loading indicator and show "No Results" or similar.
+	 * @param {RowDescriptor[]} - the rows to render
 	 */
 	setTableRows: function(rows){
 		if(!membersMap.has(this)){
@@ -34,6 +46,14 @@ _.extend(CollapsibleSectionView.prototype, {
 		}
 
 		membersMap.get(this).collapsibleSectionViewDriver.setTableRows(rows);
+	},
+
+	/**
+	* Sets an arbitrary or custom HTML element as the content of this CollapsibleSectionView
+	* @param {HTMLElement} - the element to show in the collapsible section
+	*/
+	setContent: function(ele){
+		// TODO not implemented
 	},
 
 	setCollapsed: function(value){
@@ -106,24 +126,7 @@ function _bindToEventStream(collapsibleSectionView, collapsibleSectionViewDriver
 }
 
 
-var ResultDescriptor = /** @lends ResultDescriptor */ {
-
-/**
- * An optional url to an icon to display an icon alongside the name of the NavItem
- * ^optional
- * ^default=null
- * @type {string}
- */
-iconUrl: null,
-
-/**
- * An optional class to apply to the icon
- * ^optional
- * ^default=null
- * @type {string}
- */
-iconClass: null,
-
+var ResultDescriptor = /** @lends RowDescriptor */ {
 
 /**
  * Bolded text, first textual column
@@ -143,9 +146,24 @@ body: null,
  */
 extraText: null,
 
+/**
+* An optional url to an icon to display on the left side of the row
+* ^optional
+* ^default=null
+* @type {string}
+*/
+iconUrl: null,
 
 /**
- * The name of the route to navigate to when the result is clicked on
+* An optional class to apply to the icon
+* ^optional
+* ^default=null
+* @type {string}
+*/
+iconClass: null,
+
+/**
+ * The name of the route to navigate to when the row is clicked on
  * ^optional
  * ^default=null
  * @type {string}
@@ -153,7 +171,7 @@ extraText: null,
 routeID: null,
 
 /**
- * The parameters of the route being navigated to when the result is clicked on
+ * The parameters of the route being navigated to when the row is clicked on
  * ^optional
  * ^default=null
  * @type {string[]}
@@ -162,7 +180,7 @@ routeParams: null,
 
 
 /**
- * Callback for when the result is clicked on
+ * Callback for when the row is clicked on
  * ^optional
  * ^default=null
  * @type {function}
