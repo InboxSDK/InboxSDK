14c4fda5156e13aa35df654d30c539438d2f636a
Chris Cowan <agentme49@gmail.com>
2015-01-29 16:20:30 -0800

    Make createLink not generate an absolute link that causes the page to reload.

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/goto-view.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/goto-view.js
@@ -1,14 +1,5 @@
 var getURLObject = require('./get-url-object');
 
 module.exports = function(gmailDriver, viewName, params){
-
-	var newURL = gmailDriver.createLink(viewName, params);
-	var currentURLObject = getURLObject(location.href);
-
-	if(currentURLObject.query){
-		newURL += '?' + currentURLObject.query;
-	}
-
-	location.hash = newURL.split('#')[1];
+	window.location = gmailDriver.createLink(viewName, params);
 };
-
