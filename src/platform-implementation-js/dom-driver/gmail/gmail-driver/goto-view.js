34b026556a65f7a6b77aa1a4438d3f31b935e69a
Chris Cowan <agentme49@gmail.com>
2015-01-29 19:08:58 -0800

    Revert goto-view changes.
    Gmail changes the hash after location changes to re-add the query string
    and breaks the back button, so we need to work around that.

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/goto-view.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/goto-view.js
@@ -1,5 +1,16 @@
 var getURLObject = require('./get-url-object');
 
 module.exports = function(gmailDriver, viewName, params){
-	window.location = gmailDriver.createLink(viewName, params);
+
+	var newURL = gmailDriver.createLink(viewName, params);
+	var currentURLObject = getURLObject(location.href);
+
+	// Gmail changes the hash after location changes to re-add the query string
+	// and breaks the back button, so we need to work around that.
+
+	if(currentURLObject.query){
+		newURL += '?' + currentURLObject.query;
+	}
+
+	window.location.hash = newURL.split('#')[1];
 };
