66b9e0f7910ec087be86a50703006401b42fad3f
Chris Cowan <agentme49@gmail.com>
2015-02-02 16:44:29 -0800

    Tighten some variable scoping with ES6 magic.

--- src/injected-js/setup-gmail-interceptor.js
+++ src/injected-js/setup-gmail-interceptor.js
@@ -130,81 +130,83 @@ function setupGmailInterceptor() {
     });
   }
 
-  // Search query replacer.
-  // The content script tells us search terms to watch for. Whenever we see a
-  // search query containing the term, we delay it being sent out, trigger an
-  // event containing the full query, and wait for a response event from the
-  // content script that contains a new query to substitute in.
-  var customSearchTerms = [];
-  var queryReplacement;
+  {
+    // Search query replacer.
+    // The content script tells us search terms to watch for. Whenever we see a
+    // search query containing the term, we delay it being sent out, trigger an
+    // event containing the full query, and wait for a response event from the
+    // content script that contains a new query to substitute in.
+    let customSearchTerms = [];
+    let queryReplacement;
 
-  document.addEventListener('inboxSDKcreateCustomSearchTerm', function(event) {
-    customSearchTerms.push(event.detail.term);
-  });
+    document.addEventListener('inboxSDKcreateCustomSearchTerm', function(event) {
+      customSearchTerms.push(event.detail.term);
+    });
 
-  document.addEventListener('inboxSDKsearchReplacementReady', function(event) {
-    // Go through all the queries, resolve the matching ones, and then remove
-    // them from the list.
-    if (queryReplacement.query === event.detail.query) {
-      queryReplacement.newQuery.resolve(event.detail.newQuery);
-    }
-  });
-
-  js_frame_wrappers.push({
-    isRelevantTo: function(connection) {
-      var customSearchTerm;
-      var params = connection.params;
-      if (
-        connection.method === 'POST' &&
-        params.search && params.view === 'tl' &&
-        connection.url.match(/^\?/) &&
-        params.q &&
-        (customSearchTerm = _.intersection(customSearchTerms, quotedSplit(params.q))[0])
-      ) {
-        if (queryReplacement && queryReplacement.query === params.q && queryReplacement.start != params.start) {
-          // If this is the same query that was made last, but just for a
-          // different page, then re-use the replacement query we got last time.
-          // Don't wait on the extension to come up with it again (and risk it
-          // giving an inconsistent answer between pages).
-          connection._queryReplacement = queryReplacement;
-          // Mark the old queryReplacement with this page now so we can tell on
-          // a later request whether the page was changed or the list refresh
-          // button was hit.
-          queryReplacement.start = params.start;
-        } else {
-          if (queryReplacement) {
-            // Resolve the old one with something because no one else is going
-            // to after it's replaced in a moment.
-            queryReplacement.newQuery.resolve(queryReplacement.query);
-          }
-          queryReplacement = connection._queryReplacement = {
-            term: customSearchTerm,
-            query: params.q,
-            start: params.start,
-            newQuery: RSVP.defer()
-          };
-          triggerEvent({
-            type: 'searchQueryForReplacement',
-            term: customSearchTerm,
-            query: params.q
-          });
-        }
-        return true;
+    document.addEventListener('inboxSDKsearchReplacementReady', function(event) {
+      // Go through all the queries, resolve the matching ones, and then remove
+      // them from the list.
+      if (queryReplacement.query === event.detail.query) {
+        queryReplacement.newQuery.resolve(event.detail.newQuery);
       }
-      return false;
-    },
-    requestChanger: function(connection, request) {
-      return connection._queryReplacement.newQuery.promise.then(function(newQuery) {
-        var newParams = _.clone(connection.params);
-        newParams.q = newQuery;
-        return {
-          method: request.method,
-          url: '?'+stringify(newParams),
-          body: request.body
-        };
-      });
-    }
-  });
+    });
+
+    js_frame_wrappers.push({
+      isRelevantTo: function(connection) {
+        var customSearchTerm;
+        const params = connection.params;
+        if (
+          connection.method === 'POST' &&
+          params.search && params.view === 'tl' &&
+          connection.url.match(/^\?/) &&
+          params.q &&
+          (customSearchTerm = _.intersection(customSearchTerms, quotedSplit(params.q))[0])
+        ) {
+          if (queryReplacement && queryReplacement.query === params.q && queryReplacement.start != params.start) {
+            // If this is the same query that was made last, but just for a
+            // different page, then re-use the replacement query we got last time.
+            // Don't wait on the extension to come up with it again (and risk it
+            // giving an inconsistent answer between pages).
+            connection._queryReplacement = queryReplacement;
+            // Mark the old queryReplacement with this page now so we can tell on
+            // a later request whether the page was changed or the list refresh
+            // button was hit.
+            queryReplacement.start = params.start;
+          } else {
+            if (queryReplacement) {
+              // Resolve the old one with something because no one else is going
+              // to after it's replaced in a moment.
+              queryReplacement.newQuery.resolve(queryReplacement.query);
+            }
+            queryReplacement = connection._queryReplacement = {
+              term: customSearchTerm,
+              query: params.q,
+              start: params.start,
+              newQuery: RSVP.defer()
+            };
+            triggerEvent({
+              type: 'searchQueryForReplacement',
+              term: customSearchTerm,
+              query: params.q
+            });
+          }
+          return true;
+        }
+        return false;
+      },
+      requestChanger: function(connection, request) {
+        return connection._queryReplacement.newQuery.promise.then(function(newQuery) {
+          var newParams = _.clone(connection.params);
+          newParams.q = newQuery;
+          return {
+            method: request.method,
+            url: '?'+stringify(newParams),
+            body: request.body
+          };
+        });
+      }
+    });
+  }
 }
 
 function triggerEvent(detail) {
