3882c4cf59ea63a5b48d31bbaa49750cdc6c8cb7
Chris Cowan <agentme49@gmail.com>
2015-02-05 15:30:31 -0800

    Fix attachment icon

--- src/platform-implementation-js/dom-driver/gmail/lib/update-icon/update-icon.js
+++ src/platform-implementation-js/dom-driver/gmail/lib/update-icon/update-icon.js
@@ -4,16 +4,6 @@ var updateIconClass = require('./update-icon-class');
 module.exports = function(iconSettings, containerElement, append, iconClass, iconUrl){
 
 	updateIconUrl.apply(iconSettings, [containerElement, append, iconUrl]);
-
-	if(iconUrl){
-		if(iconSettings._iconElement){
-			iconSettings._iconElement.remove();
-			iconSettings._iconElement = null;
-		}
-
-		return;
-	}
-
 	updateIconClass.apply(iconSettings, [containerElement, append, iconClass]);
 
 };
