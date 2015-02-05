2fe8f099b61ababa1c52c0397122881119704651
Chris Cowan <agentme49@gmail.com>
2015-02-04 19:01:12 -0800

    rename event

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/gmail-load-event.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/gmail-load-event.js
@@ -3,7 +3,7 @@ const gmailElementGetter = require('../gmail-element-getter');
 module.exports = function gmailLoadEvent(driver) {
   const pageCommunicator = driver.getPageCommunicator();
 
-  driver.getLogger().eventGmail('gmailLoaded', {
+  driver.getLogger().eventGmail('gmailSettings', {
     language: pageCommunicator.getUserLanguage(),
     previewPane: pageCommunicator.getUserOriginalPreviewPaneMode()
     // isConversationViewDisabled: Gmail.isConversationViewDisabled()
