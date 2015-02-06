be56c88e703643cba03a0024277e93a200c7f2dc
Chris Cowan <agentme49@gmail.com>
2015-02-05 18:02:31 -0800

    Inline the updateIcon functions together.
    They all manipulate the same state object and aren't useful independently.

--- src/platform-implementation-js/dom-driver/gmail/lib/update-icon/update-icon.js
+++ src/platform-implementation-js/dom-driver/gmail/lib/update-icon/update-icon.js
@@ -1,10 +1,67 @@
-var updateIconUrl = require('./update-icon-url');
-var updateIconClass = require('./update-icon-class');
+function createIconElement(iconSettings, containerElement, append){
+	iconSettings._iconElement = document.createElement('div');
+	iconSettings._iconElement.classList.add('inboxsdk__button_icon');
+	iconSettings._iconElement.innerHTML = '&nbsp;';
 
-// TODO make this into just one class
-module.exports = function(iconSettings, containerElement, append, iconClass, iconUrl){
+	if(append){
+		containerElement.appendChild(iconSettings._iconElement);
+	}
+	else{
+		containerElement.insertBefore(iconSettings._iconElement, containerElement.firstElementChild);
+	}
+}
 
-	updateIconUrl.apply(iconSettings, [containerElement, append, iconUrl]);
-	updateIconClass.apply(iconSettings, [containerElement, append, iconClass]);
+function createIconImgElement(iconSettings, containerElement, append){
+	if(!iconSettings._iconElement){
+		createIconElement(iconSettings, containerElement, append);
+	}
 
+	iconSettings._iconElement.innerHTML = '';
+
+	iconSettings._iconImgElement = document.createElement('img');
+	iconSettings._iconImgElement.classList.add('inboxsdk__button_iconImg');
+
+	iconSettings._iconImgElement.src = iconSettings._iconUrl;
+	iconSettings._iconElement.appendChild(iconSettings._iconImgElement);
+}
+
+// TODO make this return a class instead of taking the iconSettings state object
+module.exports = function updateIcon(iconSettings, containerElement, append, newIconClass, newIconUrl){
+	if(!iconSettings._iconUrl && newIconUrl){
+		iconSettings._iconUrl = newIconUrl;
+		createIconImgElement(iconSettings, containerElement, append);
+	}
+	else if(iconSettings._iconUrl && !newIconUrl){
+		iconSettings._iconImgElement.remove();
+		iconSettings._iconImgElement = null;
+		if (!iconSettings._iconClass) {
+			iconSettings._iconElement.remove();
+			iconSettings._iconElement = null;
+		}
+		iconSettings._iconUrl = newIconUrl;
+	}
+	else if(newIconUrl){
+		iconSettings._iconImgElement.src = newIconUrl;
+		iconSettings._iconUrl = newIconUrl;
+	}
+
+	if(!iconSettings._iconElement && newIconClass){
+		createIconElement(iconSettings, containerElement, append);
+	}
+	else if(iconSettings._iconClass && !newIconClass){
+		if(!iconSettings._iconUrl){
+			iconSettings._iconElement.remove();
+			iconSettings._iconElement = null;
+			iconSettings._iconClass = newIconClass;
+		}
+		else{
+			iconSettings._iconElement.setAttribute('class', 'inboxsdk__button_icon ');
+			iconSettings._iconClass = newIconClass;
+		}
+	}
+
+	if(newIconClass) {
+		iconSettings._iconElement.setAttribute('class', 'inboxsdk__button_icon ' + newIconClass);
+		iconSettings._iconClass = newIconClass;
+	}
 };
