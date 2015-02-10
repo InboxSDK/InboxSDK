387656e7ffa12ac1ef0c4b7a6d9d3434e5eff5e1
Aleem Mawani <aleem@streak.com>
2015-02-10 13:31:51 -0800

    document Search namespace

--- src/platform-implementation-js/platform-implementation/search.js
+++ src/platform-implementation-js/platform-implementation/search.js
@@ -5,6 +5,13 @@ var Map = require('es6-unweak-collections').Map;
 
 var memberMap = new Map();
 
+
+/**
+* @class
+* This namespace allows you to interact Gmail and Inbox's default search behaviour.
+* It allows for two use cases: 1) To provide autocomplete suggestions as the user
+* types in their query and 2) To rewrite sarch queries.
+*/
 var Search = function(appId, driver){
 	var members = {};
 	memberMap.set(this, members);
@@ -14,12 +21,17 @@ var Search = function(appId, driver){
 };
 
 
-_.extend(Search.prototype,  {
+_.extend(Search.prototype,  /** @lends Search */{
 
-	// handler must be a function that takes a string, and returns (optionally a
-	// Promise for) an array of suggestions objects. Each suggestion object may
-	// contain the properties name, nameHTML, description, descriptionHTML,
-	// iconURL, routeName, routeParams, and externalURL.
+	/**
+	* Registers a search autocomplete suggestion provider. This allows you to add results to Gmails
+	* autocomplete results when a user types into a search box. Each providers results are visually
+	* seperated from each other in the UI.
+	* @param {function(query)} handler - a handler that takes in a string <code>query</code> and
+	* returns an <code>Array</code> (or <code>Promise</code> for one) of
+	* <code>AutocompleteSearchResult</code>s.
+	* @return {void}
+	*/
 	registerSearchSuggestionsProvider: function(handler) {
 		if (typeof handler != 'function') {
 			throw new Error("Incorrect arguments");
@@ -28,15 +40,28 @@ _.extend(Search.prototype,  {
 		members.driver.registerSearchSuggestionsProvider(handler);
 	},
 
-	registerSearchQueryRewriter: function(obj) {
-		if (typeof obj.termReplacer != 'function' || typeof obj.term != 'string') {
+	/**
+	* Allows you to rewrite certain queries that may be typed in or programatically set. If a
+	* user types in a search, their search term will still be visible but the search that is
+	* actually executed by Gmails servers will be what you rewrite the query to.
+	*
+	* One common use case is to have a special view that shows emails that your app wants to render. You can
+	* navigate the user to a specific search results view (using <code>Router</code>) programtically with a
+	* search term of your choosing (so its displayed to the user), and then use <code>registerSearchQueryRewriter</code>
+	* to make sure the results you want are shown.
+	* @param {SearchQueryRewriter} rewriter - a rewriter object that specifies the term to rewrite and a function to
+	* get the new query
+	* @return {void}
+	*/
+	registerSearchQueryRewriter: function(rewriter) {
+		if (typeof rewriter.termReplacer != 'function' || typeof rewriter.term != 'string') {
 			throw new Error("Incorrect arguments");
 		}
-		if (!obj.term.match(/^app:/)) {
+		if (!rewriter.term.match(/^app:/)) {
 			throw new Error("Custom search term must begin with 'app:'");
 		}
 		var members = memberMap.get(this);
-		members.driver.registerSearchQueryRewriter(obj);
+		members.driver.registerSearchQueryRewriter(rewriter);
 	},
 
 	/* Proposed. Probably would have to return promises.
@@ -48,6 +73,12 @@ _.extend(Search.prototype,  {
 
 	},*/
 
+	/**
+	* A convenience function that generates a search query for threads matching RFC2822 Message-Id headers.
+	* This is typically used when you want to specifically show messages with a given ID
+	* @param {String[]} ids - an array of RFC2822 Message-IDs
+	* @return {String} a search query
+	*/
 	generateSearchQueryForMessagesByRfcID: function(ids) {
 		return ids.map(function(id) {
 			return 'rfc822msgid:' + id;
@@ -60,3 +91,96 @@ _.extend(Search.prototype,  {
 
 
 module.exports = Search;
+
+
+/**
+* @class
+* This type is returned by the function you pass into the
+* <code>Search.registerSearchSuggestionsProvider</code> method as a way to add
+* autocomplete suggestions to the Gmail/Inbox search box.
+*/
+var AutocompleteSearchResult = /** @lends AutocompleteSearchResult */ {
+
+	/**
+	* The name of the result to display. This property or <code>nameHTML</code> must be set
+	* @type{String}
+	*/
+	name:  null,
+
+	/**
+	* HTML to display in the name area of the result. This property or <code>name</code> must be set
+	* @type{String}
+	*/
+	nameHTML: null,
+
+	/**
+	* The name of the result to display. This property or <code>nameHTML</code> must be set
+	* ^optional
+	* ^default=empty
+	* @type{String}
+	*/
+	description:  null,
+
+	/**
+	* HTML to display in the name area of the result. This property or <code>name</code> must be set
+	* ^optional
+	* ^default=empty
+	* @type{String}
+	*/
+	descriptionHTML: null,
+
+
+	/**
+	* URL for the icon to show in the result. Should be a local extension file URL or a HTTPS url
+	* ^optional
+	* ^default=empty
+	* @type {String}
+	*/
+	iconUrl: null,
+
+	/**
+	* The name of the route to navigate to when the autocomplete search result is selected
+	* ^optional
+	* ^default=empty
+	* @type {String}
+	*/
+	routeName: null,
+
+	/**
+	* The parameters of the route being navigated to when the autocomplete search result is selected
+	* ^optional
+	* ^default=[]
+	* @type {String[]}
+	*/
+	routeParams: null,
+
+	/**
+	* An external URL to navigate to when the autocomplete search result is selected
+	* ^optional
+	* ^default=empty
+	* @type {String}
+	*/
+	externalURL: null
+};
+
+
+/**
+* @class
+* This type is passed into the
+* <code>Search.registerSearchQueryRewriter</code> method as a way to add
+* rewrite search queries
+*/
+var SearchQueryRewriter = /** @lends SearchQueryRewriter */ {
+
+	/**
+	* The query term that you'd like to rewrite/replace. No wildcards are currently supported.
+	* @type{String}
+	*/
+	term:  null,
+
+	/**
+	* A function which takes returns a String (or Promise) with the query to actually perform
+	* @type{function}
+	*/
+	termReplacer: null
+};
