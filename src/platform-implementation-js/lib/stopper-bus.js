082cc4423b5ac69a1820f0587ef1078220ec3496
Chris Cowan <agentme49@gmail.com>
2015-02-17 18:19:06 -0800

    simplify stopperbus

--- src/platform-implementation-js/lib/stopper-bus.js
+++ src/platform-implementation-js/lib/stopper-bus.js
@@ -8,17 +8,20 @@ function StopperBus(streams) {
   this._ended = false;
   this._bus = new Bacon.Bus();
 
-  // force evaluation of bus so it remembers if it gets ended while no one is
-  // listening to it.
-  this._bus.onValue(function() {});
+  this.stream = this._bus.filter(false).mapEnd(null);
 
-  this.stream = this._bus.mapEnd(null);
   this._streamCount = 0;
+  this._bus.onValue(() => {
+    this._streamCount--;
+    if (this._streamCount === 0) {
+      this._ended = true;
+      this._bus.end();
+    }
+  });
   this.add(streams);
 }
 
 StopperBus.prototype.add = function(newStreams) {
-  var self = this;
   if (this._ended) {
     throw new Error("Tried to add a stream to a stopped StopperBus");
   }
@@ -26,15 +29,7 @@ StopperBus.prototype.add = function(newStreams) {
     newStreams = [newStreams];
   }
   this._streamCount += newStreams.length;
-  newStreams.forEach(function(newStream) {
-    newStream.take(1).takeUntil(self.stream).onValue(function() {
-      self._streamCount--;
-      if (self._streamCount === 0) {
-        self._ended = true;
-        self._bus.end();
-      }
-    });
-  });
+  this._bus.plug(Bacon.mergeAll(newStreams.map(newStream => newStream.take(1))));
 };
 
 StopperBus.prototype.getSize = function() {
