099339622e322847716cb2fa9ab9b0662e100ac3
Chris Cowan <agentme49@gmail.com>
2015-04-16 18:13:25 -0700

    Make keyboard-shortcut-stream not break unit tests

--- src/platform-implementation-js/lib/dom/keyboard-shortcut-stream.js
+++ src/platform-implementation-js/lib/dom/keyboard-shortcut-stream.js
@@ -1,21 +1,13 @@
-'use strict';
-
 var Bacon = require('baconjs');
 var Combokeys = require('combokeys-capture');
 
-var combokeys = new Combokeys(document.body, true);
+const combokeys = global.document && new Combokeys(document.body, true);
 
 module.exports = function(chord){
-
 	return Bacon.fromBinder(function(sink){
-
-		var unbinder = combokeys.bind(chord, function(domEvent){
-											sink(domEvent);
-											return false;
-										}, 'keydown');
-
-		return unbinder;
-
+		return combokeys && combokeys.bind(chord, function(domEvent){
+			sink(domEvent);
+			return false;
+		}, 'keydown');
 	});
-
 };
