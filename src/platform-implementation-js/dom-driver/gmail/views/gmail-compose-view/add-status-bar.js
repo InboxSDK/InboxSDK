8c927d3ae680de55bf5b0a15cbac4a4e35611b61
Chris Cowan <agentme49@gmail.com>
2015-05-01 19:02:35 -0700

    Fix status bar overlapping footer in reply view

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/add-status-bar.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/add-status-bar.js
@@ -1,20 +1,38 @@
 import _ from 'lodash';
 import Bacon from 'baconjs';
 
+import Logger from '../../../../lib/logger';
 import SafeEventEmitter from '../../../../lib/safe-event-emitter';
 
 export default function addStatusBar(gmailComposeView) {
+	const composeEl = gmailComposeView.getElement();
+	const isInline = gmailComposeView.isInlineReplyForm();
 	const el = document.createElement('div');
 	el.className = 'aDh inboxsdk__compose_statusbar';
 
-	gmailComposeView.getElement().querySelector('.aDg .aDj > .aDh').appendChild(el);
-	gmailComposeView.getElement().classList.add('inboxsdk__compose_statusbarActive');
+	const statusArea = composeEl.querySelector('.aDg .aDj > .aDh');
+	if (!statusArea) {
+		Logger.error(new Error("Failed to find compose status area"));
+	} else {
+		composeEl.classList.add('inboxsdk__compose_statusbarActive');
+		statusArea.appendChild(el);
+
+		if (isInline) {
+			const currentPad = parseInt(composeEl.style.paddingBottom, 10) || 0;
+			composeEl.style.paddingBottom = (currentPad+40)+'px';
+		}
+	}
 
 	const statusbar = _.assign(new SafeEventEmitter(), {
 		el,
 		destroy: _.once(() => {
 			statusbar.emit('destroy');
 			el.remove();
+
+			if (isInline) {
+				const currentPad = parseInt(composeEl.style.paddingBottom, 10) || 0;
+				composeEl.style.paddingBottom = (currentPad-40)+'px';
+			}
 		})
 	});
 	gmailComposeView.getEventStream().filter(false).mapEnd()
