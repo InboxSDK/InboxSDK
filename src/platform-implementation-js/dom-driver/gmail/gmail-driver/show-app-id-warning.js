a4bdf452c7c7b2a62f2660f55cfc94ba3282cc25
Chris Cowan <agentme49@gmail.com>
2015-05-05 19:10:07 -0700

    Make appId warning closeable.

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-app-id-warning.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-app-id-warning.js
@@ -7,6 +7,11 @@ export default function showAppIdWarning(driver) {
   <div>You've loaded the InboxSDK with an unregistered appId. Registration is free but necessary to load the SDK.</div>
 </div>
 <a class="inboxsdk__appid_register" target="_blank" href="https://www.inboxsdk.com/register">Register Your App</a>
+<input type="button" value="" title="Close" class="inboxsdk__x_close_button" />
 `;
+  topDiv.querySelector('.inboxsdk__x_close_button')
+    .addEventListener('click', function(e) {
+      topDiv.remove();
+    });
   document.body.insertBefore(topDiv, document.body.firstChild);
 }
