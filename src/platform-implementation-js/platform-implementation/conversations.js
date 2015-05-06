00ba01ec00e7156129c16cfd1c82481f45f5858f
Chris Cowan <agentme49@gmail.com>
2015-05-05 17:20:45 -0700

    simplify enum declaration

--- src/platform-implementation-js/platform-implementation/conversations.js
+++ src/platform-implementation-js/platform-implementation/conversations.js
@@ -62,32 +62,22 @@ var Conversations = function(appId, driver, membraneMap){
 	* @class
 	* @name MessageViewViewStates
 	*/
-	this.MessageViewViewStates = {};
-	Object.defineProperties(this.MessageViewViewStates, /**@lends MessageViewViewStates */ {
+	this.MessageViewViewStates = Object.freeze(/**@lends MessageViewViewStates */{
 		/**
 		* In this state none of the message is visible except for the outline of its existence.
 		* @type string
 		*/
-		'HIDDEN': {
-			value: "HIDDEN",
-			writable: false
-		},
+		'HIDDEN': "HIDDEN",
 		/**
 		* In this state most of the body of the message is not visible and some recipients may not be showing.
 		* @type string
 		*/
-		'COLLAPSED': {
-			value: "COLLAPSED",
-			writable: false
-		},
+		'COLLAPSED': "COLLAPSED",
 		/**
 		* In this state all of the message is visible including the body.
 		* @type string
 		*/
-		'EXPANDED': {
-			value: "EXPANDED",
-			writable: false
-		}
+		'EXPANDED': "EXPANDED"
 	});
 };
 
