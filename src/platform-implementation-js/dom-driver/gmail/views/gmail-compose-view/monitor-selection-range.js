d1dd949f6275c6ac4abfb3153b2b42ebdec81a28
Chris Cowan <agentme49@gmail.com>
2015-05-12 16:50:57 -0700

    fix bug where compose view could forget your last selection

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/monitor-selection-range.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/monitor-selection-range.js
@@ -1,18 +1,19 @@
 const Bacon = require('baconjs');
 
-module.exports = function(gmailComposeView){
-    Bacon.mergeAll(
-        Bacon.fromEventTarget(document.body, 'mousedown'),
-        Bacon.fromEventTarget(document.body, 'keydown')
-    ).takeUntil(gmailComposeView.getEventStream().filter(false).mapEnd())
-    .onValue(function(event){
-        const body = gmailComposeView.getBodyElement();
-        const selection = document.getSelection();
-        if(body && selection && selection.rangeCount > 0 && body.contains(selection.anchorNode)){
-            gmailComposeView.setLastSelectionRange(selection.getRangeAt(0));
-        }
-        else{
-            gmailComposeView.setLastSelectionRange(null);
-        }
-    });
-};
+export default function(gmailComposeView) {
+  Bacon.mergeAll(
+    Bacon.fromEventTarget(document.body, 'mousedown'),
+    Bacon.fromEventTarget(document.body, 'keydown')
+  ).takeUntil(gmailComposeView.getEventStream().filter(false).mapEnd())
+  .onValue(function(event) {
+    const body = gmailComposeView.getBodyElement();
+    const selection = document.getSelection();
+    if (body && selection.rangeCount > 0) {
+      if (body.contains(selection.anchorNode)) {
+        gmailComposeView.setLastSelectionRange(selection.getRangeAt(0));
+      } else {
+        gmailComposeView.setLastSelectionRange(null);
+      }
+    }
+  });
+}
