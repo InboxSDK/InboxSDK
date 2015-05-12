1da6921b9da2e8c6533ce901e3ae7cbfe5a9a3bf
Chris Cowan <agentme49@gmail.com>
2015-05-12 16:54:15 -0700

    Make setToRecipients work reliably

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/set-recipients.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/set-recipients.js
@@ -1,25 +1,53 @@
+import _ from 'lodash';
+import simulateClick from '../../../../lib/dom/simulate-click';
+import simulateKey from '../../../../lib/dom/simulate-key';
+
+_.assign(window, {simulateClick, simulateKey});
+
 // contactRowIndex values: 0:to, 1:cc, 2:bcc
-module.exports = function(gmailComposeView, contactRowIndex, emailAddresses){
-	var contactRows = _getContactRows(gmailComposeView);
+export default function(gmailComposeView, contactRowIndex, emailAddresses){
+	const contactRows = _getContactRows(gmailComposeView);
 
-	if(!contactRows || contactRows.length === 0){
+	if(!contactRows || contactRows.length === 0 || !contactRows[contactRowIndex]){
 		return;
 	}
 
-	if(!contactRows[contactRowIndex]){
-		return;
-	}
-
-	var contactRow = contactRows[contactRowIndex];
-	var emailAddressEntry = contactRow.querySelector('.vO');
+	const contactRow = contactRows[contactRowIndex];
+	const emailAddressEntry = contactRow.querySelector('textarea.vO');
 
 	if(!emailAddressEntry){
 		return;
 	}
 
-	emailAddressEntry.value = emailAddresses.join(',');
-};
+	// Remove existing recipients
+	_.forEach(contactRow.querySelectorAll('.vR .vM'), el => {
+		simulateClick(el);
+	});
 
+	emailAddressEntry.value = emailAddresses.join(',');
+
+	const newEvent = document.createEvent('Events');
+	newEvent.initEvent('input', true, true);
+	emailAddressEntry.dispatchEvent(newEvent);
+
+	simulateKey(emailAddressEntry, 13, 0);
+
+	const oldRange = gmailComposeView.getLastSelectionRange();
+
+	const cover = gmailComposeView.getElement().querySelector('div.aoD.hl');
+	const focusEvent = new FocusEvent('focus');
+	cover.dispatchEvent(focusEvent);
+
+	if (document.activeElement) {
+		document.activeElement.blur();
+	}
+
+	if (oldRange) {
+		const sel = document.getSelection();
+		sel.removeAllRanges();
+		sel.addRange(oldRange);
+	}
+}
 
 function _getContactRows(gmailComposeView){
 	return gmailComposeView.getElement().querySelectorAll('.GS tr');
