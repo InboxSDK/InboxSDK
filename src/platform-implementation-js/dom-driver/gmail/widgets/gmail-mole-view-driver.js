186a2ae98e54b4b685b938ca86a7fd0576813aaa
Chris Cowan <agentme49@gmail.com>
2015-05-13 19:15:58 -0700

    add close button to mole widgets

--- src/platform-implementation-js/dom-driver/gmail/widgets/gmail-mole-view-driver.js
+++ src/platform-implementation-js/dom-driver/gmail/widgets/gmail-mole-view-driver.js
@@ -7,6 +7,15 @@ import assertInterface from '../../../lib/assert-interface';
 import MoleViewDriver from '../../../driver-interfaces/mole-view-driver';
 import GmailElementGetter from '../gmail-element-getter';
 
+function setHoverClass(el, hoverClass) {
+  el.addEventListener('mouseenter', function() {
+    el.classList.add(hoverClass);
+  });
+  el.addEventListener('mouseleave', function() {
+    el.classList.remove(hoverClass);
+  });
+}
+
 export default class GmailMoleViewDriver {
   constructor(options) {
     this._stopper = kefirStopper();
@@ -14,10 +23,22 @@ export default class GmailMoleViewDriver {
       className: 'inboxsdk__mole_view',
       innerHTML: `
 <div class="inboxsdk__mole_view_inner">
-  <div class="inboxsdk__mole_view_titlebar"><h2></h2></div>
+  <div class="inboxsdk__mole_view_titlebar">
+    <div class="inboxsdk__mole_title_buttons">
+      <!--<img class="Hl" src="images/cleardot.gif" alt="Minimize" aria-label="Minimize" data-tooltip-delay="800" data-tooltip="Minimize"><img class="Hq aUG" src="images/cleardot.gif" alt="Pop-out" aria-label="Full-screen (Shift for Pop-out)" data-tooltip-delay="800" data-tooltip="Full-screen (Shift for Pop-out)">--><img class="Ha" src="images/cleardot.gif" alt="Close" aria-label="Close" data-tooltip-delay="800" data-tooltip="Close">
+    </div>
+    <h2></h2>
+  </div>
   <div class="inboxsdk__mole_view_content"></div>
 </div>
 `
+    });
+    //setHoverClass(this._element.querySelector('.Hl'), 'Hn');
+    //setHoverClass(this._element.querySelector('.Hq'), 'Hr');
+    const closeBtn = this._element.querySelector('.Ha');
+    setHoverClass(closeBtn, 'Hb');
+    closeBtn.addEventListener('click', e => {
+      this.destroy();
     });
     this._element.querySelector('.inboxsdk__mole_view_content').appendChild(options.el);
     this.setTitle(options.title || '');
