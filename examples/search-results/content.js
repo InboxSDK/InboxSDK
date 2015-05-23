74dfdbac86d7a722e241128f00ccc022104023a1
Omar Ismail <omar@streak.com>
2015-05-23 10:08:57 -0700

    mixup internal search results example

--- examples/search-results/content.js
+++ examples/search-results/content.js
@@ -3,15 +3,13 @@ InboxSDK.load(1.0, 'search-example').then(function(inboxSDK){
 	inboxSDK.Router.handleListRoute(inboxSDK.Router.NativeRouteIDs.SEARCH, function(listRouteView){
 
 		var section = listRouteView.addCollapsibleSection({
-			title: 'Monkeys'
-		});
-
-		setTimeout(function(){
-			section.setTableRows([
+			title: 'Monkeys',
+			tableRows: [
 				{
 					title: 'title',
 					body: 'body',
 					shortDetailText: 'extra',
+					isRead: true,
 					iconUrl: chrome.runtime.getURL('monkey.png'),
 					onClick: function(){
 						console.log('hi');
@@ -22,8 +20,9 @@ InboxSDK.load(1.0, 'search-example').then(function(inboxSDK){
 					body: '2222',
 					routeID: inboxSDK.Router.NativeRouteIDs.INBOX
 				}
-			]);
-		}, 2000);
+			]
+		});
+
 
 		section.on('expanded', console.log.bind(console, 'expanded'));
 		section.on('collapsed', console.log.bind(console, 'collapsed'));
