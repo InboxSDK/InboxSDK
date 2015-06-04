a861f45bb806e7e270ceb7667869ec56cd2852b4
Omar Ismail <omar@streak.com>
2015-06-03 20:41:25 -0700

    make appId warning use topMessageBar widget

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-app-id-warning.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-app-id-warning.js
@@ -1,6 +1,14 @@
+'use strict';
+
+import Bacon from 'baconjs';
+import baconCast from 'bacon-cast';
+
 import fakeWindowResize from '../../../lib/fake-window-resize';
 
 export default function showAppIdWarning(driver) {
+
+
+
   const topDiv = document.createElement('div');
   topDiv.className = 'inboxsdk__appid_warning';
   topDiv.innerHTML = `
@@ -11,11 +19,12 @@ export default function showAppIdWarning(driver) {
 <a class="inboxsdk__appid_register" target="_blank" href="https://www.inboxsdk.com/register">Register Your App</a>
 <input type="button" value="" title="Close" class="inboxsdk__x_close_button" />
 `;
+
+ const topMessageBarDriver = driver.createTopMessageBarDriver(baconCast(Bacon, {el: topDiv}));
+
   topDiv.querySelector('.inboxsdk__x_close_button')
     .addEventListener('click', function(e) {
-      topDiv.remove();
-      fakeWindowResize();
+      topMessageBarDriver.remove();
     });
-  document.body.insertBefore(topDiv, document.body.firstChild);
-  fakeWindowResize();
+
 }
