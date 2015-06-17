fe5876f4f8c958686d9dd5fdf0624a472424b212
Chris Cowan <agentme49@gmail.com>
2015-06-17 13:47:36 -0700

    Add tools/install-deps.sh

---
+++ tools/install-deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ..
+
+# When https://github.com/npm/npm/issues/7268 is fixed, remove this if-check
+# and just always call `npm install`.
+if ! ./node_modules/.bin/gulp noop &>/dev/null; then
+  echo "Installing dependencies..."
+  rm -rf node_modules
+  npm install
+
+  if ! ./node_modules/.bin/gulp noop &>/dev/null; then
+    echo "Failed to install dependencies." >&2
+    exit 11
+  fi
+fi
+
+echo "Dependencies installed."
