fc1ff247be1e37605723d1f34f65e09a7c2e4e3c
Chris Cowan <agentme49@gmail.com>
2015-06-22 14:45:44 -0700

    Whitelist certain attribute values from censorHTMLstring.

--- src/common/censor-html-string.js
+++ src/common/censor-html-string.js
@@ -1,10 +1,19 @@
 /* @flow */
 //jshint ignore:start
 
+var ATTRIBUTE_WHITELIST = new Set([
+  "class", "rel", "target", "aria-hidden", "type", "tabindex", "id",
+  "style", "role"
+]);
+
 // Takes an HTML string, and returns it with all of the text nodes and
 // attribute values censored.
+// When we fail to parse data out of Gmail's HTML, we can use this function and
+// then log the censored HTML so we know we aren't logging user data.
 export default function censorHTMLstring(html: string): string {
   return html.replace(/(^|>)([^<]+)/g, (((match: string, start: string, text: string) =>
     start + (text.trim().length ? '...' : '')
-  ): Function)).replace(/"[^"]+"/g, '"..."').replace(/'[^']+'/g, "'...'");
+  ): Function)).replace(/(\S+)\s*=\s*"[^"]+"/g, (((match: string, attr: string) =>
+    ATTRIBUTE_WHITELIST.has(attr) ? match : `${attr}="..."`
+  ): Function));
 }
--- test/element-to-censored-string.js
+++ test/element-to-censored-string.js
@@ -5,8 +5,8 @@ import censorHTMLstring from '../src/common/censor-html-string';
 describe('censorHTMLstring', function() {
   it('works', function() {
     assert.strictEqual(
-      censorHTMLstring('abc<b href="&amp;" bar src="foo">foo</b><i src="aa"> </i>def'),
-      '...<b href="..." bar src="...">...</b><i src="..."></i>...'
+      censorHTMLstring('abc<b href="&amp;" bar src="foo" class="whitelisted">foo</b><i src="aa"> </i>def'),
+      '...<b href="..." bar src="..." class="whitelisted">...</b><i src="..."></i>...'
     );
   });
 });
