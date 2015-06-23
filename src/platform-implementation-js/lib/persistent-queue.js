5081582ace9f51195add03ef5d288dd42c65e329
Chris Cowan <agentme49@gmail.com>
2015-06-23 12:18:26 -0700

    type persistentqueue to catch errors with its use and make it clearer.

--- src/platform-implementation-js/lib/persistent-queue.js
+++ src/platform-implementation-js/lib/persistent-queue.js
@@ -1,103 +1,108 @@
+/* @flow */
+//jshint ignore:start
+
 // Queue which tries to persist its data into localStorage so its contents
 // aren't wiped when the page is closed. It handles localStorage not being
 // present or not working (such as if we're running into quota issues).
 // Safe for use in multiple tabs on the same domain at once.
 
-function PersistentQueue(id) {
-  if (typeof id != "string") {
-    throw new Error("PersistentQueue requires a string id");
+class PersistentQueue<T> {
+  _fallbackQueue: T[];
+  _sid: string;
+
+  constructor(id: string) {
+    this._fallbackQueue = [];
+    this._sid = "inboxsdk__persistentqueue_" + id;
   }
-  this._fallbackQueue = [];
-  this._sid = "inboxsdk__persistentqueue_" + id;
+
+  _getSavedQueue(): T[] {
+    var queue = JSON.parse((localStorage.getItem(this._sid): any));
+    if (queue == null) {
+      queue = [];
+    }
+    return queue;
+  }
+
+  _putSavedQueue(queue: T[]) {
+    localStorage.setItem(this._sid, JSON.stringify(queue));
+  }
+
+  _clearSavedQueue() {
+    localStorage.removeItem(this._sid);
+  }
+
+  add(val: T) {
+    var success = false;
+    if (global.localStorage) {
+      try {
+        var queue = this._getSavedQueue();
+        queue.push(val);
+        this._putSavedQueue(queue);
+        success = true;
+      } catch (e) {}
+    }
+    if (!success) {
+      this._fallbackQueue.push(val);
+    }
+  }
+
+  remove(): T {
+    var ret;
+    if (global.localStorage) {
+      try {
+        var queue = this._getSavedQueue();
+        var tmpRet = queue.shift();
+        this._putSavedQueue(queue);
+        ret = tmpRet;
+      } catch (e) {}
+    }
+    // If we didn't get anything from the localStorage queue, or if we
+    // failed to update the queue, then try the fallback instead.
+    if (ret === undefined) {
+      ret = this._fallbackQueue.shift();
+    }
+    return ret;
+  }
+
+  removeAll(): T[] {
+    var ret;
+    if (global.localStorage) {
+      try {
+        var queue = this._getSavedQueue();
+        this._clearSavedQueue();
+        ret = queue;
+      } catch (e) {}
+    }
+    if (ret === undefined) {
+      ret = [];
+    }
+    ret = ret.concat(this._fallbackQueue);
+    this._fallbackQueue = [];
+    return ret;
+  }
+
+  peekAll(): T[] {
+    var ret;
+    if (global.localStorage) {
+      try {
+        ret = this._getSavedQueue();
+      } catch (e) {}
+    }
+    if (ret === undefined) {
+      ret = [];
+    }
+    ret = ret.concat(this._fallbackQueue);
+    return ret;
+  }
+
+  clear() {
+    if (global.localStorage) {
+      try {
+        this._clearSavedQueue();
+      } catch (e) {}
+    }
+    this._fallbackQueue.length = 0;
+  }
 }
 
-PersistentQueue.prototype._getSavedQueue = function() {
-  var queue = JSON.parse(localStorage.getItem(this._sid));
-  if (queue == null) {
-    queue = [];
-  }
-  return queue;
-};
-
-PersistentQueue.prototype._putSavedQueue = function(queue) {
-  localStorage.setItem(this._sid, JSON.stringify(queue));
-};
-
-PersistentQueue.prototype._clearSavedQueue = function() {
-  localStorage.removeItem(this._sid);
-};
-
-PersistentQueue.prototype.add = function(val) {
-  var success = false;
-  if (global.localStorage) {
-    try {
-      var queue = this._getSavedQueue();
-      queue.push(val);
-      this._putSavedQueue(queue);
-      success = true;
-    } catch (e) {}
-  }
-  if (!success) {
-    this._fallbackQueue.push(val);
-  }
-};
-
-PersistentQueue.prototype.remove = function() {
-  var ret;
-  if (global.localStorage) {
-    try {
-      var queue = this._getSavedQueue();
-      var tmpRet = queue.shift();
-      this._putSavedQueue(queue);
-      ret = tmpRet;
-    } catch (e) {}
-  }
-  // If we didn't get anything from the localStorage queue, or if we
-  // failed to update the queue, then try the fallback instead.
-  if (ret === undefined) {
-    ret = this._fallbackQueue.shift();
-  }
-  return ret;
-};
-
-PersistentQueue.prototype.removeAll = function() {
-  var ret;
-  if (global.localStorage) {
-    try {
-      var queue = this._getSavedQueue();
-      this._clearSavedQueue();
-      ret = queue;
-    } catch (e) {}
-  }
-  if (ret === undefined) {
-    ret = [];
-  }
-  ret = ret.concat(this._fallbackQueue);
-  this._fallbackQueue = [];
-  return ret;
-};
-
-PersistentQueue.prototype.peekAll = function() {
-  var ret;
-  if (global.localStorage) {
-    try {
-      ret = this._getSavedQueue();
-    } catch (e) {}
-  }
-  if (ret === undefined) {
-    ret = [];
-  }
-  ret = ret.concat(this._fallbackQueue);
-  return ret;
-};
-
-PersistentQueue.prototype.clear = function() {
-  if (global.localStorage) {
-    try {
-      this._clearSavedQueue();
-    } catch (e) {}
-  }
-  this._fallbackQueue.length = 0;
-};
-
-module.exports = PersistentQueue;
+export default PersistentQueue;
