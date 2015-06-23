05393bbae475472d3f415162705792b18d619f25
Chris Cowan <agentme49@gmail.com>
2015-06-23 11:15:42 -0700

    Add comment describing this workaround.

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-native-route-view.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/show-native-route-view.js
@@ -17,6 +17,10 @@ export default function showNativeRouteView(gmailDriver: any) {
 
 	if (document.body.classList.contains('inboxsdk__custom_view_active')) {
 		document.body.classList.remove('inboxsdk__custom_view_active');
+		// Work around issue where Gmail sometimes screws up its formatting until
+		// the user resizes their screen. Reproduction steps that sometimes work:
+		// use multiple inboxes lab, with multiple inboxes set below the inbox, go
+		// to a pipeline, resize the window slightly, and then go to inbox.
 		fakeWindowResize();
 	}
 }
