ff27f626f6f45677a7efd59b4371c215c4581723
Chris Cowan <agentme49@gmail.com>
2015-06-29 17:26:06 -0700

    Fix mole widget in safari.
    Most of the work was just wrestling with Safari's dev tools. The issue
    itself was just a simple CSS thing.

--- examples/Basic InboxSDK.safariextension/content.js
+++ examples/Basic InboxSDK.safariextension/content.js
@@ -1,6 +1,5 @@
 function main() {
 	InboxSDK.load('1', 'Hello World!', {inboxBeta: true}).then(function(sdk){
-
 		sdk.Router.handleAllRoutes(function(routeView) {
 			console.log('got a routeView', routeView);
 			console.log('routeType', routeView.getRouteType());
@@ -13,7 +12,6 @@ function main() {
 
 		// the SDK has been loaded, now do something with it!
 		sdk.Compose.registerComposeViewHandler(function(composeView){
-
 			// a compose view has come into existence, do something with it!
 			composeView.addButton({
 				title: "My Nifty Button!",
@@ -22,11 +20,34 @@ function main() {
 					event.composeView.insertTextIntoBodyAtCursor('Hello World!');
 				},
 			});
-
 		});
 
 		console.log('user email', sdk.User.getEmailAddress());
 		console.log('all user contacts', sdk.User.getAccountSwitcherContactList());
+
+		var i = 0;
+
+		window.makeMoleWidget = function() {
+			var div = document.createElement('div');
+			div.style.width = '200px';
+			div.style.height = '400px';
+			div.style.backgroundColor = 'red';
+
+			var mole = sdk.Widgets.showMoleView({
+				el: div,
+				title: 'Mole Example '+(++i)
+			});
+
+			div.onclick = function() {
+				mole.close();
+			};
+
+			mole.on('destroy', console.log.bind(console, 'mole destroy'));
+			mole.on('minimize', console.log.bind(console, 'mole minimize'));
+			mole.on('restore', console.log.bind(console, 'mole restore'));
+		};
+
+		makeMoleWidget();
 	});
 }
 
