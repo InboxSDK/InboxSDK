cac3133a52331dd568cfcedf0cd8ed4e7d719368
Chris Cowan <agentme49@gmail.com>
2015-07-15 13:25:51 -0700

    Whoops, fix dispatchCustomEvent. This had broken compose button grouping.

--- src/platform-implementation-js/lib/dom/dispatch-custom-event.js
+++ src/platform-implementation-js/lib/dom/dispatch-custom-event.js
@@ -2,7 +2,7 @@
 //jshint ignore:start
 
 export default function dispatchCustomEvent(element: HTMLElement, eventName: string, data: any) {
-	document.dispatchEvent(new CustomEvent(eventName, {
+	element.dispatchEvent(new CustomEvent(eventName, {
 		bubbles: true,
 		cancelable: false,
 		detail: data
