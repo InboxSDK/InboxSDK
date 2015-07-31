27f3b4750f704f01686e1e29abfdba186bcc5f3d
Aleem Mawani <aleem@streak.com>
2015-07-30 17:39:58 -0700

    document load options in SDK

--- src/inboxsdk-js/inboxsdk.js
+++ src/inboxsdk-js/inboxsdk.js
@@ -16,6 +16,7 @@ var InboxSDK = {
 * @function
 * @param {string} version - The version of the SDK to load, the only acceptable value currently is "1.0".
 * @param {string} appId - The AppId that you registered for on the <a href="/register">AppId Registration page</a>.
+* @param {LoadOptions} opts - Options for the loading of the SDK
 * @return {Promise} A promise which resolves when the SDK is loaded and ready to be used.
 */
 InboxSDK.load = function(version, appId, opts){
@@ -52,3 +53,25 @@ if (['https://mail.google.com', 'https://inbox.google.com'].indexOf(document.loc
 }
 
 module.exports = InboxSDK;
+
+/**
+ * @class
+ * This type is passed into {load} method.
+ */
+var LoadOptions = /** @lends LoadOptions */{
+	/**
+  * The name of your app. This is used by several methods in the SDK.
+  * ^optional
+  * ^default=null
+  * @type {string}
+	 */
+	appName: null,
+
+	/**
+	 * The URL of the icon of your app. Can be HTTPS or a chrome runtime url.
+	 * ^optional
+	 * ^default=null
+	 * @type {string}
+	 */
+	appIconUrl:null
+};
