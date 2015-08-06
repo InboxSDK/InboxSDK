095b338f47d4bb21fd1872fc7a759e16a0dc09cf
Chris Cowan <agentme49@gmail.com>
2015-08-06 12:41:56 -0700

    handle single user

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/user-info.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/user-info.js
@@ -31,12 +31,27 @@ export default class UserInfo {
   }
 
   getAccountSwitcherContactList(): Contact[] {
-    return _.map(
+    var main: Contact[] = _.chain(document.querySelectorAll('[role=banner] div[aria-label] div div a[href^="https://myaccount.google."]'))
+      .take(1)
+      .map((btn: HTMLElement) => {
+        var btnParent: HTMLElement = (btn:any).parentElement;
+        var nameEl = btnParent.children[0];
+        var emailAddressEl = btnParent.children[1];
+        if (!nameEl || !emailAddressEl) return null;
+        return {
+          name: nameEl.textContent,
+          emailAddress: emailAddressEl.textContent
+        };
+      })
+      .filter()
+      .value();
+    var extras: Contact[] = _.map(
       document.querySelectorAll('[role=banner] div[aria-label] div > a[target="_blank"] > img + div'),
       (el: HTMLElement) => ({
         name: el.children[0].textContent,
         emailAddress: el.children[1].textContent.match(/\S+/)[0]
       }));
+    return _.chain([main, extras]).flatten().uniq(x => x.emailAddress.toLowerCase()).value();
   }
 
   waitForAccountSwitcherReady(): Promise<void> {
