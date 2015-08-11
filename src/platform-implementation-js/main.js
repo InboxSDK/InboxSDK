f98e4289cbc9036e3990ec3cc1fdca0500d18e67
Chris Cowan <agentme49@gmail.com>
2015-08-11 11:11:50 -0700

    Fix AMD incompatibility

--- src/platform-implementation-js/main.js
+++ src/platform-implementation-js/main.js
@@ -1,31 +1,57 @@
 if (!global.__InboxSDKImpLoader) {
-  require('safari-fix-map');
-  var RSVP = require('rsvp');
-  var Kefir = require('kefir');
-
-  var onready = new RSVP.Promise((resolve, reject) => {
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-      resolve();
-    } else {
-      resolve(
-        Kefir.merge([
-          Kefir.fromEvents(document, 'DOMContentLoaded'),
-          Kefir.fromEvents(window, 'load')
-        ]).take(1).map(() => null).toPromise(RSVP.Promise)
-      );
+  var oldDefine;
+  try {
+    if (typeof define !== "undefined" && define && define.amd) {
+      // work around amd compatibility issue
+      // https://groups.google.com/forum/#!msg/inboxsdk/U_bq82Exmwc/I3iIinxxCAAJ
+      oldDefine = define;
+      define = null;
     }
-  });
+    require('safari-fix-map');
+    var RSVP = require('rsvp');
+    var Kefir = require('kefir');
 
-  global.__InboxSDKImpLoader = {
-    load: function(version, appId, opts) {
-      if (version !== "0.1") {
-        throw new Error("Unsupported InboxSDK version");
+    var onready = new RSVP.Promise((resolve, reject) => {
+      if (document.readyState === 'complete' || document.readyState === 'interactive') {
+        resolve();
+      } else {
+        resolve(
+          Kefir.merge([
+            Kefir.fromEvents(document, 'DOMContentLoaded'),
+            Kefir.fromEvents(window, 'load')
+          ]).take(1).map(() => null).toPromise(RSVP.Promise)
+        );
       }
+    });
 
-      return onready.then(() => {
-        const {makePlatformImplementation} = require('./platform-implementation');
-        return makePlatformImplementation(appId, opts);
-      });
+    global.__InboxSDKImpLoader = {
+      load: function(version, appId, opts) {
+        if (version !== "0.1") {
+          throw new Error("Unsupported InboxSDK version");
+        }
+
+        return onready.then(() => {
+          var oldDefine;
+          try {
+            if (typeof define !== "undefined" && define && define.amd) {
+              // work around amd compatibility issue
+              // https://groups.google.com/forum/#!msg/inboxsdk/U_bq82Exmwc/I3iIinxxCAAJ
+              oldDefine = define;
+              define = null;
+            }
+            const {makePlatformImplementation} = require('./platform-implementation');
+            return makePlatformImplementation(appId, opts);
+          } finally {
+            if (oldDefine) {
+              define = oldDefine;
+            }
+          }
+        });
+      }
+    };
+  } finally {
+    if (oldDefine) {
+      define = oldDefine;
     }
-  };
+  }
 }
