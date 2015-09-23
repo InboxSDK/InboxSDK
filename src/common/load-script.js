d063c17a1e5f0aa23d9e3210e3765b76378de382
Chris Cowan <agentme49@gmail.com>
2015-09-22 17:36:21 -0700

    If we get a syntax error in loadScript, then try loading the script again up to a few times with cachebusting enabled.

--- src/common/load-script.js
+++ src/common/load-script.js
@@ -4,11 +4,12 @@
 const once = require('lodash/function/once');
 const defer = require('lodash/function/defer');
 import ajax from './ajax';
+import delay from './delay';
 
 declare var chrome: ?Object;
 declare var safari: ?Object;
 
-var isContentScript: () => boolean = once(function() {
+const isContentScript: () => boolean = once(function() {
   if (typeof chrome != 'undefined' && chrome && chrome.extension)
     return true;
   if (typeof safari != 'undefined' && safari && safari.extension)
@@ -17,13 +18,13 @@ var isContentScript: () => boolean = once(function() {
 });
 
 function addScriptToPage(url: string, cors: boolean): Promise<void> {
-  var script = document.createElement('script');
+  const script = document.createElement('script');
   script.type = 'text/javascript';
   if (cors) {
     script.crossOrigin = 'anonymous';
   }
 
-  var promise = new global.Promise(function(resolve, reject) {
+  const promise = new global.Promise(function(resolve, reject) {
     script.addEventListener('error', function(event:any) {
       reject(event.error ||
         new Error(
@@ -48,32 +49,48 @@ export type loadScriptOpts = {
 
 export default function loadScript(url: string, opts?: loadScriptOpts): Promise<void> {
   if (isContentScript()) {
-    return ajax({
-      url: url
-    }).then(function(response) {
-      // jshint evil:true
+    function attempt(num: number, lastErr: ?Error): Promise<void> {
+      if (num > 3) {
+        throw lastErr || new Error("Ran out of loadScript attempts for unknown reason");
+      }
 
-      // Q: Why put the code into a function before executing it instead of
-      //    evaling it immediately?
-      // A: Chrome would execute it before applying any remembered
-      //    breakpoints.
-      // Q: Why not just use `... = new Function(...)`?
-      // A: The sourcemaps would be off by one line in Chrome because of
-      //    https://code.google.com/p/chromium/issues/detail?id=109362
-      // Q: indirectEval?
-      // A: Using the eval value rather than the eval keyword causes the
-      //    code passed to it to be run in the global scope instead of the
-      //    current scope. (Seriously, it's a javascript thing.)
-      var code = response.text;
-      var indirectEval = eval;
-      if (!opts || !opts.nowrap) {
-        code = "(function(){"+code+"\n});";
-      }
-      var program = indirectEval(code+"\n//# sourceURL="+url+"\n");
-      if (!opts || !opts.nowrap) {
-        program();
-      }
-    });
+      return ajax({
+        url, cachebust: num > 0
+      }).then(response => {
+        // jshint evil:true
+
+        // Q: Why put the code into a function before executing it instead of
+        //    evaling it immediately?
+        // A: Chrome would execute it before applying any remembered
+        //    breakpoints.
+        // Q: Why not just use `... = new Function(...)`?
+        // A: The sourcemaps would be off by one line in Chrome because of
+        //    https://code.google.com/p/chromium/issues/detail?id=109362
+        // Q: indirectEval?
+        // A: Using the eval value rather than the eval keyword causes the
+        //    code passed to it to be run in the global scope instead of the
+        //    current scope. (Seriously, it's a javascript thing.)
+        let code = response.text;
+        const indirectEval = eval;
+        if (!opts || !opts.nowrap) {
+          code = "(function(){"+code+"\n});";
+        }
+        let program;
+        try {
+          program = indirectEval(code+"\n//# sourceURL="+url+"\n");
+        } catch(err) {
+          if (err && err.name === 'SyntaxError') {
+            console.warn(`SyntaxError in loading ${url}. Did we not load it fully? Trying again...`, err);
+            return delay(5000).then(() => attempt(num+1, err));
+          }
+          throw err;
+        }
+        if (!opts || !opts.nowrap) {
+          program();
+        }
+      });
+    }
+    return attempt(0, null);
   } else {
     // Try to add script as CORS first so we can get error stack data from it.
     return addScriptToPage(url, true).catch(() => {
