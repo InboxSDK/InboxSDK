6bad5d1d2d8be92f7285c5f87cbe91a74fb422d7
Chris Cowan <agentme49@gmail.com>
2015-11-19 14:33:21 -0800

    Document addToolbarButtonForApp + deprecation notice on setAppToolbarButton

--- src/docs/app-toolbar-button-view.js
+++ src/docs/app-toolbar-button-view.js
@@ -1,7 +1,7 @@
 /**
  * @class
  * Object that represents an app toolbar button that has been already added to the top
- * navigation area of Gmail or Inbox. This class is returned by {Toolbars.setAppToolbarButton()}.
+ * navigation area of Gmail or Inbox. This class is returned by {Toolbars.addToolbarButtonForApp()}.
  */
 var AppToolbarButtonView = /** @lends AppToolbarButtonView */ {
 
