9125ffa9fc42a2ab6895d5953813321aa4156b5f
Chris Cowan <agentme49@gmail.com>
2015-12-09 18:04:37 -0800

    Make getDraftIDForMessageID cache its returned promise, so multiple calls to it at the same time (such as from GmailComposeView and GmailThreadRowView) don't cause multiple requests.

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/get-draft-id-for-message-id.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/get-draft-id-for-message-id.js
@@ -1,26 +1,29 @@
 /* @flow */
 //jshint ignore:start
 
+import _ from 'lodash';
 import ajax from '../../../../common/ajax';
 import {readDraftId} from '../gmail-response-processor';
 import type GmailDriver from '../gmail-driver';
 
-export default async function getDraftIDForMessageID(driver: GmailDriver, messageID: string): Promise<?string> {
-  const response = await ajax({
-    method: 'GET',
-    url: (document.location:any).origin+document.location.pathname,
-    data: {
-      ui: '2',
-      ik: driver.getPageCommunicator().getIkValue(),
-      view: 'cv',
-      th: messageID,
-      prf: '1',
-      nsc: '1',
-      mb: '0',
-      rt: 'j',
-      search: 'drafts'
-    }
-  });
-  const draftID = readDraftId(response.text, messageID);
-  return draftID;
-}
+const getDraftIDForMessageID: (driver: GmailDriver, messageID: string) => Promise<?string> =
+  _.memoize(async function(driver: GmailDriver, messageID: string): Promise<?string> {
+    const response = await ajax({
+      method: 'GET',
+      url: (document.location:any).origin+document.location.pathname,
+      data: {
+        ui: '2',
+        ik: driver.getPageCommunicator().getIkValue(),
+        view: 'cv',
+        th: messageID,
+        prf: '1',
+        nsc: '1',
+        mb: '0',
+        rt: 'j',
+        search: 'drafts'
+      }
+    });
+    return readDraftId(response.text, messageID);
+  }, (driver, messageID) => messageID);
+
+export default getDraftIDForMessageID;
