e4343f806b1e8ab5569772eb78fcdd6933a87385
Aleem Mawani <aleem@streak.com>
2016-05-05 12:31:39 -0700

    add hiring header to sdk

--- src/inboxsdk-js/header.js
+++ src/inboxsdk-js/header.js
@@ -4,4 +4,15 @@
  *
  * The use of InboxSDK is governed by the Terms of Services located at
  * https://www.inboxsdk.com/terms
+
+
+ *  __    __            _     _          _                _                      ___                 _ _ ___
+ * / / /\ \ \__ _ _ __ | |_  | |_ ___   | |__   __ _  ___| | __   ___  _ __     / _ \_ __ ___   __ _(_) / _ \
+ * \ \/  \/ / _` | '_ \| __| | __/ _ \  | '_ \ / _` |/ __| |/ /  / _ \| '_ \   / /_\/ '_ ` _ \ / _` | | \// /
+ *  \  /\  / (_| | | | | |_  | || (_) | | | | | (_| | (__|   <  | (_) | | | | / /_\\| | | | | | (_| | | | \/
+ *   \/  \/ \__,_|_| |_|\__|  \__\___/  |_| |_|\__,_|\___|_|\_\  \___/|_| |_| \____/|_| |_| |_|\__,_|_|_| ()
+ *
+ * Like complex reverse engineering? Want to make Gmail and Inbox a hackable platform?
+ *
+ * Join us at: wwww.streak.com/careers?source=sdk
  */
