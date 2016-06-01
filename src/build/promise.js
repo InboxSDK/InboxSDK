c3e5b44418253268d459adf3b4985b6d1376745c
Chris Cowan <agentme49@gmail.com>
2016-06-01 14:23:39 -0700

    fix replaced helper

--- src/build/promise.js
+++ src/build/promise.js
@@ -1,3 +1,3 @@
 // We only target browsers that natively support Promises, so we replace
 // Babel's Promise helper with a reference to the native promise.
-module.exports = global.Promise;
+module.exports = {default: global.Promise};
