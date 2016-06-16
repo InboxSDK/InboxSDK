02d832d9939bf27aa67c073a8a56ccb6d3406804
Chris Cowan <agentme49@gmail.com>
2016-06-16 14:06:32 -0700

    Log score, and log errors for bad scoring elements too.

--- src/platform-implementation-js/lib/dom/detectionRunner.js
+++ src/platform-implementation-js/lib/dom/detectionRunner.js
@@ -31,16 +31,16 @@ export default function detectionRunner<P: GenericParserResults>(
   return watcher(root)
     .map(({el, removalStream}) => {
       const parsed = parser(el);
+
+      if (parsed.errors.length > 0) {
+        logError(new Error(`detectionRunner errors: ${name}`), {
+          score: parsed.score,
+          errors: parsed.errors,
+          html: censorHTMLtree(el)
+        });
+      }
+
       return {el, removalStream, parsed};
     })
     // .filter(({parsed}) => parsed.score < 0.5)
-    .map(event => {
-      if (event.parsed.errors.length > 0) {
-        logError(new Error(`detectionRunner errors: ${name}`), {
-          errors: event.parsed.errors,
-          html: censorHTMLtree(event.el)
-        });
-      }
-      return event;
-    });
 }
