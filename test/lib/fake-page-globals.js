125610cff124c69af8f975fb8a4254a4f0bb86f3
Chris Cowan <agentme49@gmail.com>
2016-06-15 18:52:04 -0700

    Fix tests.

--- test/lib/fake-page-globals.js
+++ test/lib/fake-page-globals.js
@@ -1,19 +1,54 @@
+/* @flow */
+
 import MockMutationObserver from './mock-mutation-observer';
 import jsdomDoc from './jsdom-doc';
+import once from 'lodash/function/once';
 
-export default function fakePageGlobals() {
-  global.MutationObserver = MockMutationObserver;
-  global.CustomEvent = function CustomEvent(type, options={}) {
-    var event = document.createEvent("CustomEvent");
-    event.initCustomEvent(type, options.bubbles, options.cancelable, options.detail);
-    return event;
-  };
+const docCb = once(() => jsdomDoc(''));
 
-  const doc = jsdomDoc('');
+function addHtmlClasses() {
+  const names = [];
+  const doc = docCb();
   Object.keys(doc.defaultView).filter(x => x.startsWith('HTML')).forEach(name => {
     global[name] = doc.defaultView[name];
+    names.push(name);
   });
-  doc.defaultView.close();
+  return names;
+}
 
-  if (!global.Promise) global.Promise = Promise; // node 0.10 compat
+function CustomEvent(type, options={}) {
+  const event = document.createEvent("CustomEvent");
+  (event: any).initCustomEvent(type, options.bubbles, options.cancelable, options.detail);
+  return event;
+};
+
+export default function fakePageGlobals() {
+  if (global.before) { // We're in mocha
+    // First we set all of the globals to undefined so that mocha doesn't think
+    // they're accidental globals introduced later.
+    let names = ['MutationObserver', 'CustomEvent'];
+    names = names.concat(addHtmlClasses());
+
+    names.forEach(name => {
+      global[name] = undefined;
+    });
+
+    // Now set the globals to their real values.
+    global.before(function() {
+      addHtmlClasses();
+      global.MutationObserver = MockMutationObserver;
+      global.CustomEvent = CustomEvent;
+    });
+
+    // Unset them so that we don't affect the rest of the tests.
+    global.after(function() {
+      names.forEach(name => {
+        global[name] = undefined;
+      });
+    });
+  } else { // not in mocha
+    addHtmlClasses();
+    global.MutationObserver = MockMutationObserver;
+    global.CustomEvent = CustomEvent;
+  }
 }
