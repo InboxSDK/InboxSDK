5bfbde9ce2eef9ae6f1712af7986e6f6ca4d1ec4
Chris Cowan <agentme49@gmail.com>
2016-07-11 17:08:55 -0700

    appToolbarButton progress

--- src/platform-implementation-js/dom-driver/inbox/views/inbox-app-toolbar-button-view.js
+++ src/platform-implementation-js/dom-driver/inbox/views/inbox-app-toolbar-button-view.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+import _ from 'lodash';
 import {defn} from 'ud';
 import Kefir from 'kefir';
 import kefirStopper from 'kefir-stopper';
@@ -7,12 +8,33 @@ import type {ElementWithLifetime} from '../../../lib/dom/make-element-child-stre
 
 class InboxAppToolbarButtonView {
   _stopper: Kefir.Stream&{destroy():void} = kefirStopper();
+  _ready: Kefir.Stream&{destroy():void} = kefirStopper();
 
   constructor(appToolbarLocationStream: Kefir.Stream<ElementWithLifetime>, searchBarStream: Kefir.Stream<ElementWithLifetime>) {
     searchBarStream
       .take(1)
       .takeUntilBy(this._stopper)
       .onValue(({el}) => this._adjustSearchBarMargin(el));
+
+    appToolbarLocationStream
+      .take(1)
+      .takeUntilBy(this._stopper)
+      .onValue(({el}) => this._setupButton(el));
+  }
+
+  _setupButton(appToolbarLocation: HTMLElement) {
+    let appToolbarButtonContainer = _.find(appToolbarLocation.children, el => el.classList.contains('inboxsdk__appButton_container'));
+    if (!appToolbarButtonContainer) {
+      appToolbarButtonContainer = document.createElement('div');
+      appToolbarButtonContainer.className = 'inboxsdk__appButton_container';
+      appToolbarLocation.insertBefore(appToolbarButtonContainer, appToolbarLocation.firstChild);
+    }
+
+    const button = document.createElement('div');
+    button.textContent = ':)';
+    appToolbarButtonContainer.insertBefore(button, appToolbarButtonContainer.firstChild);
+
+    this._ready.destroy();
   }
 
   _adjustSearchBarMargin(searchBar: HTMLElement) {
@@ -58,7 +80,12 @@ class InboxAppToolbarButtonView {
   }
 
   waitForReady(): Promise<this> {
-    return Promise.resolve(this);
+    return this._ready
+      .map(() => this)
+      .merge(this._stopper.flatMap(() =>
+        Kefir.constantError(new Error('InboxAppToolbarButtonView was destroyed early'))
+      ))
+      .take(1).takeErrors(1).toPromise(Promise);
   }
 
   getStopper() {
