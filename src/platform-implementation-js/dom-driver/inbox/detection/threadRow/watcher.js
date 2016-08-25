ae5a944fd324cd7bcba7f662bd8e7c877d016cc3
Chris Cowan <agentme49@gmail.com>
2016-08-24 18:25:30 -0700

    some cleanup

--- src/platform-implementation-js/dom-driver/inbox/detection/threadRow/watcher.js
+++ src/platform-implementation-js/dom-driver/inbox/detection/threadRow/watcher.js
@@ -1,12 +1,8 @@
 /* @flow */
-//jshint ignore:start
 
 import _ from 'lodash';
 import Kefir from 'kefir';
-import udKefir from 'ud-kefir';
-import Logger from '../../../../lib/logger';
 import type {ElementWithLifetime} from '../../../../lib/dom/make-element-child-stream';
-import makeMutationObserverChunkedStream from '../../../../lib/dom/make-mutation-observer-chunked-stream';
 import selectorStream from '../../../../lib/dom/selectorStream';
 
 export default function watcher(root: Document=document): Kefir.Stream<ElementWithLifetime> {
