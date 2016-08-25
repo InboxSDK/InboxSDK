9a957ef2601e958b62b010cbae7fa3249218a93b
Chris Cowan <agentme49@gmail.com>
2016-08-24 18:21:21 -0700

    Make attachmentOverlay watcher use selectorStream.

--- src/platform-implementation-js/dom-driver/inbox/detection/attachmentCard/watcher.js
+++ src/platform-implementation-js/dom-driver/inbox/detection/attachmentCard/watcher.js
@@ -2,15 +2,9 @@
 
 import _ from 'lodash';
 import Kefir from 'kefir';
-import udKefir from 'ud-kefir';
 import Logger from '../../../../lib/logger';
-import streamWaitFor from '../../../../lib/stream-wait-for';
-import delayAsap from '../../../../lib/delay-asap';
-import censorHTMLtree from '../../../../../common/censor-html-tree';
-import makeElementChildStream from '../../../../lib/dom/make-element-child-stream';
 import type ItemWithLifetimePool from '../../../../lib/ItemWithLifetimePool';
 import type {ElementWithLifetime} from '../../../../lib/dom/make-element-child-stream';
-import makeMutationObserverChunkedStream from '../../../../lib/dom/make-mutation-observer-chunked-stream';
 import selectorStream from '../../../../lib/dom/selectorStream';
 import threadRowWatcher from '../threadRow/watcher';
 import messageWatcher from '../message/watcher';
