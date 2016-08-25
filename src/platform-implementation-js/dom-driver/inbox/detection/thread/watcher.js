b6fafed29eefac5f1e8f0a46959ea979bb6f0b7a
Chris Cowan <agentme49@gmail.com>
2016-08-24 18:29:43 -0700

    Make thread watcher use selectorStream.

--- src/platform-implementation-js/dom-driver/inbox/detection/thread/watcher.js
+++ src/platform-implementation-js/dom-driver/inbox/detection/thread/watcher.js
@@ -3,39 +3,18 @@
 
 import _ from 'lodash';
 import Kefir from 'kefir';
-import udKefir from 'ud-kefir';
-import Logger from '../../../../lib/logger';
-import streamWaitFor from '../../../../lib/stream-wait-for';
-import delayAsap from '../../../../lib/delay-asap';
-import censorHTMLtree from '../../../../../common/censor-html-tree';
-import makeElementChildStream from '../../../../lib/dom/make-element-child-stream';
 import type ItemWithLifetimePool from '../../../../lib/ItemWithLifetimePool';
 import type {ElementWithLifetime} from '../../../../lib/dom/make-element-child-stream';
-import makeMutationObserverChunkedStream from '../../../../lib/dom/make-mutation-observer-chunked-stream';
-
+import selectorStream from '../../../../lib/dom/selectorStream';
 import threadRowWatcher from '../threadRow/watcher';
 
 export default function watcher(root: Document=document, threadRowElPool: ?ItemWithLifetimePool<*>=null): Kefir.Stream<ElementWithLifetime> {
   const threadRowElStream = threadRowElPool ? threadRowElPool.items() : threadRowWatcher(root);
 
-  const openedThreads = threadRowElStream
-    .flatMap(({el,removalStream}) => {
-      // Only emit the element when it is opened, and trigger the removalStream
-      // when it is closed.
-      const expanded = makeMutationObserverChunkedStream(el, {
-        attributes: true, attributeFilter:['aria-expanded', 'class']
-      })
-        .toProperty(()=>null)
-        .map(() =>
-          el.getAttribute('aria-expanded') === 'true' || el.classList.contains('scroll-list-item-open')
-        )
-        .takeUntilBy(removalStream)
-        .beforeEnd(()=>false)
-        .skipDuplicates();
-      const opens = expanded.filter(x => x);
-      const closes = expanded.filter(x => !x);
-      return opens.map(_.constant({el, removalStream:closes.changes()}));
-    });
+  const selector = selectorStream([
+    {$watch: '[aria-expanded=true], .scroll-list-item-open'}
+  ]);
 
-  return openedThreads;
+  return threadRowElStream
+    .flatMap(({el,removalStream}) => selector(el).takeUntilBy(removalStream));
 }
