aaeb86757ef83b8d6ea91d78f370c56f58a1c1d1
Chris Cowan <agentme49@gmail.com>
2016-08-25 17:50:51 -0700

    docs++;

--- src/platform-implementation-js/lib/dom/selectorStream.js
+++ src/platform-implementation-js/lib/dom/selectorStream.js
@@ -12,15 +12,51 @@ import type {ElementWithLifetime} from './make-element-child-stream';
 import makeCssSelectorChecker from './css/makeCssSelectorChecker';
 import getRelevantAttributeList from './css/getRelevantAttributeList';
 
-export type SelectorItem = string
-  | {$or: Array<Selector>}
-  | {$log: string}
-  | {$watch: string | {attributeFilter: string[], fn: (el: HTMLElement) => boolean}}
+/*
+selectorStream :: Selector -> HTMLElement -> Kefir.Stream<ElementWithLifetime>
+
+The function takes the Selector array, and returns a function that can apply
+the Selector to an HTMLElement to return a stream of matched elements.
+
+The Selector array is made up of SelectorItems. Initially, only the input
+element is matched. Each SelectorOperator transforms the matched set of
+elements sequentially.
+*/
+export type SelectorOperator =
+  string
+  // The children operator: this will change the matched set to contain only
+  // the direct children of the current matched set, and then filters them
+  // based on a css selector string.
+
   | {$filter: (el: HTMLElement) => boolean}
+  // The $filter operator allows you to specify a function which will be called
+  // on every matched element. If the function returns false, then the element
+  // will be removed from the matched set.
+
   | {$map: (el: HTMLElement) => ?HTMLElement}
+  // The $map operator allows you to specify a function which will be called
+  // on every matched element, and each element in the matched set will be
+  // replaced with the element returned by your function. If your function
+  // returns null, then the element will just be removed from the matched set.
+
+  | {$watch: string | {attributeFilter: string[], fn: (el: HTMLElement) => boolean}}
+  // The $watch operator allows you to specify either a css selector string, or
+  // an attributeFilter list and a function. The currently matched elements
+  // will be removed from the matched set if they don't match the css selector
+  // string or pass the given function. If the element has any attributes
+  // changed, then it will be re-considered and may be added or removed from
+  // the matched set.
+
+  | {$or: Array<Selector>}
+  // The $or operator forks the operator list into multiple lists, and then
+  // re-combines the resulting matched sets.
+
+  | {$log: string}
+  // The $log operator uses `console.log` to log every element in the matched
+  // set to the console with a given string prefix.
 ;
 
-export type Selector = Array<SelectorItem>;
+export type Selector = Array<SelectorOperator>;
 
 const cssProcessor = cssParser();
 
