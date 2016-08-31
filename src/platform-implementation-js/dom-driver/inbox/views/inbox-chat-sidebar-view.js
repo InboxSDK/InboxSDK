3fc67928d0fb87b2a39c4363ab7dbd2e654a76c6
Chris Cowan <agentme49@gmail.com>
2016-08-31 13:00:16 -0700

    buggy sidebar prototype

--- src/platform-implementation-js/dom-driver/inbox/views/inbox-chat-sidebar-view.js
+++ src/platform-implementation-js/dom-driver/inbox/views/inbox-chat-sidebar-view.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import {defn} from 'ud';
+import _ from 'lodash';
 import Kefir from 'kefir';
 import kefirStopper from 'kefir-stopper';
 import type {Parsed} from '../detection/chatSidebar/parser';
@@ -11,10 +12,42 @@ class InboxChatSidebarView {
   _el: HTMLElement;
   _p: Parsed;
   _stopper = kefirStopper();
+  _appSidebar: ?{
+    container: HTMLElement;
+    buttonContainer: HTMLElement;
+    contentArea: HTMLElement;
+  } = null;
 
   constructor(el: HTMLElement, p: Parsed) {
     this._el = el;
     this._p = p;
+  }
+
+  _setupAppSidebar() {
+    if (!this._appSidebar) {
+      const container = document.createElement('div');
+      container.className = 'inboxsdk__sidebar_panel_container';
+      container.innerHTML = `
+        <div class="inboxsdk__sidebar_panel_buttons"></div>
+        <div class="inboxsdk__sidebar_panel_content_area"></div>
+      `;
+
+      const buttonContainer = container.querySelector('.inboxsdk__sidebar_panel_buttons');
+      const contentArea = container.querySelector('.inboxsdk__sidebar_panel_content_area');
+
+      const chatButton = document.createElement('button');
+      chatButton.type = 'button';
+      chatButton.textContent = 'Chat';
+      chatButton.addEventListener('click', () => {
+        this._hideAllPanels();
+        container.classList.remove('inboxsdk__hide_everything_after');
+      });
+      buttonContainer.appendChild(chatButton);
+
+      this._el.insertBefore(container, this._el.firstChild);
+      this._appSidebar = {container, buttonContainer, contentArea};
+    }
+    return this._appSidebar;
   }
 
   getStopper(): Kefir.Observable<null> {
@@ -25,13 +58,45 @@ class InboxChatSidebarView {
     this._stopper.destroy();
   }
 
+  _hideAllPanels() {
+    if (!this._appSidebar) return;
+    const {contentArea} = this._appSidebar;
+    _.forEach(contentArea.children, el => {
+      el.style.display = 'none';
+    });
+  }
+
   addSidebarContentPanel(descriptor: Kefir.Observable<Object>) {
+    const {container, buttonContainer, contentArea} = this._setupAppSidebar();
+
     const view = new InboxSidebarContentPanelView(descriptor);
-    this._el.insertBefore(view.getElement(), this._el.firstChild);
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.textContent = String(buttonContainer.childElementCount);
+    buttonContainer.appendChild(button);
+
+    button.addEventListener('click', () => {
+      container.classList.add('inboxsdk__hide_everything_after');
+      this._hideAllPanels();
+      view.getElement().style.display = '';
+    });
+
+    container.classList.add('inboxsdk__hide_everything_after');
+    this._hideAllPanels();
+    contentArea.appendChild(view.getElement());
+
     this._stopper
       .takeUntilBy(view.getStopper())
       .onValue(() => {
         view.remove();
+      });
+
+    view.getStopper()
+      .onValue(() => {
+        button.remove();
+        this._hideAllPanels();
+        container.classList.remove('inboxsdk__hide_everything_after');
       });
     return view;
   }
