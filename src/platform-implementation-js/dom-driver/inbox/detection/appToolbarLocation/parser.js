dcd947caa8377a6cec0c03636fdb99d0dda66266
Chris Cowan <agentme49@gmail.com>
2016-09-08 17:06:41 -0700

    Find chatSidebarButton.

--- src/platform-implementation-js/dom-driver/inbox/detection/appToolbarLocation/parser.js
+++ src/platform-implementation-js/dom-driver/inbox/detection/appToolbarLocation/parser.js
@@ -4,28 +4,41 @@ import ErrorCollector from '../../../../lib/ErrorCollector';
 import querySelectorOne from '../../../../lib/dom/querySelectorOne';
 
 export default function parser(el: HTMLElement) {
-  const errors: any[] = [];
-  if (el.childElementCount < 2) {
-    errors.push('expected more children');
-  }
-  const rect = el.getBoundingClientRect();
-  if (rect.top > 100) {
-    errors.push('expected element to border top of screen');
-  }
-  if (window.innerWidth - rect.right > 100) {
-    errors.push('expected element to border right side of screen');
-  }
-  if (rect.width > 500) {
-    errors.push('expected element to span only the right of the screen');
-  }
-  if (rect.height > 150) {
-    errors.push('expected element to span only the right of the screen');
-  }
+  const ec = new ErrorCollector('appToolbarLocation');
 
+  ec.run('children check', () => {
+    if (el.childElementCount < 2) {
+      throw new Error('expected more children');
+    }
+  });
+
+  ec.run('position check', () => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top > 100) {
+      throw new Error('expected element to border top of screen');
+    }
+    if (window.innerWidth - rect.right > 100) {
+      throw new Error('expected element to border right side of screen');
+    }
+    if (rect.width > 500) {
+      throw new Error('expected element to span only the right of the screen');
+    }
+    if (rect.height > 150) {
+      throw new Error('expected element to span only the right of the screen');
+    }
+  });
+
+  const chatSidebarButton = ec.run('chatSidebarButton', () =>
+    querySelectorOne(el, 'div[role=button][jsaction*="global.toggle_chat_roster"]')
+  );
+
+  const score = 1 - (ec.errorCount() / ec.runCount());
   return {
-    elements: {},
-    score: errors.length === 0 ? 1 : 0,
-    errors
+    elements: {
+      chatSidebarButton
+    },
+    score,
+    errors: ec.getErrorLogs()
   };
 }
 
