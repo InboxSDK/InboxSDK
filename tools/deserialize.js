2c97e81ee3c569706fcfd31b2b166bb94d4b6278
Chris Cowan <agentme49@gmail.com>
2016-10-04 13:28:07 -0700

    Add serialize and deserialize command-line scripts.

---
+++ tools/deserialize.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env babel-node
+/* @flow */
+
+import concatStream from 'concat-stream';
+import {deserialize} from '../src/platform-implementation-js/dom-driver/gmail/gmail-response-processor';
+
+process.stdin.pipe(concatStream(buffer => {
+  const parsed = deserialize(buffer.toString());
+  console.log(JSON.stringify(parsed, null, 2));
+}));
