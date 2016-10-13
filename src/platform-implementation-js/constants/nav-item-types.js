c727524a681f4d02bb9247a045c88315c50d8285
Chris Cowan <agentme49@gmail.com>
2016-10-13 13:32:41 -0700

    fix doc

--- src/platform-implementation-js/constants/nav-item-types.js
+++ src/platform-implementation-js/constants/nav-item-types.js
@@ -6,11 +6,13 @@
 var navItemTypes = Object.freeze(/** @lends NavItemTypes */ {
 	/**
 	 * standard nav item for navigating
+	 * @type {string}
 	 */
 	'NAVIGATION': 'NAVIGATION',
 
 	/**
 	 * nav item that looks like a link
+	 * @type {string}
 	 */
 	'LINK': 'LINK',
 
