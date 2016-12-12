0e112e4b0feda9279f9bec007ef9a6f2abb58d8e
Chris Cowan <agentme49@gmail.com>
2016-12-12 12:08:06 -0800

    Understand ReactFloatAnchor parenthood.

---
+++ src/platform-implementation-js/lib/dom/outsideClicksAndEscape.test.js
@@ -0,0 +1,60 @@
+/* @flow */
+
+import Kefir from 'kefir';
+import kefirBus from 'kefir-bus';
+
+import outsideClicksAndEscape from './outsideClicksAndEscape';
+
+const stopper = kefirBus();
+afterEach(() => {
+  stopper.emit(null);
+});
+
+const div1 = document.createElement('div');
+div1.innerHTML = '<p>foo <p>bar';
+document.body.appendChild(div1);
+
+const div2 = document.createElement('div');
+div2.innerHTML = '<p>foo2 <p>bar2';
+document.body.appendChild(div2);
+
+const float = document.createElement('div');
+float.innerHTML = '<p>float <p>anchor';
+(float:any).rfaAnchor = div1;
+document.body.appendChild(float);
+
+test('outside click works', () => {
+  const onValue = jest.fn();
+  outsideClicksAndEscape([div1])
+    .takeUntilBy(stopper)
+    .onValue(onValue);
+
+  expect(onValue).toHaveBeenCalledTimes(0);
+  const evt = Object.assign(new MouseEvent('click'), ({__testAllow: true}: any));
+  div2.querySelector('p').dispatchEvent(evt);
+  expect(onValue).toHaveBeenCalledTimes(1);
+});
+
+test('inside click ignored', () => {
+  const onValue = jest.fn();
+  outsideClicksAndEscape([div1, div2])
+    .takeUntilBy(stopper)
+    .onValue(onValue);
+
+  expect(onValue).toHaveBeenCalledTimes(0);
+  const evt = Object.assign(new MouseEvent('click'), ({__testAllow: true}: any));
+  div2.querySelector('p').dispatchEvent(evt);
+  expect(onValue).toHaveBeenCalledTimes(0);
+});
+
+test('float anchor click ignored', () => {
+  const onValue = jest.fn();
+  outsideClicksAndEscape([div1])
+    .takeUntilBy(stopper)
+    .onValue(onValue);
+
+  expect(onValue).toHaveBeenCalledTimes(0);
+  const evt = Object.assign(new MouseEvent('click'), ({__testAllow: true}: any));
+  float.querySelector('p').dispatchEvent(evt);
+  expect(onValue).toHaveBeenCalledTimes(0);
+});
