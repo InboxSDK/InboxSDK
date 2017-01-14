a3fad4be5b9f7b73cc9fc1c8f8072de56bfc8e6a
Chris Cowan <agentme49@gmail.com>
2017-01-13 16:01:48 -0800

    Drop entities dependency from bundle.

--- src/common/html-to-text.js
+++ src/common/html-to-text.js
@@ -1,12 +1,9 @@
 /* @flow */
 
-import entities from 'entities';
-
-// This function does about the same as creating a node, setting it to
-// have the given HTML, and reading its textContent property, but without
-// touching the DOM or introducing an XSS vulnerability.
-// If you already have a DOM node with the HTML, just use node.textContent
-// instead!
+// Quick function for converting HTML with entities into text without
+// introducing an XSS vulnerability.
 export default function htmlToText(html: string): string {
-  return entities.decodeHTML(html.replace(/<[^>]*>?/g, ''));
+  const div = document.createElement('div');
+  div.innerHTML = html.replace(/<[^>]*>?/g, '');
+  return div.textContent;
 }
