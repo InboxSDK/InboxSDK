2e31327d4cac708b28662d53f3177c04255cf645
Chris Cowan <agentme49@gmail.com>
2017-02-17 15:49:49 -0800

    simplify install script

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -3,17 +3,13 @@ set -euo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ..
 
-# When https://github.com/npm/npm/issues/7268 is fixed, remove this if-check
-# and just always call `npm install`.
 if ! ./node_modules/.bin/gulp noop &>/dev/null; then
   echo "Installing dependencies..."
-  rm -rf node_modules
   npm install
 
   if ! ./node_modules/.bin/gulp noop; then
     echo "Failed to install dependencies." >&2
-    echo "need to do:"
-    npm install --dry-run
+    echo "Make sure your npm version ($(npm -v)) is up to date." >&2
     exit 11
   fi
 fi
