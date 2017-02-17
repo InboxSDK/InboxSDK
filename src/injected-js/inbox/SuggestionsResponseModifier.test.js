808ed4d03d8133627d35a6ec0c45e47e8b35f41b
Chris Cowan <agentme49@gmail.com>
2017-02-17 15:27:05 -0800

    Remove ajax interception of suggestion requests

--- src/injected-js/inbox/SuggestionsResponseModifier.test.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-import SuggestionsResponseModifier from './SuggestionsResponseModifier';
-
-test('works', () => {
-  const s = new SuggestionsResponseModifier(JSON.stringify(require('./testdata/r2r-suggest.json')));
-  expect(s.getWarningError()).toBe(undefined);
-});
--- src/injected-js/inbox/setupAjaxInterceptor.js
+++ src/injected-js/inbox/setupAjaxInterceptor.js
@@ -4,7 +4,6 @@ import isEqual from 'lodash/isEqual';
 import difference from 'lodash/difference';
 import * as logger from '../injected-logger';
 import XHRProxyFactory from '../xhr-proxy-factory';
-import SuggestionsResponseModifier from './SuggestionsResponseModifier';
 
 function logErrorExceptEventListeners(err, details) {
   // Don't log the page's own errors
@@ -40,52 +39,5 @@ export default function setupAjaxInterceptor() {
     logger.eventSdkPassive('inboxUseXhrProxy');
   }
 
-  main_wrappers.push({
-    isRelevantTo(connection) {
-      return connection.method === 'POST' && /\/suggest(\?|$)/.test(connection.url);
-    },
-    originalSendBodyLogger(connection, body) {
-      try {
-        const parsed = JSON.parse(body);
-        const rootKeys = Object.keys(parsed);
-
-        const weirdness = [];
-        if (!isEqual(rootKeys, ['1', '63893336'])) {
-          weirdness.push({unexpectedRootKeys: rootKeys});
-        }
-        const qPart = parsed['63893336'];
-        if (!qPart) {
-          weirdness.push('missing query part');
-        } else {
-          const qKeys = Object.keys(qPart);
-          if (!isEqual(qKeys, ['1'])) {
-            weirdness.push({unexpectedQueryKeys: qKeys});
-          }
-        }
-
-        if (weirdness.length) {
-          logger.error(new Error('Suggestion Request Parse Failure'), {
-            type: 'interceptSuggestRequest',
-            weirdness
-          });
-        } else {
-          logger.eventSdkPassive('inboxSuggestRequestParseSuccess');
-        }
-      } catch (err) {
-        logger.error(err, {type: 'interceptSuggestRequest'});
-      }
-    },
-    originalResponseTextLogger(connection, responseText) {
-      if (connection.status !== 200) {
-        return;
-      }
-      const response = new SuggestionsResponseModifier(responseText);
-      const warning = response.getWarningError();
-      if (warning) {
-        logger.error(warning);
-      } else {
-        logger.eventSdkPassive('inboxSuggestResponseParseNoWarnings');
-      }
-    }
-  });
+  // xhr proxy wrappers will go here eventually
 }
