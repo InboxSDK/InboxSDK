b4de7a3d4a1dec9101a3f26b39de588ef9e06d73
erikmunson <erik@ipsumcreative.com>
2017-02-21 17:11:16 -0800

    Convert all these damn tabs to spaces

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/get-presending-stream.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/get-presending-stream.js
@@ -7,56 +7,53 @@ import fromEventTargetCapture from '../../../../lib/from-event-target-capture';
 import type GmailComposeView from '../gmail-compose-view';
 
 const dispatchCancel = (element) => (
-	asap(() => (
-		element.dispatchEvent(new CustomEvent('inboxSDKsendCanceled', {
-		  bubbles: false,
-		  cancelable: false,
-		  detail: null
-	  }))
-	))
+  asap(() => (
+    element.dispatchEvent(new CustomEvent('inboxSDKsendCanceled', {
+      bubbles: false,
+      cancelable: false,
+      detail: null
+    }))
+  ))
 );
 
 export default function(gmailComposeView: GmailComposeView): Kefir.Observable<Object> {
 
-	var element = gmailComposeView.getElement();
-	var sendButtonElement = gmailComposeView.getSendButton();
-	var sendAndArchiveButtonElement = gmailComposeView.getSendAndArchiveButton();
+  var element = gmailComposeView.getElement();
+  var sendButtonElement = gmailComposeView.getSendButton();
+  var sendAndArchiveButtonElement = gmailComposeView.getSendAndArchiveButton();
 
-	var domEventStream = Kefir.merge([
-		fromEventTargetCapture(element, 'keydown')
-			.filter(function(domEvent){
-				return domEvent.ctrlKey || domEvent.metaKey;
-			})
-			.filter(function(domEvent){
-				return domEvent.which === 13 || domEvent.keyCode === 13;
-			}),
+  var domEventStream = Kefir.merge([
+    fromEventTargetCapture(element, 'keydown')
+      .filter(function(domEvent){
+        return domEvent.ctrlKey || domEvent.metaKey;
+      })
+      .filter(function(domEvent){
+        return domEvent.which === 13 || domEvent.keyCode === 13;
+      }),
 
-		fromEventTargetCapture(element, 'keydown')
-			.filter(function(domEvent){
-				return [13, 32].indexOf(domEvent.which) > -1 ||  [13, 32].indexOf(domEvent.keyCode) > -1;
-			})
-			.filter(function(domEvent){
-				return (sendButtonElement && sendButtonElement.contains(domEvent.srcElement)) || (sendAndArchiveButtonElement && sendAndArchiveButtonElement.contains(domEvent.srcElement));
-			}),
+    fromEventTargetCapture(element, 'keydown')
+      .filter(function(domEvent){
+        return [13, 32].indexOf(domEvent.which) > -1 ||  [13, 32].indexOf(domEvent.keyCode) > -1;
+      })
+      .filter(function(domEvent){
+        return (sendButtonElement && sendButtonElement.contains(domEvent.srcElement)) || (sendAndArchiveButtonElement && sendAndArchiveButtonElement.contains(domEvent.srcElement));
+      }),
 
-		fromEventTargetCapture(element, 'click')
-			.filter(function(domEvent){
-				return (sendButtonElement && sendButtonElement.contains(domEvent.srcElement)) || (sendAndArchiveButtonElement && sendAndArchiveButtonElement.contains(domEvent.srcElement));
-			})
-	]);
+    fromEventTargetCapture(element, 'click')
+      .filter(function(domEvent){
+        return (sendButtonElement && sendButtonElement.contains(domEvent.srcElement)) || (sendAndArchiveButtonElement && sendAndArchiveButtonElement.contains(domEvent.srcElement));
+      })
+  ]);
 
-	return domEventStream.map(function(domEvent){
-							return {
-								eventName: 'presending',
-								data: {
-									cancel: function(){
-										domEvent.preventDefault();
-										domEvent.stopPropagation();
-										domEvent.stopImmediatePropagation();
-										dispatchCancel(element)
-									}
-								}
-							};
-						});
-
+  return domEventStream.map((domEvent) => ({
+    eventName: 'presending',
+    data: {
+      cancel() {
+        domEvent.preventDefault();
+        domEvent.stopPropagation();
+        domEvent.stopImmediatePropagation();
+        dispatchCancel(element)
+      }
+    }
+  }));
 }
