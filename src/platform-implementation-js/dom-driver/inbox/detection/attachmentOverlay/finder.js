8648ef3736fd09c04d32ea82d9ce2ed9920453ad
Chris Cowan <agentme49@gmail.com>
2017-03-01 14:47:49 -0800

    Drop lodash use in attachmentOverlay finder.

--- src/platform-implementation-js/dom-driver/inbox/detection/attachmentOverlay/finder.js
+++ src/platform-implementation-js/dom-driver/inbox/detection/attachmentOverlay/finder.js
@@ -1,13 +1,15 @@
 /* @flow */
 
-import _ from 'lodash';
+import t from 'transducers.js';
 import findParent from '../../../../../common/find-parent';
 
+const _t = t.compose(
+  t.filter(iframe => iframe.style.display !== 'none'),
+  t.mapcat(iframe =>
+    Array.from(iframe.contentDocument.querySelectorAll('div[role=dialog]:not([aria-hidden=true])'))
+  )
+);
+
 export default function finder(root: Document=document): Array<HTMLElement> {
-  return _.chain(Array.from(root.querySelectorAll('iframe[frameborder]:not([src])')))
-    .filter(iframe => iframe.style.display !== 'none')
-    .flatMap(iframe =>
-      Array.from(iframe.contentDocument.querySelectorAll('div[role=dialog]:not([aria-hidden=true])'))
-    )
-    .value();
+  return t.toArray(Array.from(root.querySelectorAll('iframe[frameborder]:not([src])')), _t);
 }
