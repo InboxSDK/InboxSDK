0c65250d474cb7bef4701219b9f0e3367c9047fe
erikmunson <erik@ipsumcreative.com>
2017-04-19 14:55:52 -0700

    Try 500ms timeout

--- src/platform-implementation-js/dom-driver/inbox/views/inbox-compose-view.js
+++ src/platform-implementation-js/dom-driver/inbox/views/inbox-compose-view.js
@@ -141,7 +141,7 @@ class InboxComposeView {
           });
           this._driver.getPageCommunicator().notifyEmailSending();
 
-          Kefir.later(700)
+          Kefir.later(500)
             .takeUntilBy(sendCanceledStream)
             .onValue(() => {
               // In cases where Inbox decides to cancel the send client-side,
