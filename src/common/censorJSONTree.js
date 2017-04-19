8c37d6ede20b43a9eabf6adde829982b33b7c091
erikmunson <erik@ipsumcreative.com>
2017-04-19 11:27:48 -0700

    Only log inital 10 chars for ppl with streak

--- src/common/censorJSONTree.js
+++ src/common/censorJSONTree.js
@@ -1,9 +1,19 @@
 /* @flow */
 
+import isStreakAppId from '../platform-implementation-js/lib/is-streak-app-id';
+
+const hasStreakInstalled = () => {
+  const appIds = JSON.parse(
+    global.document &&
+    ((document.documentElement:any):HTMLElement).getAttribute('data-inboxsdk-active-app-ids') || '[]'
+  );
+  return appIds.find(({appId}) => isStreakAppId(appId));
+};
+
 export default function censorJSONTree(object: any): string {
   return JSON.stringify(object, (key, value) => {
     if (typeof value !== 'string' || value.length <= 10) return value;
 
-    return `${value.substring(0, 10)}...`;
+    return hasStreakInstalled() ? `${value.substring(0, 10)}...` : '...';
   });
 }
