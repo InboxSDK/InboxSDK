dcd836ad839c406f3568e537f02b9bdbcb33e577
Chris Cowan <agentme49@gmail.com>
2017-06-15 14:30:12 -0700

    Use async methods in example.

--- examples/thread-test/content.js
+++ examples/thread-test/content.js
@@ -4,26 +4,30 @@ InboxSDK.load(2, 'thread-example').then(sdk => {
 	window._sdk = sdk;
 
 	sdk.Conversations.registerThreadViewHandler(threadView => {
-		window._lastThreadView = threadView;
-		console.log('threadView', threadView.getSubject());
-		console.log(
-			'%s loaded message views, %s all message views',
-			threadView.getMessageViews().length,
-			threadView.getMessageViewsAll().length
-		);
-		threadView.on('destroy', () => {
-			console.log('threadView destroy');
-		});
+		(async () => {
+			window._lastThreadView = threadView;
+			console.log('threadView', await threadView.getThreadIDAsync(), threadView.getSubject());
+			console.log(
+				'%s loaded message views, %s all message views',
+				threadView.getMessageViews().length,
+				threadView.getMessageViewsAll().length
+			);
+			threadView.on('destroy', () => {
+				console.log('threadView destroy');
+			});
+		})();
 	});
 
 	sdk.Conversations.registerMessageViewHandler(messageView => {
-		window._lastMessageView = messageView;
-		console.log('messageView', messageView.getBodyElement().textContent.slice(0,20));
-		console.log('messageView.getViewState()', messageView.getViewState());
-		['viewStateChange', 'destroy'].forEach(name => {
-			messageView.on(name, event => {
-				console.log('messageView', name, messageView.getBodyElement().textContent.slice(0,20), event);
+		(async () => {
+			window._lastMessageView = messageView;
+			console.log('messageView', messageView.getMessageID(), messageView.getBodyElement().textContent.slice(0,20));
+			console.log('messageView.getViewState()', messageView.getViewState());
+			['viewStateChange', 'destroy'].forEach(name => {
+				messageView.on(name, event => {
+					console.log('messageView', name, messageView.getBodyElement().textContent.slice(0,20), event);
+				});
 			});
-		});
+		})();
 	});
 });
