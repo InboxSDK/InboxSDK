7aaeca21f83583f0dfdc05eef75074f986f4224a
Chris Cowan <agentme49@gmail.com>
2017-07-11 14:09:56 -0700

    Make tests more dependable.
    Dispose of PageParserTree instances after each test.

---
+++ test/lib/makePageParserTree.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import type {Driver} from '../../src/platform-implementation-js/driver-interfaces/driver';
+import _makePageParserTree from '../../src/platform-implementation-js/dom-driver/inbox/makePageParserTree';
+import type PageParserTree from 'page-parser-tree';
+
+const activePageParserTrees = [];
+beforeEach(function() {
+  if (activePageParserTrees.length) {
+    throw new Error(`activePageParserTrees.length ${activePageParserTrees.length}`);
+  }
+});
+afterEach(function() {
+  activePageParserTrees.forEach(page => {
+    page.dump();
+  });
+  activePageParserTrees.length = 0;
+});
+
+export default function makePageParserTree(driver: ?Driver, root: Document|HTMLElement): PageParserTree {
+  const page = _makePageParserTree(driver, root);
+  activePageParserTrees.push(page);
+  return page;
+}
