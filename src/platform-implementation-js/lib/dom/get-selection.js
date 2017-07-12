d539eed0d107b446de473b62846ebc13b2c674eb
Chris Cowan <agentme49@gmail.com>
2017-07-12 12:06:32 -0700

    More _ -> transducers

--- src/platform-implementation-js/lib/dom/get-selection.js
+++ src/platform-implementation-js/lib/dom/get-selection.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-var _ = require('lodash');
+import t from 'transducers.js';
 
 function isRangeEmpty(range: Range): boolean {
   return range.startContainer === range.endContainer &&
@@ -12,34 +12,36 @@ function isRangeInElement(range: Range, el: HTMLElement): boolean {
   return el.contains(range.startContainer) || el.contains(range.endContainer);
 }
 
-function isRangeSuitable(range: ?Range, el: HTMLElement): boolean {
-  return !!range && !isRangeEmpty(range) && isRangeInElement(range, el);
+function isRangeSuitable(range: Range, el: HTMLElement): boolean {
+  return !isRangeEmpty(range) && isRangeInElement(range, el);
 }
 
 export function getSelectedTextInElement(element: HTMLElement, lastRange?: ?Range): ?string {
-  var selection = (document:any).getSelection();
-  return _.chain([
+  const selection = (document:any).getSelection();
+  return t.toArray([
       selection && selection.rangeCount ? selection.getRangeAt(0) : null,
       lastRange
-    ])
-    .filter(range => isRangeSuitable(range, element))
-    .map(range => range.toString())
-    .head()
-    .value();
+    ], t.compose(
+      t.keep(),
+      t.filter((range: Range) => isRangeSuitable(range, element)),
+      t.map((range: Range) => range.toString()),
+      t.take(1)
+    ))[0];
 }
 
 export function getSelectedHTMLInElement(element: HTMLElement, lastRange?: ?Range): ?string {
-  var selection = (document:any).getSelection();
-  return _.chain([
+  const selection = (document:any).getSelection();
+  return t.toArray([
       selection && selection.rangeCount ? selection.getRangeAt(0) : null,
       lastRange
-    ])
-    .filter(range => isRangeSuitable(range, element))
-    .map(range => {
-      var div = document.createElement('div');
-      div.appendChild(range.cloneContents());
-      return div.innerHTML;
-    })
-    .head()
-    .value();
+    ], t.compose(
+      t.keep(),
+      t.filter((range: Range) => isRangeSuitable(range, element)),
+      t.map((range: Range) => {
+        const div = document.createElement('div');
+        div.appendChild(range.cloneContents());
+        return div.innerHTML;
+      }),
+      t.take(1)
+    ))[0];
 }
