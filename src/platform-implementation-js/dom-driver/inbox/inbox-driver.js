f60861ecbf1a9637c858768ccc617af97794d65e
erikmunson <erik@ipsumcreative.com>
2017-07-25 14:34:19 -0700

    Record hourly aggregate stats on Inbox thread ID fetches

--- src/platform-implementation-js/dom-driver/inbox/inbox-driver.js
+++ src/platform-implementation-js/dom-driver/inbox/inbox-driver.js
@@ -85,6 +85,8 @@ import type {RouteParams} from '../../namespaces/router';
 import type {Driver} from '../../driver-interfaces/driver';
 import type {PiOpts, EnvData} from '../../platform-implementation';
 
+const THREAD_ID_STAT_SAMPLE_RATE = 0.3;
+
 class InboxDriver {
   _appId: string;
   _logger: Logger;
@@ -113,6 +115,13 @@ class InboxDriver {
   _lastInteractedAttachmentCardViewSet: Bus<any> = kefirBus();
   _appSidebarView: ?InboxAppSidebarView = null;
   _customRouteIDs: Set<string> = new Set();
+  _threadIdStats: {
+    threadRows: {
+      totalCalls: number,
+      callsWithFetch: number,
+      callsWithoutGmailId: number
+    }
+  };
 
   getGmailMessageIdForInboxMessageId: (inboxMessageId: string) => Promise<string>;
   getInboxMessageIdForInboxThreadId: (inboxThreadId: string) => Promise<string>;
@@ -135,6 +144,8 @@ class InboxDriver {
       this._logger.setUserEmailAddress(this.getUserEmailAddress());
     });
 
+    this._setupThreadIdStats();
+
     {
       if (global.localStorage) {
         // We used to not always identify the ids of messages correctly, so we
@@ -155,7 +166,10 @@ class InboxDriver {
     {
       const inboxMessageIdForInboxThreadIdCache = new BiMapCache({
         key: 'inboxsdk__cached_inbox_message_and_inbox_thread_ids',
-        getAfromB: (inboxThreadId: string) => getInboxMessageIdForInboxThreadId(this, inboxThreadId),
+        getAfromB: (inboxThreadId: string) => {
+          this._threadIdStats.threadRows.callsWithFetch += 1;
+          return getInboxMessageIdForInboxThreadId(this, inboxThreadId);
+        },
         getBfromA() {
           throw new Error('should not happen');
         }
@@ -406,6 +420,32 @@ class InboxDriver {
       });
       return view;
     });
+  }
+
+  _setupThreadIdStats() {
+    this._resetThreadIdStats();
+    if (!(Math.random() < THREAD_ID_STAT_SAMPLE_RATE)) return;
+
+    setInterval(() => {
+      if (this._threadIdStats.threadRows.totalCalls === 0) return;
+
+      this._logger.eventSdkPassive('inboxThreadIdStats', this._threadIdStats);
+
+      this._resetThreadIdStats();
+    }, 1000 * 60 * 60);
+  }
+
+  _resetThreadIdStats() {
+    this._threadIdStats = {
+      threadRows: {
+        totalCalls: 0, callsWithFetch: 0, callsWithoutGmailId: 0
+      }
+    };
+  }
+
+  trackThreadRowIdCall(hasGmailId: boolean) {
+    this._threadIdStats.threadRows.totalCalls += 1;
+    if (!hasGmailId) this._threadIdStats.threadRows.callsWithoutGmailId += 1;
   }
 
   destroy() {
