bb19e69d06a01b762d3663f54aae038c50c65fa4
erikmunson <erik@ipsumcreative.com>
2017-07-25 13:03:00 -0700

    Use extractContents()/insertNode() instead of surroundContents()
    Turns out when you use `surroundContents()` to wrap a range, it throws
    if the range splits a non-text node (i.e. half of a <div>'s contents
    are selected). If you use the `extractContents()`/`insertNode()` combo
    it is (basically) functionally equivalent, and will clone the split
    range instead of trying to re-use it.
    
    As far as I can tell, this preserves the same behavior in all
    replacement cases.

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/insert-link-into-body.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/insert-link-into-body.js
@@ -22,7 +22,8 @@ function _insertLinkIntoBody(gmailComposeView, text, href){
 	const range = selection.getRangeAt(0);
 	const link = document.createElement('a');
 	link.href = href;
-	range.surroundContents(link);
+	link.appendChild(range.extractContents());
+	range.insertNode(link);
 	link.textContent = text;
 
 	selection.selectAllChildren(link);
