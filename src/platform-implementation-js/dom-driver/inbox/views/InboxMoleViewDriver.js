81f165062d3bb39f8a534c5f0a442f040e141759
Chris Cowan <agentme49@gmail.com>
2017-08-02 18:26:42 -0700

    Get mole focusing mostly working.
    This has a bug that if you have many compose views, click one to bring
    it to the front, click the mole to bring it to the front, and then click
    the compose view that used to be at the front, it doesn't come back to
    the front.

--- src/platform-implementation-js/dom-driver/inbox/views/InboxMoleViewDriver.js
+++ src/platform-implementation-js/dom-driver/inbox/views/InboxMoleViewDriver.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import {defn} from 'ud';
+import find from 'lodash/find';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Kefir from 'kefir';
@@ -16,7 +17,6 @@ class InboxMoleViewDriver {
   _eventStream = kefirBus();
   _element: HTMLElement;
   _title: string;
-  _zIndex: number = 0;
 
   constructor(options: MoleOptions) {
     (this: MoleViewDriver);
@@ -28,11 +28,11 @@ class InboxMoleViewDriver {
     this._title = this._options.title || '';
 
     this._render();
+    (this._element.firstElementChild:any).style.zIndex = '1';
   }
   _render() {
     ReactDOM.render(
       <MoleViewContents
-        zIndex={this._zIndex}
         title={this._title}
         el={this._options.el}
       />,
@@ -64,10 +64,50 @@ class InboxMoleViewDriver {
       .onValue(() => {
         this._setupWidth();
       });
+    Kefir.fromEvents(this._element, 'click')
+      .takeUntilBy(this._stopper)
+      .onValue(() => {
+        this._bringToFront();
+      });
   }
   _setupWidth() {
     const naturalWidth = querySelector(this._element, '.inboxsdk__mole_view_inner').getBoundingClientRect().width;
     this._element.style.width = naturalWidth+'px';
+  }
+  _bringToFront() {
+    const container = this._element.parentElement;
+    if (!container) throw new Error('mole not in document');
+    const selfIndex = Array.prototype.indexOf.call(container.children, this._element);
+    if (selfIndex < 0) throw new Error('should not happen');
+    const count = container.children.length;
+
+    for (let i = 0; i < selfIndex; i++) {
+      const zIndexedChild = find(container.children[i].children, child => child.style.zIndex);
+      if (!zIndexedChild) continue;
+      zIndexedChild.style.zIndex = String(count);
+    }
+    for (let i = selfIndex; i < count; i++) {
+      const zIndexedChild = find(container.children[i].children, child => child.style.zIndex);
+      if (!zIndexedChild) continue;
+      zIndexedChild.style.zIndex = String(count-i);
+    }
+
+    Kefir.merge(Array.prototype.map.call(container.children, el => {
+      const zIndexedChild = find(el.children, child => child.style.zIndex);
+      if (!zIndexedChild) return Kefir.never();
+      const currentZindex = zIndexedChild.style.zIndex;
+      return makeMutationObserverChunkedStream(zIndexedChild, {attributes: true, attributeFilter: ['style']})
+        .filter(() => zIndexedChild.style.zIndex !== currentZindex);
+    }))
+      .takeUntilBy(this._stopper)
+      .take(1)
+      .onValue(() => {
+        const {firstElementChild} = this._element;
+        if (!firstElementChild) return;
+        if ((firstElementChild:any).style.zIndex === String(count)) {
+          (firstElementChild:any).style.zIndex = '1';
+        }
+      });
   }
   setTitle(title: string) {
     this._title = title;
@@ -99,7 +139,6 @@ class InboxMoleViewDriver {
 export default defn(module, InboxMoleViewDriver);
 
 type MoleViewContentsProps = {
-  zIndex: number;
   title: string;
   el: HTMLElement;
 };
@@ -112,9 +151,6 @@ class MoleViewContents extends React.Component {
     return (
       <div
         className="inboxsdk__mole_view_mid"
-        style={{
-          zIndex: this.props.zIndex
-        }}
       >
         <div className="inboxsdk__mole_view_inner">
           <div className="inboxsdk__mole_view_titlebar">
