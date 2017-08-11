bae3c84e2aabbf95ff9e0f643b6f6e60377f3f70
Chris Cowan <agentme49@gmail.com>
2017-08-10 18:01:23 -0700

    Inbox: Make createLink not support native routes and add test.

--- src/platform-implementation-js/dom-driver/inbox/createLink.js
+++ src/platform-implementation-js/dom-driver/inbox/createLink.js
@@ -1,10 +1,15 @@
 /* @flow */
 
+import includes from 'lodash/includes';
 import type InboxDriver from './inbox-driver';
 import populateRouteID from '../../lib/populateRouteID';
 import type {RouteParams} from '../../namespaces/router';
+import {NATIVE_ROUTE_IDS} from '../../constants/router';
 
 export default function createLink(driver: InboxDriver, routeID: string, params: ?RouteParams): string {
+  if (includes(Object.values(NATIVE_ROUTE_IDS), routeID)) {
+    throw new Error('NativeRouteIDs are not currently supported in Inbox');
+  }
   if (!driver.getCustomRouteIDs().has(routeID)) {
     throw new Error(`Invalid routeID: ${routeID}`);
   }
