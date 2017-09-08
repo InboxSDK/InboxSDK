55e3c8d62f36bed997766fbaa744298c93016d18
Omar Ismail <omar@streak.com>
2017-09-08 11:39:45 -0700

    handle new add-on html structure

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-app-sidebar-view.test.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-app-sidebar-view.test.js
@@ -83,15 +83,16 @@ describe('Without add-ons', function(){
   });
 });
 
-describe('With add-ons', function(){
+describe('With new add-ons html', function(){
   it('construction works', () => {
-    const sidebarContainer = makeAddonSidebar();
+    const sidebarContainer = makeAddonSidebarWithV2HTML();
     const gmailAppSidebarView =
       new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
   });
 
   it('sidebar is added', async () => {
-    const sidebarContainer = makeAddonSidebar();
+    debugger;
+    const sidebarContainer = makeAddonSidebarWithV2HTML();
     const gmailAppSidebarView =
       new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
 
@@ -109,7 +110,55 @@ describe('With add-ons', function(){
   });
 
   it('icons get grouped by appName', async () => {
-    const sidebarContainer = makeAddonSidebar();
+    const sidebarContainer = makeAddonSidebarWithV2HTML();
+    const gmailAppSidebarView =
+      new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
+
+    gmailAppSidebarView.addSidebarContentPanel(Kefir.constant({
+      appName: 'foo', title: 'foo 1', iconUrl: '/bar.png', el: document.createElement('div')
+    }));
+
+    gmailAppSidebarView.addSidebarContentPanel(Kefir.constant({
+      appName: 'foo', title: 'foo 2', iconUrl: '/bar.png', el: document.createElement('div')
+    }));
+
+    gmailAppSidebarView.addSidebarContentPanel(Kefir.constant({
+      appName: 'bar', title: 'bar 2', iconUrl: '/bar.png', el: document.createElement('div')
+    }));
+
+    await delay(0);
+    expect(sidebarContainer.querySelectorAll('.' + idMap('app_sidebar_content_panel')).length).toBe(3);
+    expect(sidebarContainer.querySelectorAll('.' + idMap('sidebar_button_container')).length).toBe(2);
+  });
+});
+
+describe('With old add-ons html', function(){
+  it('construction works', () => {
+    const sidebarContainer = makeAddonSidebarWithV1HTML();
+    const gmailAppSidebarView =
+      new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
+  });
+
+  it('sidebar is added', async () => {
+    const sidebarContainer = makeAddonSidebarWithV1HTML();
+    const gmailAppSidebarView =
+      new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
+
+    const panel = gmailAppSidebarView.addSidebarContentPanel(Kefir.constant({
+      title: 'foo', iconUrl: '/bar.png', el: document.createElement('div')
+    }));
+
+    await delay(0);
+    expect(sidebarContainer.querySelectorAll('.' + idMap('app_sidebar_content_panel')).length).toBe(1);
+    expect(sidebarContainer.querySelectorAll('.' + idMap('sidebar_button_container')).length).toBe(1);
+
+    panel.remove();
+    expect(sidebarContainer.querySelectorAll('.' + idMap('app_sidebar_content_panel')).length).toBe(0);
+    expect(sidebarContainer.querySelectorAll('.' + idMap('sidebar_button_container')).length).toBe(0);
+  });
+
+  it('icons get grouped by appName', async () => {
+    const sidebarContainer = makeAddonSidebarWithV1HTML();
     const gmailAppSidebarView =
       new GmailAppSidebarView(makeDriver(), document.createElement('div'), sidebarContainer);
 
@@ -148,10 +197,20 @@ function makeDriver(appId, opts): any {
   };
 }
 
-function makeAddonSidebar(): HTMLElement {
+function makeAddonSidebarWithV1HTML(): HTMLElement {
   const mainContainer = document.createElement('div');
 
   mainContainer.innerHTML = fs.readFileSync(__dirname+'/../../../../../test/data/gmail-2017-06-22-gmail-addon-sidebar.html', 'utf8');
+
+  (GmailElementGetter: any).getMainContentBodyContainerElement = () => mainContainer.querySelector('.bkK');
+
+  return (mainContainer.querySelector('.bnl'): any);
+}
+
+function makeAddonSidebarWithV2HTML(): HTMLElement {
+  const mainContainer = document.createElement('div');
+
+  mainContainer.innerHTML = fs.readFileSync(__dirname+'/../../../../../test/data/gmail-2017-09-08-gmail-addon-sidebar.html', 'utf8');
 
   (GmailElementGetter: any).getMainContentBodyContainerElement = () => mainContainer.querySelector('.bkK');
 
