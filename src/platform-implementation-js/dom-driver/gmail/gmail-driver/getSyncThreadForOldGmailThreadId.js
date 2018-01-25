7407758ed784a0f93874297f3ce48adb925065eb
Omar Ismail <omar@streak.com>
2018-01-25 14:38:57 -0800

    unit tests for sync response processor

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/getSyncThreadForOldGmailThreadId.js
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/getSyncThreadForOldGmailThreadId.js
@@ -3,6 +3,8 @@
 import {defn} from 'ud';
 import BigNumber from 'bignumber.js';
 
+import getSyncThreadsForSearch from '../../../driver-common/getSyncThreadsForSearch';
+
 import gmailAjax from '../../../driver-common/gmailAjax';
 
 import {extractThreadsFromSearchResponse} from '../gmail-sync-response-processor';
@@ -11,27 +13,6 @@ import type GmailDriver from '../gmail-driver';
 import type {SyncThread} from '../gmail-sync-response-processor';
 
 export default async function getSyncThreadForOldGmailThreadId(driver: GmailDriver, oldGmailThreadId: string): Promise<SyncThread> {
-
-  const accountParamMatch = document.location.pathname.match(/(\/u\/\d+)\//i);
-  const accountParam = accountParamMatch ? accountParamMatch[1] : '';
-
-  const {text} = await gmailAjax({
-    method: 'POST',
-    url: `https://mail.google.com/sync${accountParam}/i/bv`,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Framework-Xsrf-Token': await driver.getPageCommunicator().getXsrfToken(),
-      'X-Gmail-BTAI': await driver.getPageCommunicator().getBtaiHeader(),
-      'X-Google-BTD': '1'
-    },
-    data: JSON.stringify({
-      '1': {
-        '4':"threadid:" + new BigNumber(oldGmailThreadId, 16).toString(10),
-        '6':"itemlist-$ea-8"
-      }
-    })
-  });
-
-  const threadDescriptors = extractThreadsFromSearchResponse(text);
+  const threadDescriptors = await getSyncThreadsForSearch(driver, 'threadid:' + new BigNumber(oldGmailThreadId, 16).toString(10));
   return threadDescriptors[0];
 }
---
+++ src/platform-implementation-js/dom-driver/gmail/gmail-sync-response-processor.test.js
@@ -0,0 +1,74 @@
+/* @flow */
+
+import fs from 'fs';
+import * as GSRP from './gmail-sync-response-processor';
+
+describe('extractThreadsFromSearchResponse', () => {
+  it('regular search request', () => {
+    const data = fs.readFileSync(
+      __dirname+'/../../../../__tests__/gmail-sync-response-processor/input-triggered-search-response-2018-01-26.json',
+      'utf8'
+    );
+
+    const threads = GSRP.extractThreadsFromSearchResponse(data);
+
+    expect(threads.length).toEqual(12);
+    expect(threads[0]).toMatchObject({
+      subject: 'Introducing Comment Editing and @Mentions',
+      snippet: '@Mentions, Conversations, and Editing for Comments Happy New Year! In 2017, users added millions of new Comments to Streak - the most pouplar type of content to add. In 2018, Comments are significantly',
+      syncThreadID: 'thread-f:1589064200257780346',
+      oldGmailThreadID: '160d7d788da7ba7a',
+      extraMetaData: {
+        snippet: 'www.streak.com ) ( https://www.streak.com/careers ) @Mentions, \nConversations, and Editing for Comments Happy New Year',
+        syncMessageIDs: ['msg-f:1590406465099407434']
+      },
+      rawResponse: {"1":{"1":"Introducing Comment Editing and @Mentions","2":"@Mentions, Conversations, and Editing for Comments Happy New Year! In 2017, users added millions of new Comments to Streak - the most pouplar type of content to add. In 2018, Comments are significantly","3":"1516729798134","4":"thread-f:1589064200257780346","5":[{"1":"msg-f:1590406465099407434","2":{"1":1,"2":"support@streak.com","3":"Streak.com"},"7":"1516729798134","10":"@Mentions, Conversations, and Editing for Comments Happy New Year! In 2017, users added millions of new Comments to Streak - the most pouplar type of content to add. In 2018, Comments are significantly","11":["^a","^all","^smartlabel_notification","^u"],"18":"1516729798411","31":"1516729798411","34":2,"44":0,"48":"1590406465099407434"}],"8":"1516731537162","17":0,"18":"1589064200257780346"},"2":"8058"}
+    });
+  });
+
+  it('works for SDK triggered ajax request', () => {
+    const data = fs.readFileSync(
+      __dirname+'/../../../../__tests__/gmail-sync-response-processor/sdk-triggered-search-response-2018-01-26.json',
+      'utf8'
+    );
+
+    const threads = GSRP.extractThreadsFromSearchResponse(data);
+
+    expect(threads.length).toEqual(1);
+    expect(threads[0]).toMatchObject({
+      subject:"mail merge test 22222",
+      snippet:"dsafdsafdsafdsa ᐧ",
+      syncThreadID:"thread-f:1590533107145425147",
+      oldGmailThreadID:"1612b56f16e6c0fb",
+      rawResponse:{"1":{"1":"mail merge test 22222","2":"dsafdsafdsafdsa ᐧ","3":"1516850573718","4":"thread-f:1590533107145425147","5":[{"1":"msg-f:1590533107145425147","2":{"1":1,"2":"inboxsdktestdataonly@gmail.com","3":"Jane Doe"},"7":"1516850573718","10":"dsafdsafdsafdsa ᐧ","11":["^all","^f"],"18":"1516850573678","31":"1516850573678","34":0,"44":3,"48":"1590533107145425147"}],"15":{"1":[{"1":"","2":"oismail+test@gmail.com"}]},"17":0,"18":"1590533107145425147"},"2":"9245"},
+      extraMetaData:{
+        snippet:"",
+        syncMessageIDs:["msg-f:1590533107145425147"]
+      }
+    });
+  });
+
+});
+
+it('replaceThreadsInSearchResponse works', () => {
+
+  const originalSearchResponse = fs.readFileSync(
+    __dirname+'/../../../../__tests__/gmail-sync-response-processor/input-triggered-search-response-2018-01-26.json',
+    'utf8'
+  );
+
+  const replacementThreads = GSRP.extractThreadsFromSearchResponse(
+    fs.readFileSync(
+      __dirname+'/../../../../__tests__/gmail-sync-response-processor/sdk-triggered-search-response-2018-01-26.json',
+      'utf8'
+    )
+  );
+
+  const responesShouldLookLike = fs.readFileSync(
+    __dirname+'/../../../../__tests__/gmail-sync-response-processor/replaced-threads-response-example-data-2018-01-26.json',
+    'utf8'
+  ).replace('\n', '');
+
+  const newResponse = GSRP.replaceThreadsInSearchResponse(originalSearchResponse, replacementThreads, {start: 0});
+  expect(newResponse).toBe(responesShouldLookLike);
+});
