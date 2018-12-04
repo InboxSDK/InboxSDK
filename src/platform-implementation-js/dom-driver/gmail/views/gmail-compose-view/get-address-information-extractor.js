33ad37bfc62fe1b625813ca9d8d099b84b2343d1
Chris Cowan <agentme49@gmail.com>
2018-12-03 16:30:04 -0800

    run prettier to fix whitespace

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/get-address-information-extractor.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view/get-address-information-extractor.js
@@ -12,39 +12,41 @@ export default function getAddressInformationExtractor(
   addressType: string,
   composeView: GmailComposeView
 ): (node: HTMLElement) => ?Contact {
-	return function(node: HTMLElement): ?Contact {
-		const contactNode = node.querySelector(`input[name='${addressType}']`);
+  return function(node: HTMLElement): ?Contact {
+    const contactNode = node.querySelector(`input[name='${addressType}']`);
 
-		var emailAddress = null;
-		var name = null;
+    var emailAddress = null;
+    var name = null;
 
-		if (contactNode) {
-			var contactInfoString = (contactNode: any).value;
+    if (contactNode) {
+      var contactInfoString = (contactNode: any).value;
 
-			try {
-				return extractContactFromEmailContactString(contactInfoString);
-			} catch(err) {
-				// The user might have typed in a partial address. We can just ignore that.
-				return null;
-			}
-		} else {
-			Logger.error(new Error(`contactNode can't be found`), {
+      try {
+        return extractContactFromEmailContactString(contactInfoString);
+      } catch (err) {
+        // The user might have typed in a partial address. We can just ignore that.
+        return null;
+      }
+    } else {
+      Logger.error(new Error(`contactNode can't be found`), {
         addressType,
         composeViewHtml: censorHTMLstring(composeView.getElement().outerHTML)
-			});
+      });
 
       // Maybe the issue is Gmail is doing lazy loading? Let's test for that.
       Kefir.later(5000)
         .takeUntilBy(composeView.getStopper())
         .onValue(() => {
-          const contactNode = node.querySelector(`input[name='${addressType}']`);
+          const contactNode = node.querySelector(
+            `input[name='${addressType}']`
+          );
           Logger.error(new Error(`contactNode re-check status`), {
             addressType,
             contactNodePresent: contactNode != null
-    			});
+          });
         });
 
-			return null;
-		}
-	};
+      return null;
+    }
+  };
 }
