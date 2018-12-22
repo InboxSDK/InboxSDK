13443bab458ea005d4d4ca772f25efd06fd88d8c
Chris Cowan <agentme49@gmail.com>
2018-12-21 17:41:23 -0800

    Fix sent event methods to not use `this` on event object.

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view.js
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-compose-view.js
@@ -159,9 +159,6 @@ class GmailComposeView {
               return { eventName: 'sending' };
             }
             case 'emailSent': {
-              // `data` will get filled in with getMessageID + getThreadID props from
-              // intercept stream as soon as they are made available in the sync API response.
-
               let syncThreadID = event.threadID;
               let syncMessageID = event.messageID;
 
@@ -171,22 +168,26 @@ class GmailComposeView {
               return {
                 eventName: 'sent',
                 data: {
-                  async getThreadID() {
-                    if (!this._threadID) {
-                      this._threadID = await driver.getOldGmailThreadIdFromSyncThreadId(
+                  getThreadID: once(
+                    async (): Promise<string> => {
+                      if (event.oldThreadID) {
+                        return event.oldThreadID;
+                      }
+                      return await driver.getOldGmailThreadIdFromSyncThreadId(
                         syncThreadID
                       );
                     }
-                    return this._threadID;
-                  },
-                  async getMessageID() {
-                    if (!this._messageId) {
-                      this._messageId = driver.getGmailMessageIdForSyncMessageId(
+                  ),
+                  getMessageID: once(
+                    async (): Promise<string> => {
+                      if (event.oldMessageID) {
+                        return event.oldMessageID;
+                      }
+                      return await driver.getGmailMessageIdForSyncMessageId(
                         syncMessageID
                       );
                     }
-                    return this._messageId;
-                  }
+                  )
                 }
               };
             }
