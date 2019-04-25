6986a0fa961b9f78c68f96eacde7df7303b1a1ba
Chris Cowan <agentme49@gmail.com>
2019-04-25 16:45:06 -0700

    Persist user data directory in CI

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,15 +1,20 @@
 'use strict';
 
 const path = require('path');
+const os = require('os');
 
 const pathToExtension = path.resolve(__dirname, 'examples/browser-test');
+
+const userDataDir = path.join(os.tmpdir(), 'jest_puppeteer_userDataDir');
 
 module.exports = {
   launch: {
     headless: false,
     args: [
       `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`
-    ]
+      `--load-extension=${pathToExtension}`,
+      '--window-size=1024,700'
+    ],
+    userDataDir
   }
 };
