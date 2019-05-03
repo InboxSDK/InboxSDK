8842978c35b048dd0208e00d299185c35b45aaff
Chris Cowan <agentme49@gmail.com>
2019-05-02 17:40:45 -0700

    Copy https://github.com/StreakYC/MailFoo/pull/6424 over

--- test/chrome/jest.setupTestFrameworkScriptFile.js
+++ test/chrome/jest.setupTestFrameworkScriptFile.js
@@ -2,3 +2,23 @@
 
 // TODO this should be unnecessary, but https://github.com/smooth-code/jest-puppeteer/issues/106
 jest.setTimeout(4 * 24 * 60 * 60 * 1000);
+
+if (process.env.DISABLE_FAIL_PAUSE !== 'true' && process.env.CI !== 'true') {
+  const originalIt = global.jasmine.getEnv().it;
+
+  global.jasmine.getEnv().it = function(name, fn, timeout) {
+    const newFn = async function() {
+      try {
+        return await fn.apply(this, arguments);
+      } catch (err) {
+        console.error(
+          'Pausing on error. Disable this by setting env variable DISABLE_FAIL_PAUSE=true'
+        );
+        console.log(err);
+        await jestPuppeteer.debug();
+        throw err;
+      }
+    };
+    return originalIt.call(this, name, newFn, timeout);
+  };
+}
