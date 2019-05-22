efcd884cfe8005f8e426ee512ddecb07ba494d33
Chris Cowan <agentme49@gmail.com>
2019-05-22 16:52:08 -0700

    +attemptWithRetries

---
+++ src/platform-implementation-js/lib/attemptWithRetries.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+export default async function attemptWithRetries<T>(
+  fn: () => Promise<T>,
+  attemptCount: number,
+  errorsToRetry: (error: Error) => boolean
+): Promise<T> {
+  if (attemptCount < 1) {
+    throw new Error('Attempt count must be positive');
+  }
+  try {
+    return await fn();
+  } catch (err) {
+    if (attemptCount > 1 && errorsToRetry(err)) {
+      return attemptWithRetries(fn, attemptCount - 1, errorsToRetry);
+    } else {
+      throw err;
+    }
+  }
+}
