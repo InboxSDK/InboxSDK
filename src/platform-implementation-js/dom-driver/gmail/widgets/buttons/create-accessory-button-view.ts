260be49ee19e749c6424ea6ae04b8f18462e4415
Chris Cowan <agentme49@gmail.com>
2019-06-19 20:11:43 -0700

    fix button view destroy

--- src/platform-implementation-js/dom-driver/gmail/widgets/buttons/create-accessory-button-view.ts
+++ src/platform-implementation-js/dom-driver/gmail/widgets/buttons/create-accessory-button-view.ts
@@ -1,10 +1,11 @@
 import * as Kefir from 'kefir';
+import kefirBus from 'kefir-bus';
 import kefirStopper from 'kefir-stopper';
 import { ButtonViewI } from '../../../../widgets/buttons/basic-button-view-controller';
 
 export default class CreateAccessoryButtonView implements ButtonViewI {
   private _element: HTMLElement = document.createElement('div');
-  private _eventStream = Kefir.pool<any, any>();
+  private _eventStream = kefirBus<any, any>();
   private _stopper = kefirStopper();
 
   public constructor() {
@@ -19,7 +20,7 @@ export default class CreateAccessoryButtonView implements ButtonViewI {
   }
 
   public destroy() {
-    (this._eventStream as any).end();
+    this._eventStream.end();
     this._stopper.destroy();
     this._element.remove();
   }
