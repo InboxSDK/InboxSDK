114be5289fc3e9721b840605192b51756857987b
Chris Cowan <agentme49@gmail.com>
2019-07-22 16:54:00 -0700

    more tsification

--- src/platform-implementation-js/views/compose-button-view.ts
+++ src/platform-implementation-js/views/compose-button-view.ts
@@ -1,34 +1,38 @@
-/* @flow */
-
 import EventEmitter from '../lib/safe-event-emitter';
 import get from '../../common/get-or-fail';
-import type { ComposeViewDriver } from '../driver-interfaces/compose-view-driver';
-import type { Driver } from '../driver-interfaces/driver';
+import { ComposeViewDriver } from '../driver-interfaces/compose-view-driver';
+import { Driver } from '../driver-interfaces/driver';
 
-export type TooltipDescriptor = {
-  el?: ?HTMLElement,
-  title?: ?string,
-  subtitle?: ?string,
-  imageUrl?: ?string,
-  button?: ?{ onClick?: Function, title: string }
-};
+export interface TooltipDescriptor {
+  el?: null | HTMLElement;
+  title?: null | string;
+  subtitle?: null | string;
+  imageUrl?: null | string;
+  button?: null | { onClick?: Function; title: string };
+}
 
-type Options = {
-  buttonDescriptor: Object,
-  buttonViewController: Object
-};
-const memberMap = new WeakMap();
+interface Options {
+  buttonDescriptor: any;
+  buttonViewController: any;
+}
+const memberMap = new WeakMap<
+  ComposeButtonView,
+  {
+    driver: Driver;
+    optionsPromise: Promise<Options | null | undefined>;
+    composeViewDriver: ComposeViewDriver;
+  }
+>();
 
 export default class ComposeButtonView extends EventEmitter {
-  destroyed: boolean;
+  public destroyed: boolean = false;
 
-  constructor(
-    optionsPromise: Promise<?Options>,
+  public constructor(
+    optionsPromise: Promise<Options | null | undefined>,
     composeViewDriver: ComposeViewDriver,
     driver: Driver
   ) {
     super();
-    this.destroyed = false;
     const members = { optionsPromise, composeViewDriver, driver };
     memberMap.set(this, members);
 
@@ -44,7 +48,7 @@ export default class ComposeButtonView extends EventEmitter {
     });
   }
 
-  showTooltip(tooltipDescriptor: TooltipDescriptor) {
+  public showTooltip(tooltipDescriptor: TooltipDescriptor) {
     const members = get(memberMap, this);
     members.driver
       .getLogger()
@@ -61,7 +65,7 @@ export default class ComposeButtonView extends EventEmitter {
     });
   }
 
-  closeTooltip() {
+  public closeTooltip() {
     const members = get(memberMap, this);
     members.optionsPromise.then(options => {
       if (!options) return;
