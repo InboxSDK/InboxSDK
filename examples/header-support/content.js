ab22ec82f08afa5bbcfc6316caefe7ec784f6900
Meichen Zhou <jfmaggie@gmail.com>
2020-03-09 16:05:44 -0700

    Try a onclick on the element

--- examples/header-support/content.js
+++ examples/header-support/content.js
@@ -2,7 +2,10 @@ InboxSDK.load(2, 'header support button').then(function(sdk){
   window._sdk = sdk;
 
   const testElement = document.createElement('div')
-  testElement.innerHTML = 'test'
+  testElement.innerHTML = 'Streak help <img class="inboxsdk__button_iconImg" src="https://assets.streak.com/clientjs-release-builds/assets/logo.4cc0970dd9b42803f15cf37c8608a3bb.png">'
+  testElement.onclick = () => {
+    window.open('https://streak.com')
+  }
 
   sdk.Global.addSupportItem({
     element: testElement,
