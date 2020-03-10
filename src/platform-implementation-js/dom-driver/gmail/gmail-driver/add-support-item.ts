4a763526a285d22e4033b2f6564e3ed321b944cf
Meichen Zhou <jfmaggie@gmail.com>
2020-03-10 14:24:07 -0700

    Fix types

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/add-support-item.ts
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/add-support-item.ts
@@ -1,11 +1,11 @@
-import GmailSupportItemView from '../views/gmail-support-item-view';
+import GmailSupportItemView, {
+  SupportItemDescriptor
+} from '../views/gmail-support-item-view';
 import GmailDriver from '../gmail-driver';
 
 export default function addSupportItem(
   driver: GmailDriver,
-  supportItemDescriptor: {
-    element: HTMLElement;
-  }
+  supportItemDescriptor: SupportItemDescriptor
 ): GmailSupportItemView {
   return new GmailSupportItemView(driver, supportItemDescriptor);
 }
