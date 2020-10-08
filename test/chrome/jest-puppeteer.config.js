872a299c902925baf700c5cd3f747fbf355ed07b
Meichen Zhou <jfmaggie@gmail.com>
2020-10-08 16:14:09 -0700

    Remove plug

--- test/chrome/jest-puppeteer.config.js
+++ test/chrome/jest-puppeteer.config.js
@@ -7,14 +7,6 @@ const pathToExtension = path.resolve(__dirname, '../../examples/browser-test');
 
 const userDataDir = path.join(os.tmpdir(), 'jest_puppeteer_userDataDir');
 
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
-
-// Work around https://github.com/smooth-code/jest-puppeteer/issues/260
-require.cache[require.resolve('puppeteer')] =
-  require.cache[require.resolve('puppeteer-extra')];
-
 module.exports = {
   launch: {
     headless: false
