db681619b64c7c1773443a62efd322866096e3f3
Johnny Nguyen <hellojohnnguyen@gmail.com>
2020-11-20 09:29:11 -0800

    comment

--- src/platform-implementation-js/dom-driver/gmail/widgets/buttons/more-parent-accessory-button-view.ts
+++ src/platform-implementation-js/dom-driver/gmail/widgets/buttons/more-parent-accessory-button-view.ts
@@ -4,6 +4,8 @@ export default class MoreParentAccessoryButtonView extends CreateParentAccessory
   public constructor() {
     super();
 
+    // New Gmail left nav does not have a "more" parent level accessory icon (only "create").
+    // So we extend the CreateParentAccessoryButtonView and manually change the background-image.
     this.getElement().style.backgroundImage =
       'url(https://www.gstatic.com/images/icons/material/system_gm/1x/more_vert_black_20dp.png)';
   }
