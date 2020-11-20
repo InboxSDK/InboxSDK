0a8b7ff2909935da1ad9a7bf7e7189429004a942
Johnny Nguyen <hellojohnnguyen@gmail.com>
2020-11-19 21:28:14 -0800

    create parent accessory

---
+++ src/platform-implementation-js/dom-driver/gmail/widgets/buttons/create-parent-accessory-button-view.ts
@@ -0,0 +1,77 @@
+import * as Kefir from 'kefir';
+import kefirBus from 'kefir-bus';
+import kefirStopper from 'kefir-stopper';
+import { ButtonViewI } from '../../../../widgets/buttons/basic-button-view-controller';
+
+export default class CreateParentAccessoryButtonView implements ButtonViewI {
+  private _element: HTMLElement = document.createElement('div');
+  private _eventStream = kefirBus<any, any>();
+  private _stopper = kefirStopper();
+
+  public constructor() {
+    this._setupElement();
+    this._setupEventStream();
+  }
+
+  public activate() {
+    const innerButtonElement = this._element.firstElementChild;
+    if (innerButtonElement) {
+      innerButtonElement.classList.add('aj1');
+    }
+  }
+
+  public deactivate() {
+    const innerButtonElement = this._element.firstElementChild;
+    if (innerButtonElement) {
+      innerButtonElement.classList.remove('aj1');
+    }
+  }
+
+  public destroy() {
+    this._element.remove();
+    this._eventStream.end();
+    this._stopper.destroy();
+  }
+
+  public getElement(): HTMLElement {
+    return this._element;
+  }
+
+  public getEventStream(): Kefir.Observable<any, any> {
+    return this._eventStream;
+  }
+
+  public setEnabled() {
+    throw new Error('not implemented');
+  }
+
+  public update() {
+    // noop
+  }
+
+  private _setupElement() {
+    this._element.setAttribute(
+      'class',
+      'Yh inboxsdk__navItem_parent_accessory_button'
+    );
+    this._element.setAttribute('role', 'button');
+    this._element.setAttribute('tabindex', '0');
+    this._element.setAttribute('type', 'button');
+  }
+
+  private _setupEventStream() {
+    const clickEventStream = Kefir.fromEvents<any, never>(
+      this._element,
+      'click'
+    );
+
+    clickEventStream.onValue(event => {
+      event.stopPropagation();
+      event.preventDefault();
+    });
+
+    this._eventStream.plug(
+      clickEventStream.map(event => ({ eventName: 'click', domEvent: event }))
+    );
+  }
+}
