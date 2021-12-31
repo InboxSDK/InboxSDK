d27ece61de7f5d8853fc035cf3f721dcb34b6e53
Chris Cowan <agentme49@gmail.com>
2021-12-30 16:35:49 -0800

    MV2 support

--- packages/core/background.js
+++ packages/core/background.js
@@ -1,9 +1,16 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'inboxsdk__injectPageWorld' && sender.tab) {
-    chrome.scripting.executeScript({
-      target: { tabId: sender.tab.id },
-      world: 'MAIN',
-      files: ['pageWorld.js']
-    });
+    if (chrome.scripting) {
+      // MV3
+      chrome.scripting.executeScript({
+        target: { tabId: sender.tab.id },
+        world: 'MAIN',
+        files: ['pageWorld.js']
+      });
+      sendResponse(true);
+    } else {
+      // MV2 fallback. Tell content script it needs to figure things out.
+      sendResponse(false);
+    }
   }
 });
