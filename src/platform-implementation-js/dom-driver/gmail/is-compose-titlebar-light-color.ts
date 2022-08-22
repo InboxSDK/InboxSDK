ea8b03b03d911e2d57dcfc7e87485bf25c52fa09
Timothy Rhodes <timothy.rhodes@commit.dev>
2022-08-22 14:45:05 -0700

    tidy

--- src/platform-implementation-js/dom-driver/gmail/is-compose-titlebar-light-color.ts
+++ src/platform-implementation-js/dom-driver/gmail/is-compose-titlebar-light-color.ts
@@ -1,26 +1,20 @@
-const WIDE_ICONS = '24px';
-
 function isGmailRuleForTitlebarWideIcons(): boolean {
-  const styleSheetsWithHlRuleWithWideIcons = [...document.styleSheets].find(
-    (s) => {
-      try {
-        if (s.cssRules) {
-          return [...s.cssRules].find(
-            (rule) =>
-              rule instanceof CSSStyleRule &&
-              rule.selectorText === '.Hl' &&
-              rule.cssText.indexOf(WIDE_ICONS) > 0
-          );
-        }
-      } catch (e) {
-        // Some stylesheets are crossorigin and throw if we try to access
-        // their rules. Don't worry about them.
+  return !![...document.styleSheets].find((s) => {
+    try {
+      if (s.cssRules) {
+        return [...s.cssRules].find(
+          (rule) =>
+            rule instanceof CSSStyleRule &&
+            rule.selectorText === '.Hl' &&
+            rule.cssText.indexOf('24px') > 0
+        );
       }
-      return false;
+    } catch (e) {
+      // Some stylesheets are crossorigin and throw if we try to access
+      // their rules. Don't worry about them.
     }
-  );
-
-  return typeof styleSheetsWithHlRuleWithWideIcons !== 'undefined';
+    return false;
+  });
 }
 
 export default function isComposeTitleBarLightColor(): boolean {
