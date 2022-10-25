c62604d915c13bdbf9d4a288d649d260cdae780f
Borys Korobeinikov <borys@streak.com>
2022-10-25 14:32:37 -0700

    support contacts parsing in old req/res format

--- src/injected-js/gmail/sync-compose-request-processor.test.js
+++ src/injected-js/gmail/sync-compose-request-processor.test.js
@@ -17,6 +17,9 @@ it('handles first draft save', () => {
     type: 'FIRST_DRAFT_SAVE',
     draftID: 'r-5205311597211900881',
     subject: 'new draft',
+    to: null,
+    cc: null,
+    bcc: null,
   });
 });
 
@@ -33,6 +36,37 @@ it('handles regular draft save', () => {
     type: 'DRAFT_SAVE',
     draftID: 'r-5205311597211900881',
     subject: 'new draft',
+    to: null,
+    cc: null,
+    bcc: null,
+  });
+});
+
+it('handles regular draft save with recipients', () => {
+  const request = fs.readFileSync(
+    __dirname +
+      '/../../../__tests__/sync-compose-request-processor/draft-save-request-2018-01-25-with-recipients.json',
+    'utf8'
+  );
+
+  const composeRequest = SCRP.getDetailsOfComposeRequest(JSON.parse(request));
+  expect(composeRequest).toMatchObject({
+    body: '<div dir="ltr">new new draft 2</div>',
+    type: 'DRAFT_SAVE',
+    draftID: 'r-5205311597211900881',
+    subject: 'new draft',
+    to: [
+      { emailAddress: 'test+to@gmail.com', name: null },
+      { emailAddress: 'test+to2@gmail.com', name: 'test to2' },
+    ],
+    cc: [
+      { emailAddress: 'test+cc@gmail.com', name: null },
+      { emailAddress: 'test+cc2@gmail.com', name: 'test cc2' },
+    ],
+    bcc: [
+      { emailAddress: 'test+bcc@gmail.com', name: null },
+      { emailAddress: 'test+bcc2@gmail.com', name: 'test bcc2' },
+    ],
   });
 });
 
@@ -49,5 +83,8 @@ it('handles sending', () => {
     type: 'SEND',
     draftID: 'r-5205311597211900881',
     subject: 'new draft',
+    to: [{ emailAddress: 'test+to@test.com', name: null }],
+    cc: null,
+    bcc: null,
   });
 });
