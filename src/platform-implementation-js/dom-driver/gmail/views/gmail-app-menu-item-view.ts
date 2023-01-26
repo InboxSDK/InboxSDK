0c9251fdb509db6bf77e92aa6dc7a90527437268
Zach Wegrzyniak <zach@streak.com>
2023-01-26 15:56:06 -0500

    checkpoint

--- src/platform-implementation-js/dom-driver/gmail/views/gmail-app-menu-item-view.ts
+++ src/platform-implementation-js/dom-driver/gmail/views/gmail-app-menu-item-view.ts
@@ -24,9 +24,11 @@ export class GmailAppMenuItemView {
     // TODO handle other stuff
   }
 
+  #ICON_ELEMENT_SELECTOR = '.V6.CL';
+
   #setupElement() {
     const element = document.createElement('div');
-    element.classList.add('Xa');
+    element.classList.add('Xa', 'inboxsdk__appMenuItem');
     element.innerHTML = `
       <div class="V6 CL" aria-label="" role="link" tabindex="-1">
         <span class="XS">
@@ -42,8 +44,33 @@ export class GmailAppMenuItemView {
     if (!element || !this.#menuItemDescriptor) {
       return;
     }
-    const { name } = this.#menuItemDescriptor;
+    const { iconElement: icon, name } = this.#menuItemDescriptor;
     const headingElement = querySelector(element, '.apW');
     headingElement.textContent = name;
+
+    const existingIconContainerEl = querySelector(
+      element,
+      this.#ICON_ELEMENT_SELECTOR
+    );
+    // check for existing element
+    const existingIconEl = existingIconContainerEl.querySelector<HTMLElement>(
+      '[data-inboxsdk__icon="true"]'
+    );
+
+    if (existingIconEl) {
+      existingIconEl.innerHTML = icon;
+      return;
+    }
+
+    // create new element
+    const newElement = document.createElement('div');
+    newElement.innerHTML = icon;
+    newElement.dataset.inboxsdk__icon = 'true';
+
+    // https://stackoverflow.com/a/2007473/1924257
+    existingIconContainerEl.insertBefore(
+      newElement,
+      existingIconContainerEl.firstChild
+    );
   }
 }
