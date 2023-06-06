1a62377056f5cfdf4d3313637552a61be2a950ca
Zach Wegrzyniak <zach@streak.com>
2023-06-06 09:41:54 -0400

    Update src/inboxsdk-js/inboxsdk-NONREMOTE-INTEGRATED-PAGEWORLD.ts

--- src/inboxsdk-js/inboxsdk-NONREMOTE-INTEGRATED-PAGEWORLD.ts
+++ src/inboxsdk-js/inboxsdk-NONREMOTE-INTEGRATED-PAGEWORLD.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+
 import { injectScriptEmbedded } from '../platform-implementation-js/lib/inject-script-EMBEDDED';
 
 require('../platform-implementation-js/lib/inject-script').setInjectScriptImplementation(
