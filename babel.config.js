f9b95c524e9dfba9fa09fcc32244e2698865c2fd
Zach Wegrzyniak <zach@streak.com>
2023-07-03 09:17:09 -0400

    chore: reenable class field transforms (#938)
    terser@5.17.6 includes "fixes to mozilla AST's input and output, for class properties, private properties and static blocks"
    
    https://github.com/terser/terser/blob/master/CHANGELOG.md#v5176

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,9 @@
-function isBabelRegister(caller) {
-  return !!(caller && caller.name === '@babel/register');
-}
-
 module.exports = (api) => {
-  const isRegister = api.caller(isBabelRegister);
-
   return {
     presets: [
       [
         '@babel/preset-env',
         {
-          ...(!isRegister &&
-            !api.env('test') && {
-              // Until we no longer support Safari 15, prevent ReferenceError issues
-              // by forcing class properties transforms
-              // https://github.com/babel/babel/issues/14289
-              include: [
-                '@babel/plugin-proposal-class-properties',
-                '@babel/plugin-proposal-private-methods',
-              ],
-            }),
           ...(api.env('test') ? { targets: { node: 'current' } } : {}),
         },
       ],
