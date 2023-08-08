b3b2fedc063722bbb471e45104f01e0cd678e9f5
Zach Wegrzyniak <zach@streak.com>
2023-08-08 09:50:41 -0400

    chore: tie interface to MessageView and its Driver (#973)
    also types event emitter in both
    
    Prelim for #939

--- src/platform-implementation-js/driver-interfaces/message-view-driver.ts
+++ src/platform-implementation-js/driver-interfaces/message-view-driver.ts
@@ -1,9 +1,12 @@
 import type * as Kefir from 'kefir';
 import type { AttachmentCardViewDriver } from './driver';
 import type {
-  MessageView,
+  AttachmentIcon,
+  Contact,
+  MessageAttachmentIconDescriptor,
   MessageViewToolbarButtonDescriptor,
 } from '../../inboxsdk';
+import { type MessageViewDriverEvents } from '../dom-driver/gmail/views/gmail-message-view';
 
 export type VIEW_STATE = 'HIDDEN' | 'COLLAPSED' | 'EXPANDED';
 export type MessageViewLinkDescriptor = {
@@ -13,18 +16,28 @@ export type MessageViewLinkDescriptor = {
   href: string;
   isInQuotedArea: boolean;
 };
-export type MessageViewDriver = MessageView & {
+export interface MessageViewDriver {
+  addAttachmentIcon(
+    opts:
+      | MessageAttachmentIconDescriptor
+      | Kefir.Stream<MessageAttachmentIconDescriptor, never>
+  ): AttachmentIcon;
+  isElementInQuotedArea(element: HTMLElement): boolean;
   getMessageID(): string;
   getContentsElement(): HTMLElement;
   addMoreMenuItem(options: MessageViewToolbarButtonDescriptor): void;
   getAttachmentCardViewDrivers(): Array<AttachmentCardViewDriver>;
   addAttachmentCard(options: Record<string, any>): AttachmentCardViewDriver;
   addButtonToDownloadAllArea(options: Record<string, any>): void;
-  getEventStream(): Kefir.Observable<Record<string, any>, unknown>;
+  getEventStream(): Kefir.Observable<MessageViewDriverEvents, unknown>;
+  getMessageIDAsync(): Promise<string>;
+  getRecipientsFull(): Promise<Array<Contact>>;
   getViewState(): VIEW_STATE;
   getDateString(): string;
   getReadyStream(): Kefir.Observable<any, unknown>;
   getRecipientEmailAddresses(): Array<string>;
+  getSender(): Contact;
   getThreadViewDriver(): Record<string, any>;
   hasOpenReply(): boolean;
-};
+  isLoaded(): boolean;
+}
