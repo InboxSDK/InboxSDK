93c772060ed88c8d8d2d0ee1cbf949197ed34fd0
Zach Wegrzyniak <zach@streak.com>
2023-08-16 13:23:14 -0400

    types: connect types from legacy NavMenu namespace down to shouldAddNavItemsInline

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/isIntegratedViewGmail.ts
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/isIntegratedViewGmail.ts
@@ -1,5 +1,10 @@
 /**
- * https://support.google.com/mail/answer/11555490
+ * There's three cases this function should return true:
+    - The user is in a pre-Google-Chat Hangouts-only version of Gmail (the Gmail Settings ->
+    Chat and Meet -> Chat menu only has the options "Hangouts On" and "Hangouts Off").
+    - The user has "Classic Hangouts" picked in Gmail Settings -> Chat and Meet -> Chat.
+    - Integrated view gmail.
+ * @see https://support.google.com/mail/answer/11555490
  */
 export default function isIntegratedViewGmail(): boolean {
   const nav = document.querySelector('div[role=navigation]');
