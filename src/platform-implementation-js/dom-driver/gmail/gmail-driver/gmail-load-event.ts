6ec3812d3b3aeceea014da8bde7e93b703128487
Zach Wegrzyniak <zach@streak.com>
2023-10-09 09:06:50 -0400

    load event isSidePanelCollapsed

--- src/platform-implementation-js/dom-driver/gmail/gmail-driver/gmail-load-event.ts
+++ src/platform-implementation-js/dom-driver/gmail/gmail-driver/gmail-load-event.ts
@@ -5,6 +5,30 @@ import isIntegratedViewGmail, {
 } from './isIntegratedViewGmail';
 import { waitForMenuReady } from './add-nav-item';
 import { checkForDarkThemeSafe } from './track-gmail-styles';
+import waitFor, { WaitForError } from '../../../lib/wait-for';
+
+async function isSidePanelCollapsed() {
+  const sidePanelSelector = '.brC-aT5-aOt-Jw[role=complementary]';
+
+  try {
+    await waitFor(() => document.querySelector(sidePanelSelector), 10_000);
+    // Wait for the collapse button to appear too. Otherwise, the side panel will initially appear without a display style set for collapsed.
+    await waitFor(() => document.querySelector('.aT5-aOt-I[role=button]'));
+  } catch (e: unknown) {
+    if (e instanceof WaitForError) {
+      return null;
+    }
+
+    throw e;
+  }
+
+  const maybeCollapsedSidePanel = document.querySelector(
+    `${sidePanelSelector}[style*="display: none;"]`,
+  );
+
+  // If the sidePanel is collapsed, it will have a style attribute with display: none
+  return maybeCollapsedSidePanel != null;
+}
 
 export default async function gmailLoadEvent(driver: GmailDriver) {
   const pageCommunicator = driver.getPageCommunicator();
@@ -13,16 +37,21 @@ export default async function gmailLoadEvent(driver: GmailDriver) {
     await pageCommunicator.isConversationViewDisabled();
   await waitForMenuReady();
   const isUsingDarkTheme = await checkForDarkThemeSafe();
+  const sidePanelCollapsed = await isSidePanelCollapsed();
 
   const timing = performance?.timing;
 
-  driver.getLogger().eventSite('gmailSettings', {
+  driver.logger.eventSite('gmailSettings', {
     isGmailIntegratedView: isIntegratedViewGmail(),
     /**
      * This is the panel on the left side of the screen. If the top left burger of Gmail is collapsed, then this is true.
      */
     isCollapsiblePanelHidden: isCollapsiblePanelHidden(),
     isGoogleChatEnabled: isGoogleChatEnabled(),
+    /**
+     * This is the sidePanel on the right side of the screen. The collapse button is at the bottom right corner of the screen.
+     */
+    isSidePanelCollapsed: sidePanelCollapsed,
     isDarkTheme: isUsingDarkTheme,
     screenWidth: window.screen?.width,
     screenHeight: window.screen?.height,
