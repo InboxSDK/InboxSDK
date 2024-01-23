4eca82b415664495b710c01f68ff0b62ffeb3a66
Zach Wegrzyniak <zach@streak.com>
2024-01-23 13:51:40 -0500

    true private in list-route-view and section-view

--- src/platform-implementation-js/views/route-view/list-route-view.ts
+++ src/platform-implementation-js/views/route-view/list-route-view.ts
@@ -3,71 +3,63 @@ import Kefir from 'kefir';
 import kefirCast from 'kefir-cast';
 import CollapsibleSectionView from '../collapsible-section-view';
 import SectionView from '../section-view';
-import get from '../../../common/get-or-fail';
 import type { RouteViewDriver } from '../../driver-interfaces/route-view-driver';
 import type { Driver } from '../../driver-interfaces/driver';
 
 class ListRouteView extends RouteView {
+  #routeViewDriver;
+  #driver;
+  #appId;
+  #sectionViews: { destroy(): void }[] = [];
+
   constructor(routeViewDriver: RouteViewDriver, driver: Driver, appId: string) {
     super(routeViewDriver);
-    const members = {
-      routeViewDriver,
-      driver,
-      appId,
-      sectionViews: [],
-    };
-    membersMap.set(this, members);
+    this.#routeViewDriver = routeViewDriver;
+    this.#driver = driver;
+    this.#appId = appId;
 
-    _bindToEventStream(routeViewDriver, this);
+    this.#bindToEventStream();
   }
 
   addCollapsibleSection(
     collapsibleSectionDescriptor: Record<string, any> | null | undefined,
   ): CollapsibleSectionView {
-    const members = get(membersMap, this);
     const collapsibleSectionViewDriver =
-      members.routeViewDriver.addCollapsibleSection(
-        kefirCast(Kefir as any, collapsibleSectionDescriptor).toProperty(),
-        members.appId,
+      this.#routeViewDriver.addCollapsibleSection(
+        kefirCast(Kefir, collapsibleSectionDescriptor).toProperty(),
+        this.#appId,
       );
     const collapsibleSectionView = new CollapsibleSectionView(
       collapsibleSectionViewDriver,
-      members.driver,
+      this.#driver,
     );
-    members.sectionViews.push(collapsibleSectionView);
+    this.#sectionViews.push(collapsibleSectionView);
     return collapsibleSectionView;
   }
 
   addSection(
     sectionDescriptor: Record<string, any> | null | undefined,
   ): SectionView {
-    const members = get(membersMap, this);
-    const sectionViewDriver = members.routeViewDriver.addSection(
-      kefirCast(Kefir as any, sectionDescriptor).toProperty(),
-      members.appId,
+    const sectionViewDriver = this.#routeViewDriver.addSection(
+      kefirCast(Kefir, sectionDescriptor).toProperty(),
+      this.#appId,
     );
-    const sectionView = new SectionView(sectionViewDriver, members.driver);
-    members.sectionViews.push(sectionView);
+    const sectionView = new SectionView(sectionViewDriver, this.#driver);
+    this.#sectionViews.push(sectionView);
     return sectionView;
   }
 
   refresh() {
-    get(membersMap, this).routeViewDriver.refresh();
+    this.#routeViewDriver.refresh();
   }
-}
 
-const membersMap = new WeakMap();
-
-function _bindToEventStream(
-  routeViewDriver: RouteViewDriver,
-  routeView: ListRouteView,
-) {
-  routeViewDriver.getEventStream().onEnd(() => {
-    const members = get(membersMap, routeView);
-    members.sectionViews.forEach((sectionView: { destroy(): void }) => {
-      sectionView.destroy();
+  #bindToEventStream() {
+    this.#routeViewDriver.getEventStream().onEnd(() => {
+      this.#sectionViews.forEach((sectionView) => {
+        sectionView.destroy();
+      });
     });
-  });
+  }
 }
 
 export default ListRouteView;
