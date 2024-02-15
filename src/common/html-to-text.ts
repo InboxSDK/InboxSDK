a288659d97061a45c9bb5be4042ecc42969b75f1
Zach Wegrzyniak <zach@streak.com>
2024-02-14 19:24:28 -0500

    add trustedTypes with Safari fallback
    Account for folks starting to get https://workspaceupdates.googleblog.com/2024/01/extending-trusted-types-to-gmail.html as part of Rapid release

--- src/common/html-to-text.ts
+++ src/common/html-to-text.ts
@@ -1,7 +1,25 @@
-// Quick function for converting HTML with entities into text without
-// introducing an XSS vulnerability.
+import { sanitize } from 'dompurify';
+
+// https://developer.mozilla.org/en-US/docs/Web/API/TrustedHTML
+// TS doesn't ship types for trustedTypes yet, so we'll use any for now.
+// Safari 17 doesn't support it either, so fallback if it's not defined.
+const escapeHTMLPolicy = (globalThis as any).trustedTypes?.createPolicy(
+  'inboxSdkEscapePolicy',
+  {
+    createHTML: (string: string) => sanitize(string),
+  },
+) ?? {
+  createHTML(string: string) {
+    return sanitize(string);
+  },
+};
+
+/**
+ * Quick function for converting HTML with entities into text without
+ * introducing an XSS vulnerability.
+ */
 export default function htmlToText(html: string): string {
   const div = document.createElement('div');
-  div.innerHTML = html.replace(/<[^>]*>?/g, '');
+  div.innerHTML = escapeHTMLPolicy.createHTML(html);
   return div.textContent!;
 }
