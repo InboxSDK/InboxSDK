e6a5cceef732a91e4023ca6c79c31ab3e777fb39
Zach Wegrzyniak <zach@streak.com>
2024-02-15 07:59:25 -0500

    remove any usages and add TSDoc

--- src/common/html-to-text.ts
+++ src/common/html-to-text.ts
@@ -1,9 +1,29 @@
 import { sanitize } from 'dompurify';
 
-// https://developer.mozilla.org/en-US/docs/Web/API/TrustedHTML
-// TS doesn't ship types for trustedTypes yet, so we'll use any for now.
-// Safari 17 doesn't support it either, so fallback if it's not defined.
-const escapeHTMLPolicy = (globalThis as any).trustedTypes?.createPolicy(
+interface Policy {
+  /**
+   * This method returns a {@link TrustedTypePolicy},
+   * but typescript@5.3.3's lib.dom doesn't support assigning non-strings
+   * to {@link HTMLElement.innerHTML}.
+   */
+  createHTML(string: string): string;
+}
+
+declare global {
+  /**
+   * typescript@5.3.3 doesn't ship types for trustedTypes yet.
+   * Safari 17.2 doesn't support it either, so a fallback is needed if
+   * it's not defined.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/TrustedHTML
+   */
+  var trustedTypes:
+    | {
+        createPolicy(name: string, policy: Policy): Policy;
+      }
+    | undefined;
+}
+
+const escapeHTMLPolicy = globalThis.trustedTypes?.createPolicy(
   'inboxSdkEscapePolicy',
   {
     createHTML: (string: string) => sanitize(string),
