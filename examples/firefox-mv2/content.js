955d6c34bd574e196857a74de0aa097ac63c00a6
dapotatoman <iam.fahadul@gmail.com>
2024-03-23 15:06:10 +0600

    chore: add firefox examples

---
+++ examples/firefox-mv2/content.js
@@ -0,0 +1,24 @@
+'use strict';
+
+console.log('InboxSDK content script loaded');
+
+InboxSDK.load(2, 'sidebar-example', {
+  appName: 'Twitter',
+  appIconUrl:
+    'http://materialdesignblog.com/wp-content/uploads/2015/04/387b93c8b66379c32e1cc2b98dcf5197.png',
+  suppressAddonTitle: 'Streak',
+}).then((sdk) => {
+  console.log('InboxSDK has loaded', sdk);
+
+  sdk.Compose.registerComposeViewHandler((view) => {
+    view.addButton({
+      title: 'My button',
+      iconUrl: (globalThis.chrome || globalThis.browser).runtime.getURL(
+        'monkey.png',
+      ),
+      onClick() {
+        alert('my button works!');
+      },
+    });
+  });
+});
