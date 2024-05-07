e8774ac092d68aa39c05963e6b1eeff107cdcc0f
Zach Wegrzyniak <zach@streak.com>
2024-05-07 07:59:49 -0400

    consolidate esbuild watcher into one

--- examples/build.ts
+++ examples/build.ts
@@ -5,8 +5,7 @@ import esbuild from 'esbuild';
 type BuildConfig = Parameters<typeof esbuild.build>[0];
 type Watcher = ReturnType<typeof esbuild.context>;
 
-let watchingContentScripts: undefined | Watcher;
-let watchingSdkOutput: undefined | Watcher;
+let esbuildWatch: undefined | Watcher;
 const TIMING_LABEL = 'building ./examples took';
 
 const buildSdkFiles = ['@inboxsdk/core/pageWorld', '@inboxsdk/core/background'];
@@ -23,94 +22,56 @@ export async function buildExamples({
 }) {
   console.time(TIMING_LABEL);
 
-  const sharedConfig: BuildConfig = {
+  const sdkEntryPoints = buildSdkFiles
+    .flatMap((sdkFile) =>
+      contentScriptFps.flatMap((c) => [
+        {
+          out: path.join(path.dirname(c), path.basename(sdkFile)),
+          in: sdkFile,
+        },
+      ]),
+    )
+    .concat(
+      contentScriptFps.map((c) => ({
+        out: path.join(
+          path.dirname(c),
+          'dist',
+          path.basename(c).replace(/\.[jt]s$/, ''),
+        ),
+        in: c,
+      })),
+    );
+
+  const config: BuildConfig = {
     define: {
       // closest-ng depends on node `global`.
       global: 'globalThis',
     },
-    entryNames: '[name]',
     bundle: true,
     loader: {
       '.tsx': 'tsx',
       '.ts': 'ts',
       '.js': 'js',
     },
+    logLevel: 'info',
     tsconfig: './tsconfig.json',
     platform: 'browser',
     sourcemap: 'linked',
     target: 'es2022',
     minify,
-  };
-
-  const sdkEntryPoints = buildSdkFiles.flatMap((sdkFile) =>
-    contentScriptFps.flatMap((c) => [
-      {
-        out: path.join(path.dirname(c), path.basename(sdkFile)),
-        in: sdkFile,
-      },
-    ]),
-  );
-
-  const sdkBuildConfig: BuildConfig = {
-    ...sharedConfig,
     entryPoints: sdkEntryPoints,
     outbase: '.',
     outdir: '.',
     entryNames: '[dir]/[name]',
   };
 
-  const sdkFileBuild = watchOrBuild({
-    awaitRebuild: true,
-    config: sdkBuildConfig,
-    state: watchingSdkOutput,
-    watch,
-  });
+  if (watch && esbuildWatch == null) {
+    esbuildWatch = esbuild.context(config);
 
-  const contentScriptOpts = {
-    ...sharedConfig,
-    entryPoints: contentScriptFps,
-    entryNames: '[dir]/dist/[name]',
-    outdir: 'examples',
-  } as const;
-
-  const contentScriptBuild = watchOrBuild({
-    config: contentScriptOpts,
-    awaitRebuild: false,
-    state: watchingContentScripts,
-    watch,
-  });
-
-  await Promise.all([sdkFileBuild, contentScriptBuild]);
-  console.timeEnd(TIMING_LABEL);
-}
-
-async function watchOrBuild({
-  awaitRebuild,
-  config,
-  state,
-  watch,
-}: {
-  awaitRebuild: boolean;
-  config: BuildConfig;
-  state: Promise<esbuild.BuildContext<esbuild.BuildOptions>> | undefined;
-  watch: boolean;
-}) {
-  if (watch) {
-    const watcher = await state;
-    if (watcher == null) {
-      state = esbuild.context({
-        logLevel: 'info',
-        ...config,
-      });
-
-      (await state).watch();
-    } else {
-      if (awaitRebuild) {
-        await watcher.cancel();
-        await watcher.rebuild();
-      }
-    }
+    (await esbuildWatch).watch();
   } else {
     await esbuild.build(config);
   }
+
+  console.timeEnd(TIMING_LABEL);
 }
