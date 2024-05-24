8a55aa1336cff75ae83485a14dcbb8e3dd046c50
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2024-05-24 14:10:35 -0400

    Update src/platform-implementation-js/lib/fromEventsWithOptions.ts

--- src/platform-implementation-js/lib/fromEventsWithOptions.ts
+++ src/platform-implementation-js/lib/fromEventsWithOptions.ts
@@ -3,7 +3,7 @@ import * as Kefir from 'kefir';
 export default function fromEventsWithOptions(
   target: EventTarget,
   eventName: string,
-  options: boolean,
+  options: Parameters<typeof target.addEventListener>[2],
 ): Kefir.Observable<any, never> {
   return Kefir.stream((emitter) => {
     target.addEventListener(eventName, emitter.emit, options);
