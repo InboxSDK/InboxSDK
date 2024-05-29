38e123b6c15aca5b7c478e49a9e290f10f9f43a9
Chris Cowan <agentme49@gmail.com>
2024-05-28 17:41:14 -0700

    fix reading extension id in MV3

--- src/common/get-extension-id.ts
+++ src/common/get-extension-id.ts
@@ -1,6 +1,9 @@
 export default function getExtensionId(): string | null {
   const chrome: any = (global as any).chrome;
-  if (chrome && chrome.extension && chrome.extension.getURL) {
+  if (chrome?.runtime?.getURL) {
+    return chrome.runtime.getURL('');
+  }
+  if (chrome?.extension?.getURL) {
     return chrome.extension.getURL('');
   }
   return null;
