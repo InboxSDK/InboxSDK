e70bb373ddc0f31bef775b888a56be9593c08780
Chris Cowan <agentme49@gmail.com>
2024-06-18 13:33:05 -0700

    catch exceptions in getExtensionId

--- src/common/get-extension-id.ts
+++ src/common/get-extension-id.ts
@@ -1,10 +1,19 @@
 export default function getExtensionId(): string | null {
-  const chrome: any = (global as any).chrome;
-  if (chrome?.runtime?.getURL) {
-    return chrome.runtime.getURL('');
-  }
-  if (chrome?.extension?.getURL) {
-    return chrome.extension.getURL('');
+  try {
+    const chrome: any = (global as any).chrome;
+    if (chrome?.runtime?.getURL) {
+      return chrome.runtime.getURL('');
+    }
+    // MV2
+    if (chrome?.extension?.getURL) {
+      return chrome.extension.getURL('');
+    }
+  } catch (e) {
+    // When an extension is reloaded or removed, then Chrome APIs in any of its
+    // pre-existing content scripts start throwing "Extension context
+    // invalidated" errors. We only use extension IDs for logging, so we
+    // shouldn't treat this as fatal.
+    console.error('Failed to get extension ID:', e);
   }
   return null;
 }
