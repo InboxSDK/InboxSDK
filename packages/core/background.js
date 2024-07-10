74bae7622775eae030f1d84c2d4304cda81ed3b6
Chris Cowan <agentme49@gmail.com>
2024-07-10 12:53:52 -0700

    Add protections against https://github.com/w3c/webextensions/issues/8

--- packages/core/background.js
+++ packages/core/background.js
@@ -3,8 +3,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'inboxsdk__injectPageWorld' && sender.tab) {
     if (chrome.scripting) {
       // MV3
+      let documentIds;
+      let frameIds;
+      if (sender.documentId) {
+        // Protect against https://github.com/w3c/webextensions/issues/8 in
+        // browsers (Chrome 106+) that support the documentId property.
+        // Protections for other browsers happen inside the injected script.
+        documentIds = [sender.documentId];
+      } else {
+        frameIds = [sender.frameId];
+      }
       chrome.scripting.executeScript({
-        target: { tabId: sender.tab.id, frameIds: [sender.frameId] },
+        target: { tabId: sender.tab.id, documentIds, frameIds },
         world: 'MAIN',
         files: ['pageWorld.js'],
       });
